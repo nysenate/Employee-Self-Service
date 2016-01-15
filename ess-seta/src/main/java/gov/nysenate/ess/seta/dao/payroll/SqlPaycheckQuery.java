c3a8e187ab77becb4f861c8c7ac2f0c7ede14135
Kevin Caseiras <kcaseiras@gmail.com>
2016-01-15 13:43:56 -0500

    Remove hard coded schema from paycheck history dao

--- ess-seta/src/main/java/gov/nysenate/ess/seta/dao/payroll/SqlPaycheckQuery.java
+++ ess-seta/src/main/java/gov/nysenate/ess/seta/dao/payroll/SqlPaycheckQuery.java
@@ -8,8 +8,8 @@ public enum SqlPaycheckQuery implements BasicSqlQuery
     GET_EMPLOYEE_PAYCHECKS_BY_YEAR(
            "SELECT m.NUXREFEM, m.MONET, m.MOGROSS, m.MOCHECKAMT, m.MOADVICEAMT, m.NUPERIOD, m.DTCHECK," +
            "l.CDDEDUCTION, l.DEDEDUCTIONF, d.MODEDUCTION\n" +
-           "from ESS_DEV.PM25SALLEDG m join ESS_DEV.PD25SALLEDG d on m.nuxrefem = d.nuxrefem and m.dtcheck = d.dtcheck\n" +
-           "join ESS_DEV.PL25DEDUCTCD l on d.CDDEDUCTION = l.CDDEDUCTION\n" +
+           "from ${tsSchema}.PM25SALLEDG m join ${tsSchema}.PD25SALLEDG d on m.nuxrefem = d.nuxrefem and m.dtcheck = d.dtcheck\n" +
+           "join ${tsSchema}.PL25DEDUCTCD l on d.CDDEDUCTION = l.CDDEDUCTION\n" +
            "where m.NUXREFEM = :empId and EXTRACT(YEAR FROM m.DTCHECK) = :year and d.cdstatus = 'A'"
     );
 
