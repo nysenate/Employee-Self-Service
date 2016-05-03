7e7909d7ae4120bfd99535560239947c0e63f1c1
Kevin Caseiras <kcaseiras@gmail.com>
2016-05-03 17:08:15 -0400

    Requisition View redesign init method

--- ess-web/src/main/webapp/assets/js/src/supply/requisition/supply-view-ctrl.js
+++ ess-web/src/main/webapp/assets/js/src/supply/requisition/supply-view-ctrl.js
@@ -1,26 +1,81 @@
 essSupply = angular.module('essSupply').controller('SupplyViewController', ['$scope', 'SupplyShipmentByIdApi',
-    'SupplyInventoryService', 'LocationService', '$window', '$timeout', supplyViewController]);
+    'LocationService', '$window', '$timeout', supplyViewController]);
 
-function supplyViewController($scope, shipmentApi, inventoryService, locationService, $window, $timeout) {
+function supplyViewController($scope, shipmentApi, locationService, $window, $timeout) {
 
-    $scope.order = {};
-    
-    $scope.init = function() {
+    $scope.shipmentResource = {};
+    $scope.shipment = {};
+    $scope.versions = [];
+
+    $scope.init = function () {
         var id = locationService.getSearchParam('shipment');
+        $scope.shipmentResource = shipmentApi.get({id: id});
+        $scope.shipmentResource.$promise
+            .then(extractShipment)
+            .then(printIfRequested)
+            .then(assembleVersions)
+            .catch(shipmentResourceErrorHandler);
+    };
 
-        shipmentApi.get({id: id}, function(response) {
-            $scope.shipment = response.result;
-            $scope.order = $scope.shipment.order;
-            console.log($scope.order);
-            var print = locationService.getSearchParam('print');
-            if (print === 'true') {
-                $timeout(function() {
-                    $window.print();
-                })
+    var extractShipment = function (response) {
+        $scope.shipment = response.result;
+        console.log($scope.shipmentResource);
+    };
+
+    var printIfRequested = function () {
+        var print = locationService.getSearchParam('print');
+        if (print === 'true') {
+            $timeout(function () {
+                $window.print();
+            })
+        }
+    };
+
+    var assembleVersions = function () {
+        var orderVersions = $scope.shipment.order.history.items;
+        var shipmentVersions = $scope.shipment.history.items;
+        $scope.versions = combineVersions(orderVersions, shipmentVersions);
+        console.log($scope.versions);
+    };
+
+    // Create version object.. add append/join version functions to it?
+    function combineVersions(orderVersions, shipmentVersions) {
+        var versions = angular.copy(orderVersions);
+        joinShipmentVersions(shipmentVersions, versions);
+        return versions;
+    }
+
+    function joinShipmentVersions(shipmentVersions, versions) {
+        for (var key in shipmentVersions) {
+            if (shipmentVersions.hasOwnProperty(key)) {
+                var matchedKey = findVersionKeyMatching(versions, key);
+                if (matchedKey) {
+                    versions[matchedKey].shipmentStatus = shipmentVersions[key].status;
+                    versions[matchedKey].issuer = shipmentVersions[key].issuer;
+                }
+                else {
+                    versions[key] = {
+                        shipmentStatus: shipmentVersions[key].status,
+                        issuer: shipmentVersions[key].issuer
+                    };
+                }
             }
-        }, function(errorResponse) {
-            // TODO: handle api error
-        });
+        }
+    }
+
+    function findVersionKeyMatching(versions, shipmentKey) {
+        for (var verDate in versions) {
+            if (versions.hasOwnProperty(verDate)) {
+                if (Math.abs(new Date(verDate) - new Date(shipmentKey)) < 1000) {
+                    return verDate;
+                }
+            }
+        }
+    }
+
+    var shipmentResourceErrorHandler = function (response) {
+        console.log("Error");
+        // TODO;
     };
 
     $scope.init();
