1544ae6af77b5cdc2772d76f9ee56922e8c22238
Kevin Caseiras <kcaseiras@gmail.com>
2016-07-06 10:54:35 -0400

    Ess role dao implementation
    refs #10243

--- ess-core/src/main/java/gov/nysenate/ess/core/dao/permission/SqlRoleDao.java
+++ ess-core/src/main/java/gov/nysenate/ess/core/dao/permission/SqlRoleDao.java
@@ -1,16 +1,51 @@
 package gov.nysenate.ess.core.dao.permission;
 
 import com.google.common.collect.ImmutableList;
+import gov.nysenate.ess.core.dao.base.BasicSqlQuery;
+import gov.nysenate.ess.core.dao.base.DbVendor;
+import gov.nysenate.ess.core.dao.base.SqlBaseDao;
 import gov.nysenate.ess.core.model.permission.EssRole;
 import gov.nysenate.ess.core.model.personnel.Employee;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class SqlRoleDao implements RoleDao {
+import java.util.List;
 
-    /** {@inheritDoc}
-     * @param employee*/
+@Repository
+public class SqlRoleDao extends SqlBaseDao implements RoleDao {
+
+    /** {@inheritDoc} */
     public ImmutableList<EssRole> getRoles(Employee employee) {
-        return ImmutableList.of(EssRole.SENATE_EMPLOYEE, EssRole.SUPPLY_EMPLOYEE, EssRole.SUPPLY_MANAGER);
+        MapSqlParameterSource params = new MapSqlParameterSource("employeeId", employee.getEmployeeId());
+        String sql = SqlRoleQuery.GET_EMPLOYEE_ROLES.getSql(schemaMap());
+        List<EssRole> roles = localNamedJdbc.query(sql, params, ((rs, i) -> {
+            return EssRole.valueOf(rs.getString("role"));
+        }));
+        // Everyone has the senate employee role by default.
+        roles.add(EssRole.SENATE_EMPLOYEE);
+        return ImmutableList.copyOf(roles);
+    }
+
+    public enum SqlRoleQuery implements BasicSqlQuery {
+        GET_EMPLOYEE_ROLES(
+                "SELECT * FROM ${essSchema}.user_roles \n" +
+                "WHERE employee_id = :employeeId"
+        );
+
+        private String sql;
+
+        SqlRoleQuery(String sql) {
+            this.sql = sql;
+        }
+
+        @Override
+        public String getSql() {
+            return this.sql;
+        }
+
+        @Override
+        public DbVendor getVendor() {
+            return DbVendor.POSTGRES;
+        }
     }
 }
