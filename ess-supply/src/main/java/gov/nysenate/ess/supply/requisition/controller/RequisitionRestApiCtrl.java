98090afd85352a3b52b9a4201edf09ce83065be8
Kevin Caseiras <kcaseiras@gmail.com>
2016-07-08 14:16:51 -0400

    View and controller refactoring

--- ess-supply/src/main/java/gov/nysenate/ess/supply/requisition/controller/RequisitionRestApiCtrl.java
+++ ess-supply/src/main/java/gov/nysenate/ess/supply/requisition/controller/RequisitionRestApiCtrl.java
@@ -10,7 +10,6 @@ import gov.nysenate.ess.core.client.view.EmployeeView;
 import gov.nysenate.ess.core.controller.api.BaseRestApiCtrl;
 import gov.nysenate.ess.core.model.auth.SenatePerson;
 import gov.nysenate.ess.core.model.personnel.Employee;
-import gov.nysenate.ess.core.model.unit.Location;
 import gov.nysenate.ess.core.model.unit.LocationId;
 import gov.nysenate.ess.core.service.personnel.EmployeeInfoService;
 import gov.nysenate.ess.core.service.unit.LocationService;
@@ -19,6 +18,7 @@ import gov.nysenate.ess.core.util.PaginatedList;
 import gov.nysenate.ess.supply.item.LineItem;
 import gov.nysenate.ess.supply.item.view.LineItemView;
 import gov.nysenate.ess.supply.requisition.exception.ConcurrentRequisitionUpdateException;
+import gov.nysenate.ess.supply.requisition.view.RequisitionView;
 import gov.nysenate.ess.supply.requisition.view.SubmitRequisitionView;
 import gov.nysenate.ess.supply.requisition.Requisition;
 import gov.nysenate.ess.supply.requisition.RequisitionStatus;
@@ -51,144 +51,125 @@ public class RequisitionRestApiCtrl extends BaseRestApiCtrl {
         for (LineItemView lineItemView : submitRequisitionView.getLineItems()) {
             lineItems.add(lineItemView.toLineItem());
         }
-        LocalDateTime orderedDateTime = LocalDateTime.now();
         Requisition requisition = new Requisition.Builder()
                 .withCustomer(employeeService.getEmployee(submitRequisitionView.getCustomerId()))
                 .withDestination(locationService.getLocation(new LocationId(submitRequisitionView.getDestinationId())))
                 .withLineItems(lineItems)
                 .withStatus(RequisitionStatus.PENDING)
                 .withModifiedBy(employeeService.getEmployee(submitRequisitionView.getCustomerId()))
-                .withOrderedDateTime(orderedDateTime)
+                .withOrderedDateTime(LocalDateTime.now())
                 .build();
-        Requisition id = requisitionService.saveRequisition(requisition);
-//        return new ViewObjectResponse<>(new RequisitionView(requisitionService.getRequisitionById(id)));
-        return null;
+        Requisition savedRequisition = requisitionService.saveRequisition(requisition);
+        return new ViewObjectResponse<>(new RequisitionView(savedRequisition));
     }
-//
-//    @RequestMapping("/{id}")
-//    public BaseResponse getRequisitionById(@PathVariable int id,
-//                                           @RequestParam(defaultValue = "false", required = false) boolean detail) {
-//        Requisition requisition = requisitionService.getRequisitionById(id);
-//        return new ViewObjectResponse<>(detail ? new DetailedRequisitionView(requisition) : new RequisitionView(requisition));
-//    }
-//
-//    @RequestMapping("")
-//    public BaseResponse searchRequisitions(@RequestParam(defaultValue = "all", required = false) String location,
-//                                           @RequestParam(defaultValue = "all", required = false) String customerId,
-//                                           @RequestParam(required = false) String[] status,
-//                                           @RequestParam(required = false) String from,
-//                                           @RequestParam(required = false) String to,
-//                                           @RequestParam(required = false) String dateField,
-//                                           @RequestParam(defaultValue = "false", required = false) boolean detail,
-//                                           WebRequest webRequest) {
-//        LocalDateTime fromDateTime = getFromDateTime(from);
-//        LocalDateTime toDateTime = getToDateTime(to);
-//        EnumSet<RequisitionStatus> statuses = getStatusEnumSet(status);
-//        dateField = dateField == null ? "modified_date_time" : dateField;
-//
-//        LimitOffset limoff = getLimitOffset(webRequest, 25);
-//        Range<LocalDateTime> dateRange = getClosedRange(fromDateTime, toDateTime, "from", "to");
-//        PaginatedList<Requisition> results = requisitionService.searchRequisitions(location, customerId, statuses, dateRange, dateField, limoff);
-//        List<RequisitionView> resultViews = results.getResults().stream()
-//                                                   .map(detail ? DetailedRequisitionView::new : RequisitionView::new)
-//                                                   .collect(Collectors.toList());
-//        return ListViewResponse.of(resultViews, results.getTotal(), results.getLimOff());
-//    }
-//
-//    @RequestMapping("/orderHistory")
-//    public BaseResponse orderHistory(@RequestParam String location,
-//                                     @RequestParam int customerId,
-//                                     @RequestParam(required = false) String[] status,
-//                                     @RequestParam(required = false) String from,
-//                                     @RequestParam(required = false) String to,
-//                                     @RequestParam(required = false) String dateField,
-//                                     @RequestParam(defaultValue = "false", required = false) boolean detail,
-//                                     WebRequest webRequest) {
-//        LocalDateTime fromDateTime = getFromDateTime(from);
-//        LocalDateTime toDateTime = getToDateTime(to);
-//        EnumSet<RequisitionStatus> statuses = getStatusEnumSet(status);
-//        dateField = dateField == null ? "ordered_date_time" : dateField;
-//        LimitOffset limoff = getLimitOffset(webRequest, 25);
-//
-//        Range<LocalDateTime> dateRange = getClosedRange(fromDateTime, toDateTime, "from", "to");
-//        PaginatedList<Requisition> results = requisitionService.searchOrderHistory(location, customerId, statuses, dateRange, dateField, limoff);
-//        List<RequisitionView> resultViews = results.getResults().stream()
-//                                                   .map(detail ? DetailedRequisitionView::new : RequisitionView::new)
-//                                                   .collect(Collectors.toList());
-//        return ListViewResponse.of(resultViews, results.getTotal(), results.getLimOff());
-//    }
-//
-//
-//    /**
-//     * Call this api to add a new {@link RequisitionVersion} to a {@link Requisition} and save it.
-//     * The new version to be saved should be set as the requisitionView.activeVersion.
-//     * No other data in the RequisitionView should be touched.
-//     *
-//     * @param id              The id of the requisition.
-//     * @param requisitionView A view containing requisition meta data along with the version to be saved.
-//     */
-//    @RequestMapping(value = "/{id}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-//    public void saveRequisition(@PathVariable int id, @RequestBody RequisitionView requisitionView) {
-//        requisitionView.getActiveVersion().setCreatedBy(getSubjectEmployeeView());
-//        requisitionView.getActiveVersion().setId(0);
-//        requisitionService.updateRequisition(id, requisitionView.getActiveVersion().toRequisitionVersion(),
-//                                             requisitionView.getModifiedDateTime());
-//    }
-//
-//    @RequestMapping("/{id}/undoReject")
-//    public void undoRejection(@PathVariable int id) {
-//        // TODO: this is not setting the correct created by employee.
-//        Requisition requisition = requisitionService.getRequisitionById(id);
-//        requisitionService.undoRejection(requisition);
-//    }
-//
-//    @ResponseStatus(HttpStatus.CONFLICT)
-//    @ExceptionHandler(ConcurrentRequisitionUpdateException.class)
-//    @ResponseBody
-//    public ErrorResponse handleConcurrentRequisitionUpdate(ConcurrentRequisitionUpdateException ex) {
-//        return new ErrorResponse(ErrorCode.REQUISITION_UPDATE_CONFLICT);
-//    }
-//
-//    /**
-//     * @return the LocalDateTime represented by {@code from} or a LocalDateTime from one month ago if from is null.
-//     */
-//    private LocalDateTime getFromDateTime(String from) {
-//        return from == null ? LocalDateTime.now().minusMonths(1) : parseISODateTime(from, "from");
-//    }
-//
-//    /**
-//     * @return the LocalDateTime represented by {@code to} or the current LocalDateTime.
-//     */
-//    private LocalDateTime getToDateTime(@RequestParam(required = false) String to) {
-//        return to == null ? LocalDateTime.now() : parseISODateTime(to, "to");
-//    }
-//
-//    /**
-//     * @param status An array of strings each representing a {@link RequisitionStatus}.
-//     * @return An enumset of the given statuses or an enumset of all RequisitionStatuses if status is null.
-//     */
-//    private EnumSet<RequisitionStatus> getStatusEnumSet(String[] status) {
-//        return status == null ? EnumSet.allOf(RequisitionStatus.class) : getEnumSetFromStringArray(status);
-//    }
-//
-//    private EnumSet<RequisitionStatus> getEnumSetFromStringArray(String[] status) {
-//        List<RequisitionStatus> statusList = new ArrayList<>();
-//        for (String s : status) {
-//            statusList.add(RequisitionStatus.valueOf(s));
-//        }
-//        return EnumSet.copyOf(statusList);
-//    }
-//
-//    private EmployeeView getSubjectEmployeeView() {
-//        return new EmployeeView(getModifiedBy());
-//    }
-//
-//    private Employee getModifiedBy() {
-//        return employeeService.getEmployee(getSubjectEmployeeId());
-//    }
-//
-//    private int getSubjectEmployeeId() {
-//        SenatePerson person = (SenatePerson) getSubject().getPrincipals().getPrimaryPrincipal();
-//        return person.getEmployeeId();
-//    }
+
+    @RequestMapping("/{id}")
+    public BaseResponse getRequisitionById(@PathVariable int id,
+                                           @RequestParam(defaultValue = "false", required = false) boolean history) {
+        Requisition requisition = requisitionService.getRequisitionById(id).orElse(null);
+        return new ViewObjectResponse<>(history ? null : new RequisitionView(requisition)); // TODO: implement history
+    }
+
+    @RequestMapping("")
+    public BaseResponse searchRequisitions(@RequestParam(defaultValue = "all", required = false) String location,
+                                           @RequestParam(defaultValue = "all", required = false) String customerId,
+                                           @RequestParam(required = false) String[] status,
+                                           @RequestParam(required = false) String from,
+                                           @RequestParam(required = false) String to,
+                                           @RequestParam(required = false) String dateField,
+                                           @RequestParam(defaultValue = "false", required = false) boolean history,
+                                           WebRequest webRequest) {
+        LocalDateTime fromDateTime = getFromDateTime(from);
+        LocalDateTime toDateTime = getToDateTime(to);
+        EnumSet<RequisitionStatus> statuses = getStatusEnumSet(status);
+        dateField = dateField == null ? "modified_date_time" : dateField;
+
+        LimitOffset limoff = getLimitOffset(webRequest, 25);
+        Range<LocalDateTime> dateRange = getClosedRange(fromDateTime, toDateTime, "from", "to");
+        PaginatedList<Requisition> results = requisitionService.searchRequisitions(location, customerId, statuses, dateRange, dateField, limoff);
+        List<RequisitionView> resultViews = results.getResults().stream()
+                                                   .map(history ? null : RequisitionView::new) // TODO: history view
+                                                   .collect(Collectors.toList());
+        return ListViewResponse.of(resultViews, results.getTotal(), results.getLimOff());
+    }
+
+    @RequestMapping("/orderHistory")
+    public BaseResponse orderHistory(@RequestParam String location,
+                                     @RequestParam int customerId,
+                                     @RequestParam(required = false) String[] status,
+                                     @RequestParam(required = false) String from,
+                                     @RequestParam(required = false) String to,
+                                     @RequestParam(required = false) String dateField,
+                                     @RequestParam(defaultValue = "false", required = false) boolean history,
+                                     WebRequest webRequest) {
+        LocalDateTime fromDateTime = getFromDateTime(from);
+        LocalDateTime toDateTime = getToDateTime(to);
+        EnumSet<RequisitionStatus> statuses = getStatusEnumSet(status);
+        dateField = dateField == null ? "ordered_date_time" : dateField;
+        LimitOffset limoff = getLimitOffset(webRequest, 25);
+
+        Range<LocalDateTime> dateRange = getClosedRange(fromDateTime, toDateTime, "from", "to");
+        PaginatedList<Requisition> results = requisitionService.searchOrderHistory(location, customerId, statuses, dateRange, dateField, limoff);
+        List<RequisitionView> resultViews = results.getResults().stream()
+                                                   .map(history ? null : RequisitionView::new) // TODO: history view
+                                                   .collect(Collectors.toList());
+        return ListViewResponse.of(resultViews, results.getTotal(), results.getLimOff());
+    }
+
+    // TODO: PUT?
+    @RequestMapping(value = "/{id}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void saveRequisition(@PathVariable int id, @RequestBody RequisitionView requisitionView) {
+        requisitionView.setModifiedBy(getSubjectEmployeeView());
+        requisitionService.saveRequisition(requisitionView.toRequisition());
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(ConcurrentRequisitionUpdateException.class)
+    @ResponseBody
+    public ErrorResponse handleConcurrentRequisitionUpdate(ConcurrentRequisitionUpdateException ex) {
+        return new ErrorResponse(ErrorCode.REQUISITION_UPDATE_CONFLICT);
+    }
+
+    /**
+     * @return the LocalDateTime represented by {@code from} or a LocalDateTime from one month ago if from is null.
+     */
+    private LocalDateTime getFromDateTime(String from) {
+        return from == null ? LocalDateTime.now().minusMonths(1) : parseISODateTime(from, "from");
+    }
+
+    /**
+     * @return the LocalDateTime represented by {@code to} or the current LocalDateTime.
+     */
+    private LocalDateTime getToDateTime(@RequestParam(required = false) String to) {
+        return to == null ? LocalDateTime.now() : parseISODateTime(to, "to");
+    }
+
+    /**
+     * @param status An array of strings each representing a {@link RequisitionStatus}.
+     * @return An enumset of the given statuses or an enumset of all RequisitionStatuses if status is null.
+     */
+    private EnumSet<RequisitionStatus> getStatusEnumSet(String[] status) {
+        return status == null ? EnumSet.allOf(RequisitionStatus.class) : getEnumSetFromStringArray(status);
+    }
+
+    private EnumSet<RequisitionStatus> getEnumSetFromStringArray(String[] status) {
+        List<RequisitionStatus> statusList = new ArrayList<>();
+        for (String s : status) {
+            statusList.add(RequisitionStatus.valueOf(s));
+        }
+        return EnumSet.copyOf(statusList);
+    }
+
+    private EmployeeView getSubjectEmployeeView() {
+        return new EmployeeView(getModifiedBy());
+    }
+
+    private Employee getModifiedBy() {
+        return employeeService.getEmployee(getSubjectEmployeeId());
+    }
+
+    private int getSubjectEmployeeId() {
+        SenatePerson person = (SenatePerson) getSubject().getPrincipals().getPrimaryPrincipal();
+        return person.getEmployeeId();
+    }
 }
--- ess-supply/src/main/java/gov/nysenate/ess/supply/requisition/view/RequisitionView.java
+++ ess-supply/src/main/java/gov/nysenate/ess/supply/requisition/view/RequisitionView.java
@@ -1,104 +1,213 @@
 package gov.nysenate.ess.supply.requisition.view;
-//
-//import gov.nysenate.ess.core.client.view.base.ViewObject;
-//import gov.nysenate.ess.supply.requisition.Requisition;
-//
-//import java.time.LocalDateTime;
-//
-///**
-// * Simple view to represent a {@link Requisition} object without its history.
-// * Use {@link DetailedRequisitionView} if you need the requisitions history.
-// */
-//public class RequisitionView implements ViewObject {
-//
-//    protected int id;
-//    protected RequisitionVersionView activeVersion;
-//    protected LocalDateTime orderedDateTime;
-//    protected LocalDateTime processedDateTime;
-//    protected LocalDateTime completedDateTime;
-//    protected LocalDateTime approvedDateTime;
-//    protected LocalDateTime rejectedDateTime;
-//    protected LocalDateTime modifiedDateTime;
-//
-//    public RequisitionView() {}
-//
-//    public RequisitionView(Requisition requisition) {
-//        this.id = requisition.getId();
-//        this.activeVersion = new RequisitionVersionView(requisition.getCurrentVersion());
-//        this.orderedDateTime = requisition.getOrderedDateTime();
-//        this.processedDateTime = requisition.getProcessedDateTime().orElse(null);
-//        this.completedDateTime = requisition.getCompletedDateTime().orElse(null);
-//        this.approvedDateTime = requisition.getApprovedDateTime().orElse(null);
-//        this.rejectedDateTime = requisition.getRejectedDateTime().orElse(null);
-//        this.modifiedDateTime = requisition.getModifiedDateTime();
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public RequisitionVersionView getActiveVersion() {
-//        return activeVersion;
-//    }
-//
-//    public void setActiveVersion(RequisitionVersionView activeVersion) {
-//        this.activeVersion = activeVersion;
-//    }
-//
-//    public LocalDateTime getOrderedDateTime() {
-//        return orderedDateTime;
-//    }
-//
-//    public void setOrderedDateTime(LocalDateTime orderedDateTime) {
-//        this.orderedDateTime = orderedDateTime;
-//    }
-//
-//    public LocalDateTime getProcessedDateTime() {
-//        return processedDateTime;
-//    }
-//
-//    public void setProcessedDateTime(LocalDateTime processedDateTime) {
-//        this.processedDateTime = processedDateTime;
-//    }
-//
-//    public LocalDateTime getCompletedDateTime() {
-//        return completedDateTime;
-//    }
-//
-//    public void setCompletedDateTime(LocalDateTime completedDateTime) {
-//        this.completedDateTime = completedDateTime;
-//    }
-//
-//    public LocalDateTime getApprovedDateTime() {
-//        return approvedDateTime;
-//    }
-//
-//    public void setApprovedDateTime(LocalDateTime approvedDateTime) {
-//        this.approvedDateTime = approvedDateTime;
-//    }
-//
-//    public LocalDateTime getRejectedDateTime() {
-//        return rejectedDateTime;
-//    }
-//
-//    public void setRejectedDateTime(LocalDateTime rejectedDateTime) {
-//        this.rejectedDateTime = rejectedDateTime;
-//    }
-//
-//    public LocalDateTime getModifiedDateTime() {
-//        return modifiedDateTime;
-//    }
-//
-//    public void setModifiedDateTime(LocalDateTime modifiedDateTime) {
-//        this.modifiedDateTime = modifiedDateTime;
-//    }
-//
-//    @Override
-//    public String getViewType() {
-//        return "requisition";
-//    }
-//}
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import gov.nysenate.ess.core.client.view.EmployeeView;
+import gov.nysenate.ess.core.client.view.LocationView;
+import gov.nysenate.ess.core.client.view.base.ViewObject;
+import gov.nysenate.ess.supply.item.view.LineItemView;
+import gov.nysenate.ess.supply.requisition.Requisition;
+import gov.nysenate.ess.supply.requisition.RequisitionStatus;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@XmlRootElement
+public class RequisitionView implements ViewObject {
+
+    protected int requisitionId;
+    protected int revisionId;
+    protected EmployeeView customer;
+    protected LocationView destination;
+    protected Set<LineItemView> lineItems;
+    protected String status;
+    protected EmployeeView issuer;
+    protected String note;
+    protected EmployeeView modifiedBy;
+    protected LocalDateTime modifiedDateTime;
+    protected LocalDateTime orderedDateTime;
+    protected LocalDateTime processedDateTime;
+    protected LocalDateTime completedDateTime;
+    protected LocalDateTime approvedDateTime;
+    protected LocalDateTime rejectedDateTime;
+    protected boolean savedInSfms;
+
+    public RequisitionView() {}
+
+    public RequisitionView(Requisition requisition) {
+        this.requisitionId = requisition.getRequisitionId();
+        this.revisionId = requisition.getRevisionId();
+        this.customer = new EmployeeView(requisition.getCustomer());
+        this.destination = new LocationView(requisition.getDestination());
+        this.lineItems = requisition.getLineItems().stream()
+                                    .map(LineItemView::new)
+                                    .collect(Collectors.toSet());
+        this.status = requisition.getStatus().toString();
+        this.issuer = requisition.getIssuer().map(EmployeeView::new).orElse(null);
+        this.note = requisition.getNote().orElse(null);
+        this.modifiedBy = new EmployeeView(requisition.getModifiedBy());
+        this.modifiedDateTime = requisition.getModifiedDateTime().orElse(null);
+        this.orderedDateTime = requisition.getOrderedDateTime();
+        this.processedDateTime = requisition.getProcessedDateTime().orElse(null);
+        this.completedDateTime = requisition.getCompletedDateTime().orElse(null);
+        this.approvedDateTime = requisition.getApprovedDateTime().orElse(null);
+        this.rejectedDateTime = requisition.getRejectedDateTime().orElse(null);
+        this.savedInSfms = requisition.getSavedInSfms();
+    }
+
+    @JsonIgnore
+    public Requisition toRequisition() {
+        return new Requisition.Builder()
+                .withRequisitionId(requisitionId)
+                .withRevisionId(revisionId)
+                .withCustomer(customer.toEmployee())
+                .withDestination(destination.toLocation())
+                .withLineItems(lineItems.stream().map(LineItemView::toLineItem).collect(Collectors.toSet()))
+                .withStatus(RequisitionStatus.valueOf(status))
+                .withIssuer(issuer.toEmployee())
+                .withNote(note)
+                .withModifiedBy(modifiedBy.toEmployee())
+                .withModifiedDateTime(modifiedDateTime)
+                .withOrderedDateTime(orderedDateTime)
+                .withProcessedDateTime(processedDateTime)
+                .withCompletedDateTime(completedDateTime)
+                .withApprovedDateTime(approvedDateTime)
+                .withRejectedDateTime(rejectedDateTime)
+                .withSavedInSfms(savedInSfms)
+                .build();
+    }
+
+    public int getRequisitionId() {
+        return requisitionId;
+    }
+
+    public void setRequisitionId(int requisitionId) {
+        this.requisitionId = requisitionId;
+    }
+
+    public int getRevisionId() {
+        return revisionId;
+    }
+
+    public void setRevisionId(int revisionId) {
+        this.revisionId = revisionId;
+    }
+
+    public EmployeeView getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(EmployeeView customer) {
+        this.customer = customer;
+    }
+
+    public LocationView getDestination() {
+        return destination;
+    }
+
+    public void setDestination(LocationView destination) {
+        this.destination = destination;
+    }
+
+    public Set<LineItemView> getLineItems() {
+        return lineItems;
+    }
+
+    public void setLineItems(Set<LineItemView> lineItems) {
+        this.lineItems = lineItems;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public EmployeeView getIssuer() {
+        return issuer;
+    }
+
+    public void setIssuer(EmployeeView issuer) {
+        this.issuer = issuer;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public EmployeeView getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(EmployeeView modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public LocalDateTime getModifiedDateTime() {
+        return modifiedDateTime;
+    }
+
+    public void setModifiedDateTime(LocalDateTime modifiedDateTime) {
+        this.modifiedDateTime = modifiedDateTime;
+    }
+
+    public LocalDateTime getOrderedDateTime() {
+        return orderedDateTime;
+    }
+
+    public void setOrderedDateTime(LocalDateTime orderedDateTime) {
+        this.orderedDateTime = orderedDateTime;
+    }
+
+    public LocalDateTime getProcessedDateTime() {
+        return processedDateTime;
+    }
+
+    public void setProcessedDateTime(LocalDateTime processedDateTime) {
+        this.processedDateTime = processedDateTime;
+    }
+
+    public LocalDateTime getCompletedDateTime() {
+        return completedDateTime;
+    }
+
+    public void setCompletedDateTime(LocalDateTime completedDateTime) {
+        this.completedDateTime = completedDateTime;
+    }
+
+    public LocalDateTime getApprovedDateTime() {
+        return approvedDateTime;
+    }
+
+    public void setApprovedDateTime(LocalDateTime approvedDateTime) {
+        this.approvedDateTime = approvedDateTime;
+    }
+
+    public LocalDateTime getRejectedDateTime() {
+        return rejectedDateTime;
+    }
+
+    public void setRejectedDateTime(LocalDateTime rejectedDateTime) {
+        this.rejectedDateTime = rejectedDateTime;
+    }
+
+    public boolean isSavedInSfms() {
+        return savedInSfms;
+    }
+
+    public void setSavedInSfms(boolean savedInSfms) {
+        this.savedInSfms = savedInSfms;
+    }
+
+    @Override
+    public String getViewType() {
+        return "requisition";
+    }
+}
