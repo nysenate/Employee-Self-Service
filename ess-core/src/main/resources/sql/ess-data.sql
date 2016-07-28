d7f07e35131d12fce5544973a301c4a381dc47df
Kevin Caseiras <kcaseiras@gmail.com>
2016-07-28 11:44:24 -0400

    Update sql scripts for timeout exempt permissions.
    refs #10324

--- ess-core/src/main/resources/sql/ess-data.sql
+++ ess-core/src/main/resources/sql/ess-data.sql
@@ -21,13 +21,16 @@ INSERT INTO user_roles VALUES (3, 11591, 'SUPPLY_EMPLOYEE');
 INSERT INTO user_roles VALUES (4, 11591, 'SUPPLY_MANAGER');
 INSERT INTO user_roles VALUES (5, 9322, 'SUPPLY_EMPLOYEE');
 INSERT INTO user_roles VALUES (6, 9956, 'SUPPLY_EMPLOYEE');
+INSERT INTO user_roles VALUES (7, 9881, 'TIMEOUT_EXEMPT');
+INSERT INTO user_roles VALUES (8, 11591, 'TIMEOUT_EXEMPT');
+INSERT INTO user_roles VALUES (9, 9322, 'TIMEOUT_EXEMPT');
 
 
 --
 -- Name: user_roles_id_seq; Type: SEQUENCE SET; Schema: ess; Owner: postgres
 --
 
-SELECT pg_catalog.setval('user_roles_id_seq', 6, true);
+SELECT pg_catalog.setval('user_roles_id_seq', 9, true);
 
 
 --
