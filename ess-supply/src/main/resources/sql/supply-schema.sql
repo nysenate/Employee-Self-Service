91f09e0925c5f9f1e57a363b701d987d084851db
Kevin Caseiras <kcaseiras@gmail.com>
2016-07-29 10:28:52 -0400

    Update base supply sql scripts

--- ess-supply/src/main/resources/sql/supply-schema.sql
+++ ess-supply/src/main/resources/sql/supply-schema.sql
@@ -318,6 +318,72 @@ ALTER TABLE ONLY line_item
   ADD CONSTRAINT line_item_requisition_content_revision_id_fkey FOREIGN KEY (revision_id) REFERENCES requisition_content(revision_id);
 
 
+CREATE TABLE reconciliation_category_groups (
+  id integer NOT NULL,
+  item_category text NOT NULL,
+  page smallint NOT NULL
+);
+
+
+ALTER TABLE reconciliation_category_groups OWNER TO postgres;
+
+--
+-- Name: TABLE reconciliation_category_groups; Type: COMMENT; Schema: supply; Owner: postgres
+--
+
+COMMENT ON TABLE reconciliation_category_groups IS 'Groups supply item categories to the reconciliation page they should be displayed on.';
+
+
+--
+-- Name: COLUMN reconciliation_category_groups.item_category; Type: COMMENT; Schema: supply; Owner: postgres
+--
+
+COMMENT ON COLUMN reconciliation_category_groups.item_category IS 'The supply item category';
+
+
+--
+-- Name: COLUMN reconciliation_category_groups.page; Type: COMMENT; Schema: supply; Owner: postgres
+--
+
+COMMENT ON COLUMN reconciliation_category_groups.page IS 'The reconciliation page this item should be on.';
+
+
+--
+-- Name: reconciliation_category_groups_id_seq; Type: SEQUENCE; Schema: supply; Owner: postgres
+--
+
+CREATE SEQUENCE reconciliation_category_groups_id_seq
+START WITH 1
+INCREMENT BY 1
+NO MINVALUE
+NO MAXVALUE
+CACHE 1;
+
+
+ALTER TABLE reconciliation_category_groups_id_seq OWNER TO postgres;
+
+--
+-- Name: reconciliation_category_groups_id_seq; Type: SEQUENCE OWNED BY; Schema: supply; Owner: postgres
+--
+
+ALTER SEQUENCE reconciliation_category_groups_id_seq OWNED BY reconciliation_category_groups.id;
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: supply; Owner: postgres
+--
+
+ALTER TABLE ONLY reconciliation_category_groups ALTER COLUMN id SET DEFAULT nextval('reconciliation_category_groups_id_seq'::regclass);
+
+
+--
+-- Name: reconciliation_category_groups_pkey; Type: CONSTRAINT; Schema: supply; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY reconciliation_category_groups
+  ADD CONSTRAINT reconciliation_category_groups_pkey PRIMARY KEY (id);
+
+
 --
 -- Permissions
 --
