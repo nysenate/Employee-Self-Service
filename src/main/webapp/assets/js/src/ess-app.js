a85b9f14b0a5e769a6801f0b6bdf9d67310acc59
Chenguang <gaoyike@gmail.com>
2016-09-14 10:40:14 -0400

    fixes #10508
    disable backspace key for navigation event

--- src/main/webapp/assets/js/src/ess-app.js
+++ src/main/webapp/assets/js/src/ess-app.js
@@ -27,6 +27,32 @@ essCore.run(['$cookies', function ($cookies) {
         });
     }
     $cookies.put("appVersion", globalProps.releaseVersion);
+    // disable backspace key from nav
+    $(document).unbind('keydown').bind('keydown', function (event) {
+        var doPrevent = false;
+        if (event.keyCode === 8) {
+            var d = event.srcElement || event.target;
+            if ((d.tagName.toUpperCase() === 'INPUT' &&
+                    (
+                    d.type.toUpperCase() === 'TEXT' ||
+                    d.type.toUpperCase() === 'PASSWORD' ||
+                    d.type.toUpperCase() === 'FILE' ||
+                    d.type.toUpperCase() === 'SEARCH' ||
+                    d.type.toUpperCase() === 'EMAIL' ||
+                    d.type.toUpperCase() === 'NUMBER' ||
+                    d.type.toUpperCase() === 'DATE' )
+                ) ||
+                d.tagName.toUpperCase() === 'TEXTAREA') {
+                doPrevent = d.readOnly || d.disabled;
+            }
+            else {
+                doPrevent = true;
+            }
+        }
+        if (doPrevent) {
+            event.preventDefault();
+        }
+    });
 }]);
 
 /*Time Out*/
