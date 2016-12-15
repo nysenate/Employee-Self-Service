36487a28152086e095ac63c832329b39e050488a
Kevin Caseiras <kcaseiras@gmail.com>
2016-12-15 14:42:57 -0500

    Remove location monthly allowance functionality

--- src/main/java/gov/nysenate/ess/supply/allowance/service/LocationAllowanceService.java
+++
@@ -1,9 +0,0 @@
-package gov.nysenate.ess.supply.allowance.service;
-
-import gov.nysenate.ess.core.model.unit.LocationId;
-import gov.nysenate.ess.supply.allowance.LocationAllowance;
-
-public interface LocationAllowanceService {
-
-    LocationAllowance getLocationAllowance(LocationId locationId);
-}
