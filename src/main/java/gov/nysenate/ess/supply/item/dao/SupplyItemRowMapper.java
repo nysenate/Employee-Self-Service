93e39fbc0de6c943342f2b5bc4857c9e1fda03bb
Kevin Caseiras <kcaseiras@gmail.com>
2016-12-22 11:29:58 -0500

    Make items visible and orderd by supply by default

--- src/main/java/gov/nysenate/ess/supply/item/dao/SupplyItemRowMapper.java
+++ src/main/java/gov/nysenate/ess/supply/item/dao/SupplyItemRowMapper.java
@@ -35,8 +35,8 @@ public class SupplyItemRowMapper extends BaseRowMapper<SupplyItem> {
         return new ItemStatus(
                 // If null, initialize with default value.
                 cdStockItem == null ? false : cdStockItem.equals("Y"),
-                cdSenSuppiedItem == null ? false : !cdSenSuppiedItem.equals("Y"),
-                cdSpecPerMVisible == null ? false : cdSpecPerMVisible.equals("Y"),
+                cdSenSuppiedItem == null ? true : !cdSenSuppiedItem.equals("Y"),
+                cdSpecPerMVisible == null ? true : cdSpecPerMVisible.equals("Y"),
                 cdSpecPerMReq == null ? false : cdSpecPerMReq.equals("Y"));
     }
 }
