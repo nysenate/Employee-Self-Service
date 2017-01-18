7e1130cbecf4ea72c660131bca89ad19135fd16f
Kevin Caseiras <kcaseiras@gmail.com>
2017-01-18 15:57:06 -0500

    Allow use of number pad when changing item quantities

--- src/main/webapp/assets/js/src/supply/order/order-quantity-validator.js
+++ src/main/webapp/assets/js/src/supply/order/order-quantity-validator.js
@@ -8,12 +8,15 @@ essSupply.directive('orderQuantityValidator', [function () {
     return {
         require: 'ngModel',
         link: function (scope, elm, attrs, ngModel) {
-            // Only allow numbers, backspace, tab, and F5 keys to be pressed.
             elm.bind("keydown", function (event) {
+                // Allow backspace, tab and F5 keys to be pressed
                 if (event.keyCode === 8 || event.keyCode === 9 || event.keyCode === 116) {
                     return;
                 }
-                if (event.keyCode < 48 || event.keyCode > 57) {
+                // Allow 0-9 numbers from keyboard or numpad.
+                if ((event.keyCode >= 48 && event.keyCode <=57) || (event.keyCode >= 96 && event.keyCode <= 105)) {
+                    return;
+                } else {
                     event.preventDefault();
                 }
             });
