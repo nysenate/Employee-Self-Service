b910716971582303047ecccb37ad281ce67c0f3a
Sam Stouffer <samlthecaml@gmail.com>
2017-02-28 17:23:52 -0500

    Add better styling for error pages

--- src/main/java/gov/nysenate/ess/web/controller/page/ErrorPageCtrl.java
+++ src/main/java/gov/nysenate/ess/web/controller/page/ErrorPageCtrl.java
@@ -11,22 +11,40 @@ import java.time.LocalDateTime;
 @RequestMapping("/error")
 public class ErrorPageCtrl
 {
+    private static final String errorTemplateName = "error";
+
     @RequestMapping(value = "/404", method = RequestMethod.GET)
     public String pageNotFound(ModelMap modelMap) {
-        modelMap.put("message", "Sorry, the page you requested could not be found.");
-        return "error";
+        modelMap.addAttribute("title", "Page Not Found")
+                .addAttribute("message", "Sorry, the page you requested could not be found.");
+        return errorPage(modelMap);
     }
 
     @RequestMapping(value = "/restricted")
-    public String accessRestrictedPage() {
-        return "restricted";
+    public String accessRestrictedPage(ModelMap modelMap) {
+        modelMap.addAttribute("title", "Access Restricted")
+                .addAttribute("message", "ESS is currently only available to a subset of Senate employees for testing.");
+        return warningPage(modelMap);
     }
 
     @RequestMapping(value = "/authz-error", method = RequestMethod.GET)
     public String authzError(ModelMap modelMap) {
-        modelMap.put("message",
-                "An error occurred during ESS user authorization at " + LocalDateTime.now() + "\n" +
-                "Please report this to the STS Help Line at Senate x2011");
-        return "error";
+        modelMap.addAttribute("title", "Authorization Error")
+                .addAttribute("message",
+                        "An error occurred during ESS user authorization at " + LocalDateTime.now() + "<br>" +
+                         "Please report this to the STS Help Line at Senate x2011");
+        return errorPage(modelMap);
+    }
+
+    /* --- Internal Methods ---*/
+
+    private String warningPage(ModelMap modelMap) {
+        modelMap.addAttribute("level", "warn");
+        return errorTemplateName;
+    }
+
+    private String errorPage(ModelMap modelMap) {
+        modelMap.addAttribute("level", "error");
+        return errorTemplateName;
     }
 }
--- src/main/webapp/WEB-INF/view/error.jsp
+++ src/main/webapp/WEB-INF/view/error.jsp
@@ -1,9 +1,19 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title></title>
+  <title>ESS Error: ${title}</title>
+  <link rel="stylesheet" type="text/css" href="${ctxPath}/assets/css/dest/app.min.css?v=${releaseVersion}"/>
 </head>
 <body>
-    <h1 style='color:red'>${message}</h1>
+<div id="error-page">
+  <div class="ess-notification ${level}">
+    <h2>${title}</h2>
+    <p class="margin-10">
+      ${message}
+      <br><br>
+      <a href="${ctxPath}/logout">Return to Login</a>
+    </p>
+  </div>
+</div>
 </body>
 </html>
