df69ad547dc5773919b317ede3ad91d15ffe8aba
Sam Stouffer <samlthecaml@gmail.com>
2017-05-09 13:00:40 -0400

    Fix last record retrieval in time rec. validation
    The reference record for time record validation is currently pulled from
    the cache.  This is non-ideal because the cache may not always be up to
    date.  This commit causes the validation reference record to always be
    pulled straight from the database.
    
    Fixes #10728

--- src/main/java/gov/nysenate/ess/time/service/attendance/validation/EssTimeRecordValidationService.java
+++ src/main/java/gov/nysenate/ess/time/service/attendance/validation/EssTimeRecordValidationService.java
@@ -2,19 +2,20 @@ package gov.nysenate.ess.time.service.attendance.validation;
 
 import com.google.common.collect.ImmutableList;
 import gov.nysenate.ess.core.client.view.base.ViewObject;
-import gov.nysenate.ess.time.dao.attendance.TimeRecordDao;
 import gov.nysenate.ess.time.model.attendance.TimeRecord;
 import gov.nysenate.ess.time.model.attendance.TimeRecordAction;
+import gov.nysenate.ess.time.service.attendance.TimeRecordNotFoundException;
 import gov.nysenate.ess.time.service.attendance.TimeRecordService;
 import gov.nysenate.ess.time.service.attendance.validation.recordvalidators.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.util.*;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class EssTimeRecordValidationService implements TimeRecordValidationService {
@@ -22,7 +23,6 @@ public class EssTimeRecordValidationService implements TimeRecordValidationServi
     private static final Logger logger = LoggerFactory.getLogger(EssTimeRecordValidationService.class);
 
     @Autowired private TimeRecordService timeRecordService;
-    @Autowired private TimeRecordDao timeRecordDao;
 
     @Autowired private RecordPermittedModificationTRV recordPermittedModificationTRV;
     @Autowired private AllowanceTRV allowanceTRV;
@@ -88,16 +88,21 @@ public class EssTimeRecordValidationService implements TimeRecordValidationServi
         if (record.getTimeRecordId() == null) {
             return Optional.empty();
         }
-        // Try getting the record from the cached active records first
-        Optional<TimeRecord> prevState = timeRecordService.getActiveTimeRecords(record.getEmployeeId()).stream()
-                .filter(oRec -> record.getTimeRecordId().equals(oRec.getTimeRecordId()))
-                .findAny();
-        // If that fails, try to get it from the dao
-        if (!prevState.isPresent()) {
-            try {
-                prevState = Optional.of(timeRecordDao.getTimeRecord(record.getTimeRecordId()));
-            } catch (EmptyResultDataAccessException ignored) {}
+        try {
+            return Optional.of(timeRecordService.getTimeRecord(record.getTimeRecordId()));
+        } catch (TimeRecordNotFoundException ex) {
+            return Optional.empty();
         }
-        return prevState;
+//        // Try getting the record from the cached active records first
+//        Optional<TimeRecord> prevState = timeRecordService.getActiveTimeRecords(record.getEmployeeId()).stream()
+//                .filter(oRec -> record.getTimeRecordId().equals(oRec.getTimeRecordId()))
+//                .findAny();
+//        // If that fails, try to get it from the dao
+//        if (!prevState.isPresent()) {
+//            try {
+//                prevState = Optional.of(timeRecordService.getTimeRecord(record.getTimeRecordId()));
+//            } catch (EmptyResultDataAccessException ignored) {}
+//        }
+//        return prevState;
     }
 }
