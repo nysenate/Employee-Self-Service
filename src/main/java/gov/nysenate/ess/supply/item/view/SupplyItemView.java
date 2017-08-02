nysenate/Employee-Self-Service

commit 1b9fcb57d2466736c99546ae349ca982f4cb2266

Update synchronization procedure and add tests
Updates the synchronization procedure to produce an error if line items
are missing or cant be found in the requisition xml. Also adds a test to
verify this during the build.

refs #11242

src/main/resources/sql/supply/sfms/oracle-synchronization.sql
@@ -233,6 +233,11 @@ CREATE OR REPLACE PACKAGE BODY SYNCHRONIZE_SUPPLY AS
         RAISE_APPLICATION_ERROR(-20001, 'Requisition ' || requisition_id || ' has already been inserted into SFMS.');
       END IF;
 
+      IF requisition_xml.existsNode('//lineItems') = 0
+        THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Requisition ' || requisition_id || ' has no line items or has malformed xml');
+      END IF;
+
       -- Loop through all items in the requisition.
       WHILE requisition_xml.existsNode('//lineItems[' || item_count || ']') = 1
       LOOP

src/test/java/gov/nysenate/ess/supply/integration/synchronization/SynchronizationProcedureIT.java
@@ -1,0 +1,112 @@
+package gov.nysenate.ess.supply.integration.synchronization;
+
+import gov.nysenate.ess.core.BaseTest;
+import gov.nysenate.ess.core.annotation.IntegrationTest;
+import gov.nysenate.ess.core.config.DatabaseConfig;
+import gov.nysenate.ess.supply.requisition.service.RequisitionService;
+import gov.nysenate.ess.supply.synchronization.dao.SfmsSynchronizationProcedure;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+@Category(IntegrationTest.class)
+@Transactional(value = DatabaseConfig.remoteTxManager)
+public class SynchronizationProcedureIT extends BaseTest {
+
+    @Autowired
+    private SfmsSynchronizationProcedure synchronizationProcedure;
+
+    @Test(expected = org.springframework.dao.DataAccessException.class)
+    public void throwErrorIfLineItemsAreNotFound() {
+        synchronizationProcedure.synchronizeRequisition(xml);
+    }
+
+    /**
+     * Example XML representing a requisition. Missing Line Item elements.
+     */
+    private final String xml = "<?xml version=\"1.0\"?>\n" +
+            "<RequisitionView>\n" +
+            "  <requisitionId>2147483647</requisitionId>\n" +
+            "  <revisionId>5</revisionId>\n" +
+            "  <customer>\n" +
+            "    <employeeId>11168</employeeId>\n" +
+            "    <uid>caseiras</uid>\n" +
+            "    <firstName>Kevin</firstName>\n" +
+            "    <lastName>Caseiras</lastName>\n" +
+            "    <fullName>Kevin F. Caseiras</fullName>\n" +
+            "    <email>c@n.gov</email>\n" +
+            "    <active>true</active>\n" +
+            "    <title>Mr.</title>\n" +
+            "    <initial>F.</initial>\n" +
+            "    <suffix/>\n" +
+            "    <workPhone>123-4567</workPhone>\n" +
+            "    <homePhone>123-4567</homePhone>\n" +
+            "    <gender>M</gender>\n" +
+            "  </customer>\n" +
+            "  <destination>\n" +
+            "    <locId>A416F-W</locId>\n" +
+            "    <code>A416F</code>\n" +
+            "    <locationType>Work Location</locationType>\n" +
+            "    <locationTypeCode>W</locationTypeCode>\n" +
+            "    <address>\n" +
+            "      <addr1>16th Floor, Agency Bldg 4</addr1>\n" +
+            "      <addr2/>\n" +
+            "      <city>Albany</city>\n" +
+            "      <state>NY</state>\n" +
+            "      <zip5>12247</zip5>\n" +
+            "      <zip4/>\n" +
+            "    </address>\n" +
+            "    <respCenterHead>\n" +
+            "      <active>true</active>\n" +
+            "      <code>MAJSENIORS</code>\n" +
+            "      <shortName>MAJ LD SR STAFF</shortName>\n" +
+            "      <name>Majority Leader's Senior Staff</name>\n" +
+            "      <affiliateCode>MAJ</affiliateCode>\n" +
+            "    </respCenterHead>\n" +
+            "    <locationDescription>16TH FLOOR, AGE</locationDescription>\n" +
+            "  </destination>\n" +
+            "  <deliveryMethod>DELIVERY</deliveryMethod>\n" +
+            "  <specialInstructions/>\n" +
+            "  <status>APPROVED</status>\n" +
+            "  <issuer>\n" +
+            "    <employeeId>11591</employeeId>\n" +
+            "    <uid>tla</uid>\n" +
+            "    <firstName>T</firstName>\n" +
+            "    <lastName>La</lastName>\n" +
+            "    <fullName>T La</fullName>\n" +
+            "    <email>tla@n.gov</email>\n" +
+            "    <active>true</active>\n" +
+            "    <title>Mr.</title>\n" +
+            "    <initial></initial>\n" +
+            "    <suffix/>\n" +
+            "    <workPhone>123-4567</workPhone>\n" +
+            "    <homePhone>123-4567</homePhone>\n" +
+            "    <gender>M</gender>\n" +
+            "  </issuer>\n" +
+            "  <note/>\n" +
+            "  <modifiedBy>\n" +
+             "    <employeeId>11591</employeeId>\n" +
+            "    <uid>tla</uid>\n" +
+            "    <firstName>T</firstName>\n" +
+            "    <lastName>La</lastName>\n" +
+            "    <fullName>T La</fullName>\n" +
+            "    <email>tla@n.gov</email>\n" +
+            "    <active>true</active>\n" +
+            "    <title>Mr.</title>\n" +
+            "    <initial></initial>\n" +
+            "    <suffix/>\n" +
+            "    <workPhone>123-4567</workPhone>\n" +
+            "    <homePhone>123-4567</homePhone>\n" +
+            "    <gender>M</gender>\n" +
+            "  </modifiedBy>\n" +
+            "  <modifiedDateTime>2016-11-15T11:38:22.496</modifiedDateTime>\n" +
+            "  <orderedDateTime>2016-11-15T11:19:43.529</orderedDateTime>\n" +
+            "  <processedDateTime>2016-11-15T11:20:08.96</processedDateTime>\n" +
+            "  <completedDateTime>2016-11-15T11:38:18.498</completedDateTime>\n" +
+            "  <approvedDateTime>2016-11-15T11:38:22.45</approvedDateTime>\n" +
+            "  <rejectedDateTime/>\n" +
+            "  <lastSfmsSyncDateTime/>\n" +
+            "  <savedInSfms>false</savedInSfms>\n" +
+            "</RequisitionView>";
+}

commit 5378605824c4d55f78fbacb706308211b305ba64

Add a custom view for the supply SFMS sync process
Adds a new requisition view to be used specifically for serializing
requisitions into xml for the supply SFMS sync process. This will
prevent potential bugs in the synch process caused by changes to view
objects.

Also fixes a bug introduced in commit 1b9fcb5. That commit added the
requirement for a LineItems tag in the XML sent to SFMS. However, there
is an edge case where supply would correctly try to sync a requisition
with no line items, causing an inaccurate exception. This commit
addresses this by not sending these requisitions to SFMS.

src/main/java/gov/nysenate/ess/supply/item/view/SupplyItemView.java
@@ -8,7 +8,7 @@ public class SupplyItemView implements Comparable<SupplyItemView>, ViewObject {
     protected int id;
     protected String commodityCode;
     protected String description;
-    protected String unit; // * Oracle-Synchronization depends on this field name.
+    protected String unit;
     protected String category;
     protected int perOrderAllowance;
     protected int perMonthAllowance;

src/main/java/gov/nysenate/ess/supply/requisition/view/RequisitionView.java
@@ -18,8 +18,6 @@ import java.util.stream.Collectors;
 
 /**
  * A view representing a Requisition.
- *
- * Note: The Sfms requisition synchronization depends on the structure and names of some of these fields.
  */
 @XmlRootElement
 public class RequisitionView implements ViewObject {

src/main/java/gov/nysenate/ess/supply/requisition/view/SfmsRequisitionView.java
@@ -1,0 +1,95 @@
+package gov.nysenate.ess.supply.requisition.view;
+
+import gov.nysenate.ess.supply.item.LineItem;
+import gov.nysenate.ess.supply.requisition.model.Requisition;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * A view of a requisition formatted specifically for the SFMS synchronization process.
+ * Any updates to this view will need to be updated in the oracle-synchronization.sql script.
+ */
+@XmlRootElement(name = "Requisition")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class SfmsRequisitionView {
+
+    protected int requisitionId;
+    protected int customerId;
+    protected String destinationCode;
+    protected char destinationTypeCode;
+    protected Set<SfmsLineItemView> lineItems;
+    protected String issuerUid;
+    protected LocalDateTime approvedDateTime;
+
+    public SfmsRequisitionView(Requisition requisition) {
+        this.requisitionId = requisition.getRequisitionId();
+        this.customerId = requisition.getCustomer().getEmployeeId();
+        this.destinationCode = requisition.getDestination().getLocId().getCode();
+        this.destinationTypeCode = requisition.getDestination().getLocId().getType().getCode();
+        this.lineItems = requisition.getLineItems().stream()
+                .map(SfmsLineItemView::new)
+                .collect(Collectors.toSet());
+        this.issuerUid = requisition.getIssuer().get().getUid();
+        this.approvedDateTime = requisition.getApprovedDateTime().get();
+    }
+
+    public int getRequisitionId() {
+        return requisitionId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getDestinationCode() {
+        return destinationCode;
+    }
+
+    public char getDestinationTypeCode() {
+        return destinationTypeCode;
+    }
+
+    public Set<SfmsLineItemView> getLineItems() {
+        return lineItems;
+    }
+
+    public String getIssuerUid() {
+        return issuerUid;
+    }
+
+    public LocalDateTime getApprovedDateTime() {
+        return approvedDateTime;
+    }
+
+    /**
+     * A view of LineItem formatted specifically for the synchronization process.
+     */
+    public static class SfmsLineItemView {
+       protected int itemId;
+       protected int quantity;
+       protected String issueUnit;
+
+       public SfmsLineItemView(LineItem li) {
+           this.itemId = li.getItem().getId();
+           this.quantity = li.getQuantity();
+           this.issueUnit = li.getItem().getUnitDescription();
+       }
+
+        public int getItemId() {
+            return itemId;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public String getIssueUnit() {
+            return issueUnit;
+        }
+    }
+}

src/main/java/gov/nysenate/ess/supply/synchronization/service/SfmsSynchronizationService.java
@@ -1,6 +1,5 @@
 package gov.nysenate.ess.supply.synchronization.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Range;
 import gov.nysenate.ess.core.service.notification.slack.service.SlackChatService;
 import gov.nysenate.ess.core.util.LimitOffset;
@@ -9,7 +8,7 @@ import gov.nysenate.ess.supply.item.LineItem;
 import gov.nysenate.ess.supply.requisition.model.Requisition;
 import gov.nysenate.ess.supply.requisition.model.RequisitionStatus;
 import gov.nysenate.ess.supply.requisition.service.RequisitionService;
-import gov.nysenate.ess.supply.requisition.view.RequisitionView;
+import gov.nysenate.ess.supply.requisition.view.SfmsRequisitionView;
 import gov.nysenate.ess.supply.synchronization.dao.SfmsSynchronizationProcedure;
 import gov.nysenate.ess.supply.util.date.DateTimeFactory;
 import org.slf4j.Logger;
@@ -23,10 +22,7 @@ import org.springframework.stereotype.Service;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -49,8 +45,10 @@ public class SfmsSynchronizationService {
     private SlackChatService slackChatService;
 
     /**
-     * Inserts supply requisitions into SFMS for all approved requisitions where savedInSfms = <code>false</code>
+     * Inserts supply requisition line items into SFMS for all approved requisitions where savedInSfms = <code>false</code>.
      * On success, savedInSfms gets set to <code>true</code>.
+     * Line items of 0 quantity and items not tracked in SFMS are filtered out so they do not get synced.
+     * If after filtering, a requisiton has no other line items, it will be marked as synced in supply but will not be synced with SFMS.
      * <p>
      *     Checks all requisitions, so any errors in previous runs will be
      *     automatically attempted again in the next run.
@@ -67,28 +65,46 @@ public class SfmsSynchronizationService {
         if (!synchronizationEnabled) {
             return;
         }
-        List<Requisition> requisitions = requisitionsNeedingSync();
-        logger.info("Attempting to synchronize {} requisitions with SFMS.", requisitions.size());
-        for (Requisition requisition : requisitions) {
-            syncRequisition(requisition);
+        List<Requisition> reqs = requisitionsToBeSynced();
+        List<Requisition> filteredReqs = filterRequisitions(reqs);
+        for (Requisition r : filteredReqs) {
+            syncRequisition(r);
         }
+
     }
 
     private void syncRequisition(Requisition requisition) {
-        Requisition sfmsRequisition = filterRequisitionForSfms(requisition);
-        try {
-            synchronizationProcedure.synchronizeRequisition(OutputUtils.toXml(new RequisitionView(sfmsRequisition)));
-            requisitionService.savedInSfms(sfmsRequisition.getRequisitionId(), true);
-        } catch (DataAccessException ex) {
-            String msg = "Error synchronizing requisition " + requisition.getRequisitionId()
-                    + " with SFMS. Exception is : " + ex.getMessage();
-            logger.error(msg);
-            sendMessageToSlack(msg);
+        if (requiresSync(requisition)) {
+            logger.info("Attempting to synchronize requisition {} with SFMS.", requisition.getRequisitionId());
+            try {
+                synchronizationProcedure.synchronizeRequisition(OutputUtils.toXml(new SfmsRequisitionView(requisition)));
+                setAsSynced(requisition);
+            } catch (DataAccessException ex) {
+                String msg = "Error synchronizing requisition " + requisition.getRequisitionId()
+                        + " with SFMS. Exception is : " + ex.getMessage();
+                logger.error(msg);
+                sendMessageToSlack(msg);
+            }
+        }
+        else {
+            logger.info("Requisition {} can skip SFMS sync.", requisition.getRequisitionId());
+            setAsSynced(requisition);
         }
     }
 
-    private List<Requisition> requisitionsNeedingSync() {
-        // Get all requisitions not saved in sfms since app release in 2016.
+    private boolean requiresSync(Requisition requisition) {
+        return requisition.getLineItems().size() > 0;
+    }
+
+    private void setAsSynced(Requisition requisition) {
+        requisitionService.savedInSfms(requisition.getRequisitionId(), true);
+    }
+
+    /**
+     * Gets all requisitions which have not yet been synced with SFMS.
+     * @return
+     */
+    private List<Requisition> requisitionsToBeSynced() {
         LocalDateTime start = LocalDateTime.of(2016, 1, 1, 0, 0);
         LocalDateTime end = dateTimeFactory.now();
         Range<LocalDateTime> dateRange = Range.closed(start, end);
@@ -99,24 +115,29 @@ public class SfmsSynchronizationService {
     }
 
     /**
+     * Removes line items of 0 quantity and items that are not tracked in inventory from a requisition.
+     * These items should not be synchronized with SFMS.
+     */
+    private List<Requisition> filterRequisitions(List<Requisition> requisitions) {
+        for (Requisition req : requisitions) {
+            req.setLineItems(filterLineItems(req.getLineItems()));
+        }
+        return requisitions;
+    }
+
+    private Set<LineItem> filterLineItems(Set<LineItem> lineItems) {
+        return lineItems.stream()
+                .filter(lineItem -> lineItem.getQuantity() > 0 && lineItem.getItem().requiresSynchronization())
+                .collect(Collectors.toSet());
+    }
+
+    /**
      * Send error message to slack channel
-     *
      * @param s msg
      */
     private void sendMessageToSlack(String s) {
         DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
         Date dateobj = new Date();
         slackChatService.sendMessage(df.format(dateobj) + " Sfms Synchronization Errors: " + s + "\n");
-    }
-
-    /**
-     * Removes line items of 0 quantity and items that are not tracked in inventory from a requisition.
-     * These items should not be synchronized with SFMS.
-     */
-    private Requisition filterRequisitionForSfms(Requisition requisition) {
-        Set<LineItem> filteredLineItems = requisition.getLineItems().stream()
-                .filter(lineItem -> lineItem.getQuantity() > 0 && lineItem.getItem().requiresSynchronization())
-                .collect(Collectors.toSet());
-        return requisition.setLineItems(filteredLineItems);
     }
 }

src/main/resources/sql/supply/sfms/oracle-synchronization.sql
@@ -219,12 +219,12 @@ CREATE OR REPLACE PACKAGE BODY SYNCHRONIZE_SUPPLY AS
       requisition_xml := XMLTYPE(requisition_xml_string);
 
       -- Extract Requisition data
-      requisition_id := requisition_xml.extract('/RequisitionView/requisitionId/text()').getNumberVal();
-      customer_id := requisition_xml.extract('/RequisitionView/customer/employeeId/text()').getNumberVal();
-      approved_date_time := requisition_xml.extract('/RequisitionView/approvedDateTime/text()').getStringVal();
-      destination_code := requisition_xml.extract('/RequisitionView/destination/code/text()').getStringVal();
-      destination_type := requisition_xml.extract('/RequisitionView/destination/locationTypeCode/text()').getStringVal();
-      issuer_uid := UPPER(requisition_xml.extract('/RequisitionView/issuer/uid/text()').getStringVal());
+      requisition_id := requisition_xml.extract('/Requisition/requisitionId/text()').getNumberVal();
+      customer_id := requisition_xml.extract('/Requisition/customerId/text()').getNumberVal();
+      approved_date_time := requisition_xml.extract('/Requisition/approvedDateTime/text()').getStringVal();
+      destination_code := requisition_xml.extract('/Requisition/destinationCode/text()').getStringVal();
+      destination_type := requisition_xml.extract('/Requisition/destinationTypeCode/text()').getStringVal();
+      issuer_uid := UPPER(requisition_xml.extract('/Requisition/issuerUid/text()').getStringVal());
       responsibility_head := get_responsibility_head(destination_code, destination_type);
 
       -- Ensure this requisition has not been inserted before.
@@ -242,9 +242,9 @@ CREATE OR REPLACE PACKAGE BODY SYNCHRONIZE_SUPPLY AS
       WHILE requisition_xml.existsNode('//lineItems[' || item_count || ']') = 1
       LOOP
         -- Extract item data.
-        item_id := requisition_xml.extract('//lineItems[' || item_count || ']/item/id/text()').getNumberVal();
+        item_id := requisition_xml.extract('//lineItems[' || item_count || ']/itemId/text()').getNumberVal();
         quantity := requisition_xml.extract('//lineItems[' || item_count || ']/quantity/text()').getNumberVal();
-        issue_unit := requisition_xml.extract('//lineItems[' || item_count || ']/item/unit/text()').getStringVal();
+        issue_unit := requisition_xml.extract('//lineItems[' || item_count || ']/issueUnit/text()').getStringVal();
         nuissue := get_next_nuissue(item_id, destination_code, destination_type, approved_date_time);
         standard_quantity := quantity * get_standard_unit_size(issue_unit);
 

src/test/java/gov/nysenate/ess/supply/integration/synchronization/SynchronizationProcedureIT.java
@@ -3,12 +3,12 @@ package gov.nysenate.ess.supply.integration.synchronization;
 import gov.nysenate.ess.core.BaseTest;
 import gov.nysenate.ess.core.annotation.IntegrationTest;
 import gov.nysenate.ess.core.config.DatabaseConfig;
-import gov.nysenate.ess.supply.requisition.service.RequisitionService;
 import gov.nysenate.ess.supply.synchronization.dao.SfmsSynchronizationProcedure;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Category(IntegrationTest.class)
 @Transactional(value = DatabaseConfig.remoteTxManager)
@@ -26,87 +26,12 @@ public class SynchronizationProcedureIT extends BaseTest {
      * Example XML representing a requisition. Missing Line Item elements.
      */
     private final String xml = "<?xml version=\"1.0\"?>\n" +
-            "<RequisitionView>\n" +
+            "<Requisition>\n" +
             "  <requisitionId>2147483647</requisitionId>\n" +
-            "  <revisionId>5</revisionId>\n" +
-            "  <customer>\n" +
-            "    <employeeId>11168</employeeId>\n" +
-            "    <uid>caseiras</uid>\n" +
-            "    <firstName>Kevin</firstName>\n" +
-            "    <lastName>Caseiras</lastName>\n" +
-            "    <fullName>Kevin F. Caseiras</fullName>\n" +
-            "    <email>c@n.gov</email>\n" +
-            "    <active>true</active>\n" +
-            "    <title>Mr.</title>\n" +
-            "    <initial>F.</initial>\n" +
-            "    <suffix/>\n" +
-            "    <workPhone>123-4567</workPhone>\n" +
-            "    <homePhone>123-4567</homePhone>\n" +
-            "    <gender>M</gender>\n" +
-            "  </customer>\n" +
-            "  <destination>\n" +
-            "    <locId>A416F-W</locId>\n" +
-            "    <code>A416F</code>\n" +
-            "    <locationType>Work Location</locationType>\n" +
-            "    <locationTypeCode>W</locationTypeCode>\n" +
-            "    <address>\n" +
-            "      <addr1>16th Floor, Agency Bldg 4</addr1>\n" +
-            "      <addr2/>\n" +
-            "      <city>Albany</city>\n" +
-            "      <state>NY</state>\n" +
-            "      <zip5>12247</zip5>\n" +
-            "      <zip4/>\n" +
-            "    </address>\n" +
-            "    <respCenterHead>\n" +
-            "      <active>true</active>\n" +
-            "      <code>MAJSENIORS</code>\n" +
-            "      <shortName>MAJ LD SR STAFF</shortName>\n" +
-            "      <name>Majority Leader's Senior Staff</name>\n" +
-            "      <affiliateCode>MAJ</affiliateCode>\n" +
-            "    </respCenterHead>\n" +
-            "    <locationDescription>16TH FLOOR, AGE</locationDescription>\n" +
-            "  </destination>\n" +
-            "  <deliveryMethod>DELIVERY</deliveryMethod>\n" +
-            "  <specialInstructions/>\n" +
-            "  <status>APPROVED</status>\n" +
-            "  <issuer>\n" +
-            "    <employeeId>11591</employeeId>\n" +
-            "    <uid>tla</uid>\n" +
-            "    <firstName>T</firstName>\n" +
-            "    <lastName>La</lastName>\n" +
-            "    <fullName>T La</fullName>\n" +
-            "    <email>tla@n.gov</email>\n" +
-            "    <active>true</active>\n" +
-            "    <title>Mr.</title>\n" +
-            "    <initial></initial>\n" +
-            "    <suffix/>\n" +
-            "    <workPhone>123-4567</workPhone>\n" +
-            "    <homePhone>123-4567</homePhone>\n" +
-            "    <gender>M</gender>\n" +
-            "  </issuer>\n" +
-            "  <note/>\n" +
-            "  <modifiedBy>\n" +
-             "    <employeeId>11591</employeeId>\n" +
-            "    <uid>tla</uid>\n" +
-            "    <firstName>T</firstName>\n" +
-            "    <lastName>La</lastName>\n" +
-            "    <fullName>T La</fullName>\n" +
-            "    <email>tla@n.gov</email>\n" +
-            "    <active>true</active>\n" +
-            "    <title>Mr.</title>\n" +
-            "    <initial></initial>\n" +
-            "    <suffix/>\n" +
-            "    <workPhone>123-4567</workPhone>\n" +
-            "    <homePhone>123-4567</homePhone>\n" +
-            "    <gender>M</gender>\n" +
-            "  </modifiedBy>\n" +
-            "  <modifiedDateTime>2016-11-15T11:38:22.496</modifiedDateTime>\n" +
-            "  <orderedDateTime>2016-11-15T11:19:43.529</orderedDateTime>\n" +
-            "  <processedDateTime>2016-11-15T11:20:08.96</processedDateTime>\n" +
-            "  <completedDateTime>2016-11-15T11:38:18.498</completedDateTime>\n" +
-            "  <approvedDateTime>2016-11-15T11:38:22.45</approvedDateTime>\n" +
-            "  <rejectedDateTime/>\n" +
-            "  <lastSfmsSyncDateTime/>\n" +
-            "  <savedInSfms>false</savedInSfms>\n" +
-            "</RequisitionView>";
+            "  <customerId>11168</customerId>\n" +
+            "  <destinationCode>D07001</destinationCode>\n" +
+            "  <destinationTypeCode>W</destinationTypeCode>\n" +
+            "  <issuerUid>tlarkin</issuerUid>\n" +
+            "  <approvedDateTime>2017-06-27T12:04:18.66</approvedDateTime>\n" +
+            "</Requisition>";
 }