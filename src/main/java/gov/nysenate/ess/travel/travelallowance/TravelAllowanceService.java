4c8fd1377d7807175651786dd995c919c10fee42
= <carpenterda23@yahoo.com>
2017-10-02 16:54:35 -0400

    Added files for transportation allowance and Google maps integration

---
+++ src/main/java/gov/nysenate/ess/travel/travelallowance/TravelAllowanceService.java
@@ -0,0 +1,4 @@
+package gov.nysenate.ess.travel.travelallowance;
+
+public class TravelAllowanceService {
+}
--- src/test/java/gov/nysenate/ess/travel/GsaAllowanceServiceTest.java
+++ src/test/java/gov/nysenate/ess/travel/GsaAllowanceServiceTest.java
@@ -2,6 +2,7 @@ package gov.nysenate.ess.travel;
 
 import gov.nysenate.ess.core.annotation.UnitTest;
 import gov.nysenate.ess.core.model.unit.Address;
+import gov.nysenate.ess.travel.application.model.GsaAllowance;
 import gov.nysenate.ess.travel.application.model.Itinerary;
 import gov.nysenate.ess.travel.application.model.TravelDestination;
 import gov.nysenate.ess.travel.gsa.GsaAllowanceService;
@@ -13,6 +14,8 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 @Category(UnitTest.class)
 public class GsaAllowanceServiceTest {
 
@@ -21,16 +24,17 @@ public class GsaAllowanceServiceTest {
         Address fromAddress = new Address("515 Loudon Rd", "Loudonville", "NY", "12211");
         Address toAddress = new Address("S Mall Arterial", "Albany", "NY", "12210");
 
-        // 6:00am
-        LocalDateTime departure = LocalDateTime.of(2017, Month.SEPTEMBER, 29, 6, 0);
+        LocalDateTime arrival = LocalDateTime.of(2017, Month.SEPTEMBER, 30, 20, 0);
+        LocalDateTime departure = LocalDateTime.of(2017, Month.OCTOBER, 2, 8, 0);
 
-        // 6:45am
-        LocalDateTime arrival = LocalDateTime.of(2017, Month.SEPTEMBER, 29, 6, 45);
         TravelDestination travelDestination = new TravelDestination(arrival, departure, toAddress);
         List<TravelDestination> travelDestinations = Arrays.asList(travelDestination);
 
         GsaAllowanceService gsaAllowanceService = new GsaAllowanceService();
         Itinerary itinerary = new Itinerary(fromAddress, travelDestinations);
-        gsaAllowanceService.computeAllowance(itinerary);
+        GsaAllowance gsaAllowance = gsaAllowanceService.computeAllowance(itinerary);
+        assertEquals(gsaAllowance.getMeals().toBigInteger().intValueExact(), 13 + 26 + 13);
+        assertEquals(gsaAllowance.getLodging().toBigInteger().intValueExact(), 116 + 115);
+        assertEquals(gsaAllowance.getIncidental().toBigInteger().intValueExact(), 0);
     }
 }
