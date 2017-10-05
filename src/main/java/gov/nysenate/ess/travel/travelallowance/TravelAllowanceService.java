c22fecd47aa1d229599d878be81323ee2f6b8900
= <carpenterda23@yahoo.com>
2017-10-05 17:59:22 -0400

    Able to query Google Maps API to get distance between two locations

--- src/main/java/gov/nysenate/ess/travel/travelallowance/TravelAllowanceService.java
+++ src/main/java/gov/nysenate/ess/travel/travelallowance/TravelAllowanceService.java
@@ -1,4 +1,20 @@
 package gov.nysenate.ess.travel.travelallowance;
 
+import gov.nysenate.ess.travel.application.dao.InMemoryTravelApplicationDao;
+import gov.nysenate.ess.travel.application.model.TransportationAllowance;
+import gov.nysenate.ess.travel.application.model.TravelApplication;
+import gov.nysenate.ess.travel.application.model.TravelApplicationStatus;
+import org.springframework.stereotype.Service;
+
 public class TravelAllowanceService {
+
+    private TravelApplication travelApp;
+
+    public TravelAllowanceService(TravelApplication travelApp) {
+        this.travelApp = travelApp;
+    }
+
+    public void updateTravelAllowance() {
+        this.travelApp.Builder().setTransportationAllowance(new TransportationAllowance("0", "0"));
+    }
 }
