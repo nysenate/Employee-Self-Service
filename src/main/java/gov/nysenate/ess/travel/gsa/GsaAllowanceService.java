37d53672b3f4120f1003783dc1ba3d119f1444c6
Hamza Memon <h01memo@siena.edu>
2017-10-06 17:53:58 -0400

    Variable name changes

--- src/main/java/gov/nysenate/ess/travel/gsa/GsaAllowanceService.java
+++ src/main/java/gov/nysenate/ess/travel/gsa/GsaAllowanceService.java
@@ -16,12 +16,9 @@ import java.util.List;
 @Service
 public class GsaAllowanceService {
 
-    private MealIncidentalRates mealIncidentalRates;
-
     public GsaAllowance computeAllowance(Itinerary itinerary) {
         int mealAllowance = 0;
         int lodgingAllowance = 0;
-        int incidentalAllowance = 0;
 
         List<TravelDestination> travelDestinations = itinerary.getTravelDestinations();
         for (TravelDestination travelDestination : travelDestinations) {
@@ -33,80 +30,48 @@ public class GsaAllowanceService {
 
             GsaClient client = new GsaClient(getFiscalYear(currentDate), destinationAddress.getZip5());
             client.setLodging(currentDate.getMonth());
-
-            mealIncidentalRates = client.getMealIncidentalRates();
+            MealIncidentalRates mealIncidentalRates = client.getMealIncidentalRates();
 
             int daysThere = (int) ChronoUnit.DAYS.between(arrival, departure) + 1;
 
             // Same day
             if (daysThere == 1) {
-                // Arriving before 7am: breakfast
-                //if (arrival.getHour() < 7) {
                 mealAllowance += mealIncidentalRates.getBreakfastCost();
-                //}
-
-                // Leaving after 7pm: dinner
-                //if (departure.getHour() >= 19) {
                 mealAllowance += mealIncidentalRates.getDinnerCost();
-                //}
             }
             // More than one day
             else {
-                // Handles the first day
-                // if (arrival.getHour() < 7) {
-                // Arriving before 7am: breakfast
-                mealAllowance += mealIncidentalRates.getBreakfastCost();
-                //}
-                //if (arrival.getHour() < 19) {
-                // Get dinner as you are staying the night
-                mealAllowance += mealIncidentalRates.getDinnerCost();
-                //}
-
-                lodgingAllowance += client.getLodging();
-                currentDate = currentDate.plusDays(1);
-                client = handleIfNewMonth(arrival, currentDate, client, destinationAddress);
-
-                // Handles the inner days (not first or last)
-                for (int i = 2; i < daysThere; i++) {
+                // Handles every but the last day
+                for (int i = 1; i < daysThere; i++) {
                     mealAllowance += mealIncidentalRates.getBreakfastCost();
                     mealAllowance += mealIncidentalRates.getDinnerCost();
 
                     lodgingAllowance += client.getLodging();
                     currentDate = currentDate.plusDays(1);
                     client = handleIfNewMonth(arrival, currentDate, client, destinationAddress);
+                    mealIncidentalRates = client.getMealIncidentalRates();
                 }
 
-                // Handles the last day
-                //if (departure.getHour() > 7) {
-                // Leaving after 7am: breakfast
                 mealAllowance += mealIncidentalRates.getBreakfastCost();
-                //}
-                //if (departure.getHour() >= 19) {
-                // Leaving after 7pm: dinner
                 mealAllowance += mealIncidentalRates.getDinnerCost();
-                //}
             }
         }
 
-        return new GsaAllowance(String.valueOf(mealAllowance), String.valueOf(lodgingAllowance), String.valueOf(incidentalAllowance));
+        return new GsaAllowance(String.valueOf(mealAllowance), String.valueOf(lodgingAllowance), "0");
     }
 
     private GsaClient handleIfNewMonth(LocalDate arrival, LocalDate currentDate, GsaClient client, Address destinationAddress) {
-        GsaClient newClient = client;
-
         if (currentDate.getMonth() != arrival.getMonth()) {
-            // New month
-            newClient = new GsaClient(getFiscalYear(currentDate), destinationAddress.getZip5());
-            newClient.setLodging(currentDate.getMonth());
-            mealIncidentalRates = newClient.getMealIncidentalRates();
+            client = new GsaClient(getFiscalYear(currentDate), destinationAddress.getZip5());
+            client.setLodging(currentDate.getMonth());
         }
 
-        return newClient;
+        return client;
     }
 
-    private int getFiscalYear(LocalDate dateTime) {
-        int year = dateTime.getYear();
-        int month = dateTime.getMonthValue();
+    private int getFiscalYear(LocalDate date) {
+        int year = date.getYear();
+        int month = date.getMonthValue();
 
         int fiscalYear = year;
         if (month >= Month.OCTOBER.getValue()) {
