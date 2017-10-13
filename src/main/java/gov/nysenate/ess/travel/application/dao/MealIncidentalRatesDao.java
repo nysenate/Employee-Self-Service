d66788897c424fdc8cef5163ce8d702b02b99264
Hamza Memon <h01memo@siena.edu>
2017-10-13 17:32:58 -0400

    Started the creation of Dao and Service for meal/incidental rates

---
+++ src/main/java/gov/nysenate/ess/travel/application/dao/MealIncidentalRatesDao.java
@@ -0,0 +1,78 @@
+package gov.nysenate.ess.travel.application.dao;
+
+import gov.nysenate.ess.core.dao.base.BaseRowMapper;
+import gov.nysenate.ess.core.dao.base.BasicSqlQuery;
+import gov.nysenate.ess.core.dao.base.DbVendor;
+import gov.nysenate.ess.core.dao.base.SqlBaseDao;
+import gov.nysenate.ess.travel.application.model.MealIncidentalRate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class MealIncidentalRatesDao extends SqlBaseDao {
+
+    public void insertMealIncidentalRates(MealIncidentalRate[] mealIncidentalRates) {
+        List<SqlParameterSource> paramList = new ArrayList<>();
+        for (MealIncidentalRate mealIncidentalRate : mealIncidentalRates) {
+            MapSqlParameterSource params = new MapSqlParameterSource()
+                    .addValue("totalCost", mealIncidentalRate.getTotalCost())
+                    .addValue("breakfastCost", mealIncidentalRate.getBreakfastCost())
+                    .addValue("dinnerCost", mealIncidentalRate.getDinnerCost())
+                    .addValue("incidentalCost", mealIncidentalRate.getIncidentalCost());
+            paramList.add(params);
+        }
+
+        String sql = SqlMealIncidentalRateQuery.INSERT_RATE.getSql(schemaMap());
+        SqlParameterSource[] batchParams = new SqlParameterSource[paramList.size()];
+        batchParams = paramList.toArray(batchParams);
+        localNamedJdbc.batchUpdate(sql, batchParams);
+    }
+
+    @Transactional(value = "localTxManager")
+    public synchronized void updateMealIncidentalRates(MealIncidentalRate[] mealIncidentalRates) {
+        String sql = SqlMealIncidentalRateQuery.TRUNCATE_TABLE.getSql(schemaMap());
+        localNamedJdbc.query(sql, new MealIncidentalRateRowMapper());
+    }
+
+    private enum SqlMealIncidentalRateQuery implements BasicSqlQuery {
+        INSERT_RATE(
+                "INSERT INTO ${travelSchema}.meal_incidental_rates \n" +
+                        "VALUES (:totalCost, :breakfastCost, :dinnerCost, :incidentalCost)"
+        ),
+        TRUNCATE_TABLE(
+                "TRUNCATE ${travelSchema}.meal_incidental_rates"
+        );
+
+        private String sql;
+
+        SqlMealIncidentalRateQuery(String sql) {
+            this.sql = sql;
+        }
+
+        @Override
+        public String getSql() {
+            return this.sql;
+        }
+
+        @Override
+        public DbVendor getVendor() {
+            return DbVendor.POSTGRES;
+        }
+    }
+
+    private class MealIncidentalRateRowMapper extends BaseRowMapper<MealIncidentalRate> {
+
+        @Override
+        public MealIncidentalRate mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new MealIncidentalRate(rs.getInt("totalCost"), rs.getInt("breakfastCost"),
+                    rs.getInt("dinnerCost"), rs.getInt("incidentalCost"));
+        }
+    }
+}
---
+++ src/main/java/gov/nysenate/ess/travel/application/service/MealIncidentalRatesService.java
@@ -0,0 +1,55 @@
+package gov.nysenate.ess.travel.application.service;
+
+import gov.nysenate.ess.travel.application.dao.MealIncidentalRatesDao;
+import gov.nysenate.ess.travel.application.model.MealIncidentalRate;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class MealIncidentalRatesService {
+
+    private MealIncidentalRatesDao mealIncidentalRatesDao;
+
+    @Autowired
+    public MealIncidentalRatesService(MealIncidentalRatesDao mealIncidentalRatesDao) {
+        this.mealIncidentalRatesDao = mealIncidentalRatesDao;
+    }
+
+    public void insertMealIncidentalRates() {
+        mealIncidentalRatesDao.insertMealIncidentalRates(getMealIncidentalRates());
+    }
+
+    public void updateMealIncidentalRates() {
+        mealIncidentalRatesDao.updateMealIncidentalRates(getMealIncidentalRates());
+    }
+
+    private MealIncidentalRate[] getMealIncidentalRates() {
+        // TODO Checked scraped content with values in database
+        Document document = null;
+        try {
+            document = Jsoup.connect("https://www.gsa.gov/travel/plan-book/per-diem-rates/meals-and-incidental-expenses-mie-breakdown").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Elements elements = document.select("table tbody tr td");
+        MealIncidentalRate[] mealIncidentalRates = new MealIncidentalRate[elements.size() / 6];
+
+        for (int i = 0, j = 0; i < elements.size(); i += 6, j++) {
+            // Substring gets rid of $
+            int total = Integer.parseInt(elements.get(i).text().substring(1));
+            int breakfast = Integer.parseInt(elements.get(i + 1).text().substring(1));
+            int dinner = Integer.parseInt(elements.get(i + 3).text().substring(1));
+            int incidental = Integer.parseInt(elements.get(i + 4).text().substring(1));
+
+            mealIncidentalRates[j] = new MealIncidentalRate(total, breakfast, dinner, incidental);
+        }
+
+        return mealIncidentalRates;
+    }
+}
