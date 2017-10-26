b60194cee89792cde1f3c5f3279814942d42c291
= <carpenterda23@yahoo.com>
2017-10-26 17:56:20 -0400

    Refactored AddressValidation into a service, a controller, and a view

--- src/main/java/gov/nysenate/ess/travel/maps/AddressValidationCtrl.java
+++ src/main/java/gov/nysenate/ess/travel/maps/AddressValidationCtrl.java
@@ -9,6 +9,7 @@ import gov.nysenate.ess.core.controller.api.BaseRestApiCtrl;
 import jdk.nashorn.api.scripting.JSObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,25 +25,14 @@ public class AddressValidationCtrl extends BaseRestApiCtrl {
 
     private static final Logger log = LoggerFactory.getLogger(AddressValidationCtrl.class);
 
-    @RequestMapping(value="")
-    public boolean returnValidationResult(@RequestParam String addr1,
-                                          @RequestParam String city,
-                                          @RequestParam String state) {
+    @Autowired
+    AddressValidationService addressValidationService;
 
-        if (validateAddress(addr1, city, state).equals("SUCCESS")) return true;
-        else return false;
-    }
+    @RequestMapping(value = "")
+    public BaseResponse returnValidationResult(@RequestParam String addr1,
+                                               @RequestParam String city,
+                                               @RequestParam String state) {
 
-    private String validateAddress(String addr1, String city, String state) {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "https://pubgeo.nysenate.gov/api/v2/address/validate?" +
-                "addr1=" + addr1 + "&" +
-                "city="  + city + "&" +
-                "state=" + state;
-        String resp = restTemplate.getForObject(url, String.class);
-        JsonParser parser = new JsonParser();
-        JsonObject jsonResponse = parser.parse(resp).getAsJsonObject();
-        JsonElement status = jsonResponse.get("status");
-        return status.getAsString();
+        return new ViewObjectResponse<>(new SageResponseView(addressValidationService.validateAddress(addr1, city, state)));
     }
 }
