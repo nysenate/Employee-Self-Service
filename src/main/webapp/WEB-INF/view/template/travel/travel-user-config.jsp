d73677f46073f425e1a74607f3557df33b5d3e1e
SenateUser <hmemon67@gmail.com>
2017-10-27 17:58:06 -0400

    Continued user configuration page. Started getting all employees.

--- src/main/webapp/WEB-INF/view/template/travel/travel-user-config.jsp
+++ src/main/webapp/WEB-INF/view/template/travel/travel-user-config.jsp
@@ -8,7 +8,17 @@
       <p class="content-info">Grant another supervisor privileges to review and/or approve your travel needs.</p>
       <div class="padding-10">
         <table class="simple-table">
-
+          <thead>
+          <tr>
+            <th>#</th>
+            <th>Supervisor</th>
+            <th>Status</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+          </tr>
+          </thead>
+          <tbody>
+          </tbody>
         </table>
       </div>
     </div>
--- src/main/webapp/assets/js/src/travel/travel-user-config.js
+++ src/main/webapp/assets/js/src/travel/travel-user-config.js
@@ -1,6 +1,34 @@
 var essTravel = angular.module('essTravel');
-essTravel.controller('UserConfigCtrl', ['$scope', userConfigCtrl]);
+essTravel.controller('UserConfigCtrl', ['$scope', 'appProps', 'ActiveEmployeeApi', userConfigCtrl]);
 
-function userConfigCtrl($scope){
+function userConfigCtrl($scope, appProps, ActiveEmployeeApi){
 
+    $scope.state = {
+        empId: appProps.user.employeeId,
+        selectedGrantee: null,
+        grantees: null   // Stores an ordered list of the supervisors.
+    };
+
+    console.log(ActiveEmployeeApi.get({activeOnly: true}));
+
+    $scope.init = function(){
+        $scope.state.selectedGrantee = null;
+        $scope.state.grantees = [];
+
+        ActiveEmployeeApi.get({activeOnly: true}, function (resp) {
+            console.log("blah");
+        }).$promise.then(function (resp) {
+            // Link up with any existing grants
+            return ActiveEmployeeApi.get({activeOnly: true}, function (resp) {
+                console.log("blah");
+            }).$promise;
+        }).then(function (resp) {
+            return ActiveEmployeeApi.get({activeOnly: true}, function (resp) {
+                console.log("blah");
+            }).$promise;
+        }).catch(function (resp) {
+            modals.open('500', {details: resp});
+            console.log(resp);
+        });
+    };
 }
