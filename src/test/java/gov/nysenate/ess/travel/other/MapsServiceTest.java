d92d0fc29b74b505928d509b8a266a7b980b719b
= <carpenterda23@yahoo.com>
2017-11-06 17:27:14 -0500

    Updated test to reflect new MapsService

--- src/test/java/gov/nysenate/ess/travel/other/MapsServiceTest.java
+++ src/test/java/gov/nysenate/ess/travel/other/MapsServiceTest.java
@@ -1,25 +1,31 @@
 package gov.nysenate.ess.travel.other;
 
 import gov.nysenate.ess.core.BaseTest;
+import gov.nysenate.ess.core.model.unit.Address;
 import gov.nysenate.ess.travel.maps.MapsService;
 import gov.nysenate.ess.web.SillyTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Category(SillyTest.class)
 public class MapsServiceTest extends BaseTest {
     @Autowired
     MapsService mapsService;
 
-    String origin = "515 Loudon Road Loudonville, NY, 12211";
-    String[] destinations = new String[] {
-            "Bombers Burrito Bar, 258 Lark St, Albany, NY 12210",
-            "Times Union Center, 51 S Pearl St, Albany, NY 12207"
-    };
+    Address origin = new Address("515 Loudon Road Loudonville, NY, 12211");
+    List<Address> destinations = Arrays.asList(new Address[] {
+            new Address("515 Loudon Road Loudonville, NY, 12211"),
+            new Address("Bombers Burrito Bar, 258 Lark St, Albany, NY 12210"),
+            new Address("Times Union Center, 51 S Pearl St, Albany, NY 12207"),
+            new Address("515 Loudon Road Loudonville, NY, 12211")
+    });
 
     @Test
     public void testGetDistance() {
-        mapsService.getTripDistance(origin, destinations);
+        mapsService.getTripDistance(destinations);
     }
 }
