c2eeae6f9e993c029a859ed1a00c8eb396814e19
SenateUser <hmemon67@gmail.com>
2017-11-30 17:58:32 -0500

    Refactored Gsa integration into Component

--- src/test/java/gov/nysenate/ess/travel/unit/allowance/GsaClientIT.java
+++ src/test/java/gov/nysenate/ess/travel/unit/allowance/GsaClientIT.java
@@ -7,13 +7,21 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.junit.Assert.assertEquals;
+
 @Category(IntegrationTest.class)
-public class GsaClientIT extends BaseTest{
+public class GsaClientIT extends BaseTest {
 
     @Autowired GsaClient client;
 
     @Test(expected = IllegalArgumentException.class)
     public void incorrectFiscalYear() {
         client.scrapeGsa(0, "12110");
+    }
+
+    @Test
+    public void correctFiscalYear(){
+        client.scrapeGsa(2018, "12110");
+        assertEquals(client.getBreakfastCost(), 13);
     }
 }
