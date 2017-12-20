eae765202b32eb65e17b0deca31f5855dadfe672
Sam Stouffer <samlthecaml@gmail.com>
2017-12-20 12:10:18 -0500

    Fix bug in expected hours calculation
    In a previous commit I consolidated the two methods responsible for
    calculating expected hours: one that takes a pay period returning a
    number, and another that takes a date range returning an ExpectedHours
    object.  I did not realize that the number returned from the first
    calculation was the number of hours expected after the end of the pay
    period instead of the expected hours prior to the pay period.  This
    caused all expected hours calculations to be off by 1 pay period's worth
    of hours.
    
    In this commit I address this issue by further standardizing the
    expected hours methods to both return ExpectedHours objects.  This way,
    you know what you're getting from the method, and can extract the
    desired value from the object.  I also fixed a related bug in the
    AccrualComputeService where a date that was labeled as a "from" date was
    actually the "to" date of the previous pay period.
    
    Fixes #11572, #11588

--- src/main/java/gov/nysenate/ess/time/model/accrual/PeriodAccSummary.java
+++ src/main/java/gov/nysenate/ess/time/model/accrual/PeriodAccSummary.java
@@ -1,14 +1,12 @@
 package gov.nysenate.ess.time.model.accrual;
 
 import gov.nysenate.ess.core.model.period.PayPeriod;
+import gov.nysenate.ess.time.model.expectedhrs.ExpectedHours;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
 
 /**
  * Helper class to store accrual summary information per pay period.
@@ -64,6 +62,19 @@ public class PeriodAccSummary extends AccrualSummary
 
     public BigDecimal getTotalHoursYtd() {
         return getTotalHoursUsed();
+    }
+
+    /**
+     * Sets expected hour values for this record.
+     * The values in the database are not always correct so we override with calculated values.
+     *
+     * @param expectedHours {@link ExpectedHours}
+     */
+    public void setExpectedHours(ExpectedHours expectedHours) {
+        setPrevTotalHoursYtd(expectedHours.getYtdHoursExpected());
+        setExpectedBiweekHours(expectedHours.getPeriodHoursExpected());
+        setExpectedTotalHours(expectedHours.getPeriodEndHoursExpected());
+
     }
 
     /** --- Basic Getters/Setters --- */
@@ -164,65 +175,4 @@ public class PeriodAccSummary extends AccrualSummary
         this.periodAccUsage = periodAccUsage;
     }
 
-    @Deprecated
-    public static int getWorkingDaysBetweenDates(Date startDate, Date endDate) {
-        Calendar startCal = Calendar.getInstance();
-        startCal.setTime(startDate);
-
-        Calendar endCal = Calendar.getInstance();
-        endCal.setTime(endDate);
-        Calendar curCal = Calendar.getInstance();
-
-        curCal.setTime(startCal.getTime());
-        int workDays = 0;
-
-        // if start and end are the same and it is a weekday, return 1
-        // if start and end are the same and it is a weekend, return 0
-        if (startCal.getTimeInMillis() == endCal.getTimeInMillis()) {
-            if (curCal.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY && curCal.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
-                return 1;
-            }
-            else {
-                return 0;
-            }
-        }
-        //Return -1 if the start is later than the end date which indicates an error
-        else if (startCal.getTimeInMillis() > endCal.getTimeInMillis()) {
-            return -1;
-        }
-
-        if (startCal.getTimeInMillis() > endCal.getTimeInMillis()) {
-            startCal.setTime(endDate);
-            endCal.setTime(startDate);
-        }
-        /*
-        * Subtracting dates leaves one day lower than actual work days. We simply addUsage one day
-        * to the end date to get the correct work days.
-        * EX:   Subtracting Dates        Subtract Value    Real Work Days
-        *        1/1/14 - 1/1/14         0                 1  (0 +1)
-        *        6/9/14(Mo)-6/13/14(Fr)  4                 5  (4 +1)
-        *        6/9/14(Mo)-6/14/14(Sa)  5                 5  (5 + 0)
-        *        6/9/14(Mo)-6/15/14(Su)  5                 5  (5[Mo-Fr] + 0)
-        *        6/9/14(Mo)-6/15/14(Mo)  6                 6  (5 + 1)
-        *
-         */
-        //endCal.addUsage(Calendar.DATE, 1);
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("E");
-        SimpleDateFormat simpleDateFormat0 = new SimpleDateFormat("MM/dd/yyyy");
-        SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a");
-
-        do {
-            if (curCal.getTime().equals(endCal.getTime())) {
-                logger.debug("IN DO WHILE Current Date equals End Date");
-            }
-            //excluding start date
-            if (curCal.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY && curCal.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY) {
-                ++workDays;
-            }
-            logger.debug(simpleDateFormat0.format(curCal.getTime())+" "+simpleDateFormat.format(curCal.getTime())+" Workdays:"+workDays);
-            curCal.add(Calendar.DATE,1);
-        } while (curCal.getTime().equals(endCal.getTime())||curCal.getTime().before(endCal.getTime()));
-
-        return workDays;
-    }
 }
--- src/main/java/gov/nysenate/ess/time/service/expectedhrs/ExpectedHoursService.java
+++ src/main/java/gov/nysenate/ess/time/service/expectedhrs/ExpectedHoursService.java
@@ -5,7 +5,6 @@ import gov.nysenate.ess.core.model.period.PayPeriod;
 import gov.nysenate.ess.time.model.expectedhrs.ExpectedHours;
 import gov.nysenate.ess.time.model.expectedhrs.InvalidExpectedHourDatesEx;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 /**
@@ -16,7 +15,26 @@ import java.time.LocalDate;
  */
 public interface ExpectedHoursService {
 
-    BigDecimal getExpectedHours(int empId, PayPeriod payPeriod);
-
+    /**
+     * Calculate {@link ExpectedHours} for the given date range.
+     *
+     * @param empId int
+     * @param dateRange Range<LocalDate>
+     * @return {@link ExpectedHours}
+     * @throws InvalidExpectedHourDatesEx if provided date range is invalid
+     */
     ExpectedHours getExpectedHours(int empId, Range<LocalDate> dateRange) throws InvalidExpectedHourDatesEx;
+
+
+    /**
+     * Calculate {@link ExpectedHours} for the given pay period
+     *
+     * @param empId int
+     * @param payPeriod {@link PayPeriod}
+     * @return {@link ExpectedHours}
+     * @throws InvalidExpectedHourDatesEx if provided date range is invalid
+     */
+    default ExpectedHours getExpectedHours(int empId, PayPeriod payPeriod) throws InvalidExpectedHourDatesEx {
+        return getExpectedHours(empId, payPeriod.getDateRange());
+    }
 }
