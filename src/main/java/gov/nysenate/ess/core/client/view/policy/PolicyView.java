480291d43aef50d0ae0da18db290956ae49e4863
Sam Stouffer <samlthecaml@gmail.com>
2018-01-11 15:55:35 -0500

    Add get policy by id api method
    Adds a method to PolicyApiCtrl that gets a single policy based on its
    policy id.
    
    Also remove logic that constructs a full url for policy views.  The
    context path is present on the front end so this is unnecessary.  This
    field is now called path.

--- src/main/java/gov/nysenate/ess/core/client/view/policy/PolicyView.java
+++ src/main/java/gov/nysenate/ess/core/client/view/policy/PolicyView.java
@@ -13,16 +13,16 @@ public class PolicyView implements ViewObject {
     private static final String policyDir = "/assets/policies/";
 
     private String title;
-    private String url;
+    private String path;
     private Boolean active;
     private Integer policyId;
     private LocalDateTime effectiveDateTime;
 
     protected PolicyView() {}
 
-    public PolicyView(Policy policy, String ctxPath) {
+    public PolicyView(Policy policy) {
         this.title = policy.getTitle();
-        this.url = ctxPath + policyDir + policy.getFilename();
+        this.path = policyDir + policy.getFilename();
         this.active = policy.getActive();
         this.policyId = policy.getPolicyId();
         this.effectiveDateTime = policy.getEffectiveDateTime();
@@ -34,8 +34,8 @@ public class PolicyView implements ViewObject {
     }
 
     @XmlElement
-    public String getUrl() {
-        return url;
+    public String getPath() {
+        return path;
     }
 
     @XmlElement
--- src/main/java/gov/nysenate/ess/core/controller/api/PolicyApiCtrl.java
+++ src/main/java/gov/nysenate/ess/core/controller/api/PolicyApiCtrl.java
@@ -4,50 +4,56 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import gov.nysenate.ess.core.client.response.base.ListViewResponse;
 import gov.nysenate.ess.core.client.response.base.SimpleResponse;
+import gov.nysenate.ess.core.client.response.base.ViewObjectResponse;
 import gov.nysenate.ess.core.client.view.policy.AcknowledgementView;
 import gov.nysenate.ess.core.client.view.policy.PolicyView;
 import gov.nysenate.ess.core.model.base.InvalidRequestParamEx;
 import gov.nysenate.ess.core.model.policy.Acknowledgement;
 import gov.nysenate.ess.core.model.policy.Policy;
 import gov.nysenate.ess.core.util.ShiroUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toSet;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @RestController
 @RequestMapping(BaseRestApiCtrl.REST_PATH + "/policies")
 public class PolicyApiCtrl extends BaseRestApiCtrl {
 
-    private static final ImmutableList<Policy> dummyPolicies = ImmutableList.<Policy>builder()
-            .add(new Policy("Policy 1", "dummy.pdf", true, 1, LocalDateTime.now()))
-            .add(new Policy("Policy 2", "dummy.pdf", true, 2, LocalDateTime.now()))
-            .add(new Policy("Policy 3", "dummy.pdf", true, 3, LocalDateTime.now()))
-            .add(new Policy("Policy 4", "dummy.pdf", true, 4, LocalDateTime.now()))
-            .build();
-
-    private static final ImmutableSet<Integer> dummyPolicyIds = dummyPolicies.stream()
-            .map(Policy::getPolicyId)
-            .collect(collectingAndThen(toSet(), ImmutableSet::copyOf));
+    private static final ImmutableSet<Integer> dummyPolicyIds = ImmutableSet.of(1,2,3,4);
+    private static Policy getDummyPolicy(int pid) {
+        return new Policy("Policy " + pid, "dummy.pdf", true, pid, LocalDateTime.now());
+    }
 
     @RequestMapping(value = "", method = {GET, HEAD})
-    public ListViewResponse<PolicyView> getActivePolicies(HttpServletRequest request) {
+    public ListViewResponse<PolicyView> getActivePolicies() {
 
-        String ctxPath = request.getContextPath();
+        List<Policy> policies = dummyPolicyIds.stream()
+                .map(PolicyApiCtrl::getDummyPolicy)
+                .collect(toList());
 
-        List<PolicyView> policyViews = dummyPolicies.stream()
-                .map(policy -> new PolicyView(policy, ctxPath))
+        List<PolicyView> policyViews = policies.stream()
+                .map(PolicyView::new)
                 .collect(toList());
 
         return ListViewResponse.of(policyViews, "policies");
+    }
+
+    @RequestMapping(value = "/{policyId:\\d+}", method = {GET, HEAD})
+    public ViewObjectResponse<PolicyView> getPolicy(@PathVariable int policyId) {
+        if (!dummyPolicyIds.contains(policyId)) {
+            throw new InvalidRequestParamEx(String.valueOf(policyId), "policyId", "int",
+                    "testtttttttttt");
+        }
+        Policy policy = getDummyPolicy(policyId);
+        PolicyView policyView = new PolicyView(policy);
+        return new ViewObjectResponse<>(policyView, "policy");
     }
 
     @RequestMapping(value = "/acknowledgements", method = {GET, HEAD})
