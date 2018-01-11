cc15f2331d0b777b11e7c234f0f5ed22e7e1efd1
Anthony Calabrese <aacal050@gmail.com>
2018-01-11 15:37:50 -0500

    Update Schema with Policy Tables
    The sql for ESS policy has been added as a patch for systems that are
    already up and running and as part of the schema for anyone new looking
    to build ESS

--- src/main/resources/sql/core/ess-schema.sql
+++ src/main/resources/sql/core/ess-schema.sql
@@ -94,6 +94,25 @@ ALTER TABLE ONLY user_roles
     ADD CONSTRAINT user_roles_pkey PRIMARY KEY (id);
 
 --
+--Create tables for ESS policy and set Primary Keys
+--
+CREATE TABLE ess.policy (
+    policy_id   SERIAL PRIMARY KEY,
+    title       VARCHAR(250),
+    filename        VARCHAR(250),
+    active      BOOLEAN,
+    effective_date_time       VARCHAR(40)
+);
+
+CREATE TABLE ess.acknowledgement (
+    emp_id      INTEGER,
+    policy_id    INTEGER REFERENCES ess.policy,
+    timestamp       VARCHAR(40)
+);
+
+ALTER TABLE ess.acknowledgement ADD PRIMARY KEY (emp_id,policy_id);
+
+--
 -- Add permissions for all roles.
 --
 GRANT ALL PRIVILEGES ON SCHEMA ess TO PUBLIC;
