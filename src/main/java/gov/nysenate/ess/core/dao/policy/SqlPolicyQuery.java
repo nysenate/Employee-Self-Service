a820e07b999035e465cdfbc3ce9adda8f3784d4d
Anthony Calabrese <aacal050@gmail.com>
2018-01-11 16:35:04 -0500

    Create SqlPolicyQuery Enum
    An enum containing all of the sql ESS policy will need for an mvp has
    been created. A dao and testing will need to be created / performed on
    these classes

--- src/main/java/gov/nysenate/ess/core/dao/policy/SqlPolicyQuery.java
+++ src/main/java/gov/nysenate/ess/core/dao/policy/SqlPolicyQuery.java
@@ -1,4 +1,53 @@
 package gov.nysenate.ess.core.dao.policy;
 
-public enum SqlPolicyQuery {
+import gov.nysenate.ess.core.dao.base.BasicSqlQuery;
+import gov.nysenate.ess.core.dao.base.DbVendor;
+
+public enum SqlPolicyQuery implements BasicSqlQuery {
+
+    GET_ALL_ACTIVE_POLICIES_SQL(
+        "SELECT * FROM ess.policy \n" +
+                "WHERE active = true;"
+    ),
+
+    GET_POLICY_BY_ID_SQL(
+            "SELECT * FROM ess.policy \n" +
+                    "WHERE id = ?"
+    ),
+
+    INSERT_POLICY_SQL(
+        "INSERT INTO ess.policy (title, link, active, effective_date_time)\n" +
+                "VALUES (?, ?, ?, ?);"
+    ),
+
+    GET_ALL_ACKNOWLEDGEMENTS(
+            "SELECT * FROM ess.acknowledgement"
+    ),
+
+    GET_ACK_BY_ID(
+            "SELECT * FROM ess.acknowledgement \n" +
+                    "WHERE emp_id = ? AND policy_id = ?;"
+    ),
+
+    INSERT_ACK_SQL(
+        "INSERT INTO ess.acknowledgement (emp_id, policy_id, timestamp)\n" +
+                "VALUES (2,1,'1/11/18');"
+    );
+
+
+    private String sql;
+
+    SqlPolicyQuery(String sql) {
+        this.sql = sql;
+    }
+
+    @Override
+    public String getSql() {
+        return this.sql;
+    }
+
+    @Override
+    public DbVendor getVendor() {
+        return DbVendor.POSTGRES;
+    }
 }
