0b36900a0083840e8e0b7e0863b7a491652505fb
Anthony Calabrese <aacal050@gmail.com>
2018-01-16 15:03:43 -0500

    Create DuplicateAckEx for ESS Ack
    This exception was created to not allow users to acknowledge the same
    document twice

---
+++ src/main/java/gov/nysenate/ess/core/model/acknowledgement/DuplicateAckEx.java
@@ -0,0 +1,18 @@
+package gov.nysenate.ess.core.model.acknowledgement;
+
+/**
+ * An exception thrown when an {@link AckDoc} has already been acknowledged.
+ */
+public class DuplicateAckEx extends RuntimeException {
+    private int ackDocid;
+
+    public DuplicateAckEx(int ackDocid) {
+        super("The requested document: " + ackDocid + " has already been acknowledged");
+        this.ackDocid = ackDocid;
+    }
+
+    public int getAckDocid() {
+        return ackDocid;
+    }
+
+}
