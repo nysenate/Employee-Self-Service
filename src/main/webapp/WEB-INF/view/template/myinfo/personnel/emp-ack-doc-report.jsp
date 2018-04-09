044e54f90e6b8ca5aa35d3e0e725f3233c30bdc0
Sam Stouffer <samlthecaml@gmail.com>
2018-04-09 16:53:03 -0400

    Add ability to submit acks from ack report page.
    This commit modifies the employee acknowledgment report page by adding
    the ability for personnel ack. managers to submit acknowledgments on
    behalf of an employee.  Any pending acknowledgments on the employee
    will open an acknowledgment prompt if clicked.  If this prompt is
    accepted, an acknowledgment will be posted for selected doc/employee.
    
    Refs #11714

--- src/main/webapp/WEB-INF/view/template/myinfo/personnel/emp-ack-doc-report.jsp
+++ src/main/webapp/WEB-INF/view/template/myinfo/personnel/emp-ack-doc-report.jsp
@@ -23,6 +23,9 @@
             </select>
           </p>
         </div>
+        <p class="content-info">
+          Click on a pending acknowledgment to initiate an acknowledgment on behalf of the employee.
+        </p>
 
         <div class="ack-doc-display">
 
@@ -30,10 +33,13 @@
           <ul class="unacknowledged-doc-list">
             <li ng-show="state.unacknowledged.length == 0"><p>The employee has no pending acknowledgments</p></li>
             <li ng-repeat="status in state.unacknowledged | orderBy:'status.ackDoc.effectiveDateTime'">
-              <p class="ack-doc-list-item">
-                <span class="icon-text-document"></span>
-                <span ng-bind="status.ackDoc.title"></span>
-              </p>
+              <a ng-click="initiatePersonnelAck(status)"
+                 title="Click to record an acknowledgment on behalf of the employee.">
+                <p class="ack-doc-list-item">
+                  <span class="icon-text-document"></span>
+                  <span ng-bind="status.ackDoc.title"></span>
+                </p>
+              </a>
             </li>
           </ul>
 
@@ -54,5 +60,33 @@
         </div>
       </div>
     </div>
+  </div>
+  <div modal-container>
+    <modal modal-id="personnel-acknowledge-prompt">
+      <div confirm-modal rejectable="true" title="Submit Acknowledgment for Employee"
+           resolve-button="Submit"
+           reject-button="Cancel" reject-class="time-neutral-button">
+        <div class="content-info acknowledgment-text">
+          You are attempting to submit the following acknowledgment:
+          <ul class="bold-text">
+            <li>Employee: {{selectedEmp.fullName}}</li>
+            <li>Year: {{state.selectedDoc.effectiveDateTime | moment:'YYYY'}}</li>
+            <li>Document: {{state.selectedDoc.title}}</li>
+          </ul>
+          By pressing "Submit" you are verifying that this employee has submitted
+          a signed paper form acknowledging receipt of the policy/document.
+        </div>
+      </div>
+    </modal>
+    <modal modal-id="personnel-acknowledge-success">
+      <div confirm-modal title="Acknowledgment Complete"
+           resolve-button="Ok" resolve-class="time-neutral-button">
+        <p>
+          You have successfully acknowledged
+          <span class="ack-doc-title">{{state.selectedDoc.title}}</span>
+          on behalf of {{selectedEmp.fullName}}.
+        </p>
+      </div>
+    </modal>
   </div>
 </section>
--- src/main/webapp/assets/js/src/myinfo/personnel/emp-ack-doc-report-ctrl.js
+++ src/main/webapp/assets/js/src/myinfo/personnel/emp-ack-doc-report-ctrl.js
@@ -1,33 +1,55 @@
 (function () {
     angular.module('essMyInfo')
-        .controller('EmpAckDocReportCtrl', ['$scope', 'EmpAckReportApi' ,
+        .controller('EmpAckDocReportCtrl', ['$scope', 'modals', 'EmpAckReportApi', 'AcknowledgmentApi',
                                             empAckDocReportCtrl])
     ;
 
-    function empAckDocReportCtrl($scope, EmpAckReportApi) {
+    function empAckDocReportCtrl($scope, modals, EmpAckReportApi, AckApi) {
 
         var initialState = {
             loadingAcks: false,
             yearDocMap: {},
             years: [],
             selectedYear: null,
+            selectedDoc: null,
             unacknowledged: [],
             acknowledged: []
         };
 
         resetState();
 
+        /* --- Watches --- */
+
         $scope.$watch('selectedEmp', onSelectedEmpChange);
         $scope.$watch('state.selectedYear', onSelectedYearChange);
 
-        $scope.getYears = function () {
-            return Object.keys($scope.state.yearDocMap);
+        /* --- Display Methods --- */
+
+        /**
+         * Initiates a personnel acknowledgment for the given document by opening a prompt dialogue.
+         * If the dialogue is confirmed, an acknowledgment will be posted for the given employee/document.
+         * @param ackStatus
+         */
+        $scope.initiatePersonnelAck = function (ackStatus) {
+            $scope.state.selectedDoc = ackStatus.ackDoc;
+            modals.open('personnel-acknowledge-prompt')
+                .then(function () {
+                    postPersonnelAck(ackStatus.ackDoc);
+                });
         };
 
+        /* --- Internal methods --- */
+
+        /**
+         * Reset the scope state to the initial state.
+         */
         function resetState() {
             $scope.state = angular.copy(initialState);
         }
 
+        /**
+         * When a new employee is selected, reset page data and request new data from the api
+         */
         function onSelectedEmpChange() {
             resetState();
 
@@ -53,13 +75,19 @@
                     var docs = $scope.state.yearDocMap[year];
                     docs.push(ack);
                 });
+                // Create a list of years based on the returned documents.
                 var years = Object.keys($scope.state.yearDocMap).map(parseInt);
                 years.sort().reverse();
                 $scope.state.years = years;
+                // Select the latest year by default
                 $scope.state.selectedYear = years[0];
             }
         }
 
+        /**
+         * When the user selects a new year,
+         * load acknowledgments for that year and categorize them for display.
+         */
         function onSelectedYearChange() {
             if (!$scope.state.selectedYear) {
                 return;
@@ -76,6 +104,30 @@
                     acked.push(status);
                 }
             });
+        }
+
+        /**
+         * Post an acknowledgment on behalf of an employee.
+         * On success, reload ack data for the employee and display a success message.
+         * @param ackDoc
+         */
+        function postPersonnelAck(ackDoc) {
+            var params = {
+                empId: $scope.selectedEmp.empId,
+                ackDocId: ackDoc.id
+            };
+
+            AckApi.save(params, {}, onSuccess, $scope.handleErrorResponse);
+
+            function onSuccess() {
+                onSelectedEmpChange();
+                showPersonnelAckSuccessModal(ackDoc);
+            }
+        }
+
+        function showPersonnelAckSuccessModal(ackDoc) {
+            $scope.state.selectedDoc = ackDoc;
+            modals.open('personnel-acknowledge-success');
         }
     }
 })();
