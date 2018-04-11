ac493405607abc81a6cf0ef84c8c288b965d80f2
Sam Stouffer <samlthecaml@gmail.com>
2018-04-11 12:13:14 -0400

    Secure ack. report templates.
    Adds authorization checks to ack. report templates.  If the user is not
    authorized to view ack. reports, an template with an error message is
    returned instead.  The time message template is moved to be a common
    template for this use.

--- src/main/java/gov/nysenate/ess/web/controller/template/MyInfoTemplateCtrl.java
+++ src/main/java/gov/nysenate/ess/web/controller/template/MyInfoTemplateCtrl.java
@@ -1,8 +1,12 @@
 package gov.nysenate.ess.web.controller.template;
 
+import gov.nysenate.ess.core.model.auth.SimpleEssPermission;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.Permission;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,16 +19,38 @@ import javax.servlet.http.HttpServletRequest;
 public class MyInfoTemplateCtrl extends BaseTemplateCtrl
 {
     private static final Logger logger = LoggerFactory.getLogger(MyInfoTemplateCtrl.class);
-    protected static final String MYINFO_TMPL_BASE_URL = TMPL_BASE_URL + "/myinfo";
+    static final String MYINFO_TMPL_BASE_URL = TMPL_BASE_URL + "/myinfo";
 
     /**
      * Just return the corresponding template...
-     *
+     * Unless the template uri is caught by one of the methods below.
      * @param request HttpServletRequest
      * @return String - passed in uri
      */
     @RequestMapping(value = "/**")
     public String getMyinfoPage(HttpServletRequest request) {
         return request.getRequestURI();
+    }
+
+    /**
+     * Acknowledgment Report Pages
+     *
+     * Returns the given page if the currently authenticated user is permitted to view ack. report pages.
+     * Otherwise return an error page indicating that the user does not have required permission.
+     * @param request HttpServletRequest - the request (used to extract page uri)
+     * @return String - passed in uri or error page depending on permissions
+     */
+    @RequestMapping(value = {
+            "/personnel/ack-doc-report",
+            "/personnel/emp-ack-doc-report"
+    })
+    public String getAcknowledgmentReportPage(HttpServletRequest request, ModelMap modelMap) {
+        final Permission managementPermission = SimpleEssPermission.ACK_REPORT_GENERATION.getPermission();
+        if (SecurityUtils.getSubject().isPermitted(managementPermission)) {
+            return request.getRequestURI();
+        }
+        modelMap.addAttribute("level", "error")
+                .addAttribute("title", "This page is only available to Personnel");
+        return SIMPLE_MESSAGE_URI;
     }
 }
