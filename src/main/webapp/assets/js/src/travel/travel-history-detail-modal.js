ba6fd1ce050c6dbef29b79c48f081531b29a7440
Kevin Caseiras <kcaseiras@gmail.com>
2018-06-08 15:17:24 -0400

    Use the print view as the standard view of a travel application

--- src/main/webapp/assets/js/src/travel/travel-history-detail-modal.js
+++ src/main/webapp/assets/js/src/travel/travel-history-detail-modal.js
@@ -10,37 +10,8 @@ essTravel.directive('travelHistoryDetailModal', ['appProps', function (appProps)
     .controller('TravelHistoryDetailCtrl', ['$scope', 'modals', travelDetailCtrl]);
 
 function travelDetailCtrl($scope, modals) {
-    //display the travel application details corresponding to what they clicked on
 
     $scope.app = modals.params();
-    console.log($scope.app);
-
-    /**
-     * @return {Array} Array containing all modes of transportation in this travel application.
-     */
-    $scope.getModesOfTransportation = function() {
-        var mots = [];
-        var legs = $scope.app.route.outboundLegs.concat($scope.app.route.returnLegs);
-        legs.forEach(function(leg, index) {
-            var mot = leg.modeOfTransportation;
-            if (!arrayContains(mots, mot)) {
-                mots.push(mot);
-            }
-        });
-        return mots;
-
-        // Returns true of an array contains an object. Compares object equality.
-        function arrayContains(array, obj) {
-            var contains = false;
-            array.forEach(function(element) {
-                if (angular.equals(element, obj)) {
-                    contains = true;
-                }
-            });
-            return contains;
-        }
-
-    };
 
     $scope.exit = function () {
         modals.resolve();
