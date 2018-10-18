6bbf4dec7aaec7c86409a9149eabde730f0f7bcc
Kevin Caseiras <kcaseiras@gmail.com>
2018-10-18 10:50:44 -0400

    Update sql script to set existing orders as reconciled
    refs #10682

--- src/main/resources/sql/patches/20180319_reconciliation_col.sql
+++ src/main/resources/sql/patches/20180319_reconciliation_col.sql
@@ -1,2 +1,9 @@
 ALTER TABLE supply.requisition_content
   ADD COLUMN is_reconciled boolean NOT NULL DEFAULT FALSE;
+
+UPDATE supply.requisition_content rc
+SET is_reconciled = true
+WHERE rc.revision_id IN
+  (SELECT r.current_revision_id FROM supply.requisition r INNER JOIN supply.requisition_content rc
+    ON r.requisition_id = rc.requisition_id AND r.current_revision_id = rc.revision_id
+    WHERE rc.STATUS = 'APPROVED')
