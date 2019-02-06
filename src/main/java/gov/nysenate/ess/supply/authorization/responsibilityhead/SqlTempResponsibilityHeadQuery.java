481d45bb6b0d3a79e2417a6e7838d67392af6ce1
Kevin Caseiras <kcaseiras@gmail.com>
2019-02-06 14:07:31 -0500

    Add Temporary RCH table for supply
    Employees, especially student fellows, need a way to order for locations
    outside of their rch. To account for this edge case, this implements a
    temporary rch table which allows users to be assigned additional rch's
    for the purposes of placing supply orders.
    
    refs #12412

---
+++ src/main/java/gov/nysenate/ess/supply/authorization/responsibilityhead/SqlTempResponsibilityHeadQuery.java
@@ -0,0 +1,48 @@
+package gov.nysenate.ess.supply.authorization.responsibilityhead;
+
+import gov.nysenate.ess.core.dao.base.BasicSqlQuery;
+import gov.nysenate.ess.core.dao.base.DbVendor;
+
+public enum SqlTempResponsibilityHeadQuery implements BasicSqlQuery {
+
+    ACTIVE_TEMP_RESP_HD(
+            "SELECT id, employee_id, rch_code, start_date, end_date \n" +
+                    "FROM ${supplySchema}.employee_temporary_rch \n" +
+                    "WHERE :effectiveDateTime BETWEEN start_date AND end_date"
+    ),
+    ACTIVE_TEMP_RESP_HD_BY_EMP(
+            ACTIVE_TEMP_RESP_HD.getSql() + " \n" +
+                    "AND employee_id = :empId"
+    ),
+    INSERT_TEMP_RESP_HD(
+            "INSERT INTO ${supplySchema}.employee_temporary_rch(employee_id, rch_code, start_date, end_date) \n" +
+                    "VALUES(:empId, :rchCode, :startDate, :endDate)"
+    ),
+    UPDATE_TEMP_RESP_HD(
+            "UPDATE ${supplySchema}.employee_temporary_rch \n" +
+                    "SET employee_id = :empId, \n" +
+                    "rch_code = :rchCode, \n" +
+                    "start_date = :startDate, \n" +
+                    "end_date = :endDate"
+    ),
+    DELETE_TEMP_RESP_HD(
+            "DELETE FROM ${supplySchema}.employee_temporary_rch \n" +
+                    "WHERE id = :id"
+    );
+
+    private String sql;
+
+    SqlTempResponsibilityHeadQuery(String sql) {
+        this.sql = sql;
+    }
+
+    @Override
+    public String getSql() {
+        return this.sql;
+    }
+
+    @Override
+    public DbVendor getVendor() {
+        return DbVendor.POSTGRES;
+    }
+}
