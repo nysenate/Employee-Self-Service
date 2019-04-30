73f03e40b0e3269c938639793f4d0e0f046f82f9
Sam Stouffer <samlthecaml@gmail.com>
2019-04-30 17:12:28 -0400

    Disable scheduler for tests.
    Disables the scheduler config for the test profile.  Moves the async
    config to a separate class so that is still allowed during tests.

--- src/main/java/gov/nysenate/ess/core/config/SchedulerConfig.java
+++ src/main/java/gov/nysenate/ess/core/config/SchedulerConfig.java
@@ -1,43 +1,25 @@
 package gov.nysenate.ess.core.config;
 
-import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
-import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
+@Profile("!test")
 @EnableScheduling
-@EnableAsync
 @Configuration
-public class SchedulerConfig implements SchedulingConfigurer, AsyncConfigurer
+public class SchedulerConfig implements SchedulingConfigurer
 {
-    @Override
-    @Bean(name = "essAsync", destroyMethod = "shutdown")
-    public Executor getAsyncExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(4);
-        executor.initialize();
-        return executor;
-    }
 
     @Bean(name = "essScheduler", destroyMethod = "shutdownNow")
     public ScheduledExecutorService schedulerThreadPool() {
         return Executors.newScheduledThreadPool(4, new CustomizableThreadFactory("scheduler"));
-    }
-
-    @Override
-    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-        return new SimpleAsyncUncaughtExceptionHandler();
     }
 
     @Override
