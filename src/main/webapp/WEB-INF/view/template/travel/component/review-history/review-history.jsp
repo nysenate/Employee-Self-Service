780f99d0f25a68a23da658b988fc3a27f9a3ae40
Kevin Caseiras <kcaseiras@gmail.com>
2019-06-10 11:30:48 -0400

    Add date filters to review-history page

--- src/main/webapp/WEB-INF/view/template/travel/component/review-history/review-history.jsp
+++ src/main/webapp/WEB-INF/view/template/travel/component/review-history/review-history.jsp
@@ -3,12 +3,28 @@
     <div class="travel-hero">
       <h2>Review History</h2>
     </div>
-    <div class="content-container content-controls">
-      <h4 class="travel-content-info travel-text-bold">Applications you have reviewed.</h4>
+
+    <div class="content-container travel-content-controls">
+      <h4 class="travel-content-info travel-text-bold" style="margin-bottom: 0px;">Search reviewed applications by date.</h4>
+      <div class="text-align-center">
+        <div class="padding-10 inline-block">
+          <label class="bold">From:</label>
+          <input datepicker readonly='true' id="dateFrom" style="margin-left: 1px;"
+                 ng-model="vm.date.from" to-date="vm.date.to"
+                 alt-filed=""
+                 ng-change="vm.applyFilters()"/>
+        </div>
+        <div class="padding-10 inline-block">
+          <label class="bold">To:</label>
+          <input datepicker readonly='true' id="dateTo" style="margin-left: 1px;"
+                 ng-model="vm.date.to" from-date="vm.date.from"
+                 ng-change="vm.applyFilters()"/>
+        </div>
+      </div>
     </div>
 
     <div ng-if="vm.data.isLoading === false">
-      <div ng-if="vm.data.apps.length === 0">
+      <div ng-if="vm.data.apps.filtered.length === 0">
         <div class="content-container">
           <div class="content-info">
             <h2 class="dark-gray">No Review History.</h2>
@@ -16,9 +32,9 @@
         </div>
       </div>
 
-      <div ng-if="vm.data.apps.length > 0">
+      <div ng-if="vm.data.apps.filtered.length > 0">
         <ess-app-summary-table
-            apps="vm.data.apps"
+            apps="vm.data.apps.filtered"
             on-row-click="vm.displayAppReviewViewModal(app)"
             show-status>
         </ess-app-summary-table>
--- src/main/webapp/assets/js/src/travel/component/review-history/review-history-ctrl.js
+++ src/main/webapp/assets/js/src/travel/component/review-history/review-history-ctrl.js
@@ -4,23 +4,33 @@ essTravel.controller('ReviewHistoryCtrl', ['$scope', 'LocationService', 'modals'
 
 function reviewHistory($scope, locationService, modals, appReviewApi) {
 
+    const DATEPICKER_FORMAT = "MM/DD/YYYY";
+    const ISO_FORMAT = "YYYY-MM-DD";
+
     var vm = this;
+    vm.data = {
+        isLoading: true,
+        appReviews: [],
+        apps: {
+            all: [],
+            filtered: []
+        }
+    };
+    vm.date = {
+        from: moment().subtract(3, 'month').format(DATEPICKER_FORMAT),
+        to: moment().add(3, 'month').format(DATEPICKER_FORMAT)
+    };
 
     (function () {
-        vm.data = {
-            isLoading: true,
-            appReviews: [],
-            apps: []
-        };
-
         appReviewApi.reviewHistory()
             .then(function (appReviews) {
                 appReviews.forEach(function (review) {
                     vm.data.appReviews.push(review);
-                    vm.data.apps.push(review.travelApplication);
+                    vm.data.apps.all.push(review.travelApplication);
                 });
+                vm.applyFilters();
                 vm.data.isLoading = false;
-            })
+            });
     })();
 
     vm.displayAppReviewViewModal = function (app) {
@@ -36,5 +46,13 @@ function reviewHistory($scope, locationService, modals, appReviewApi) {
     vm.onEdit = function (appReview) {
         modals.reject();
         locationService.go("/travel/application/edit", true, {appId: appReview.travelApplication.id});
-    }
+    };
+
+    vm.applyFilters = function () {
+        vm.data.apps.filtered = angular.copy(vm.data.apps.all);
+        vm.data.apps.filtered = vm.data.apps.filtered.filter(function (app) {
+            return moment(app.startDate, ISO_FORMAT) >= moment(vm.date.from, DATEPICKER_FORMAT) &&
+                moment(app.startDate, ISO_FORMAT) <= moment(vm.date.to, DATEPICKER_FORMAT);
+        });
+    };
 }
