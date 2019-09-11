6bcae6635235be8c37273a67f2a83dcd99796f36
Kevin Caseiras <kcaseiras@gmail.com>
2019-09-11 13:27:32 -0400

    Revert "Don't create application review records for v1.0 release"
    This reverts commit 0a7d69c16de3075c8310224b8fb75c78ec724471.

--- src/main/java/gov/nysenate/ess/travel/application/TravelApplicationService.java
+++ src/main/java/gov/nysenate/ess/travel/application/TravelApplicationService.java
@@ -3,6 +3,9 @@ package gov.nysenate.ess.travel.application;
 import gov.nysenate.ess.core.model.personnel.Employee;
 import gov.nysenate.ess.travel.application.allowances.mileage.MileagePerDiems;
 import gov.nysenate.ess.travel.application.route.Leg;
+import gov.nysenate.ess.travel.application.route.destination.Destination;
+import gov.nysenate.ess.travel.application.unsubmitted.UnsubmittedAppDao;
+import gov.nysenate.ess.travel.review.ApplicationReview;
 import gov.nysenate.ess.travel.review.ApplicationReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -42,9 +45,8 @@ public class TravelApplicationService {
     public TravelApplication submitTravelApplication(TravelApplication app, Employee submitter) {
         saveTravelApplication(app, submitter);
 
-        // Don't create application reviews for v1.0 release
-//        ApplicationReview appReview = appReviewService.createApplicationReview(app);
-//        appReviewService.saveApplicationReview(appReview);
+        ApplicationReview appReview = appReviewService.createApplicationReview(app);
+        appReviewService.saveApplicationReview(appReview);
         return app;
     }
 
