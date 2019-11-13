645e9ed8c58b67774840327c83d394967fb125e2
Sam Stouffer <samlthecaml@gmail.com>
2019-11-13 18:10:32 -0500

    Fix pec sequence migration... again

--- src/main/resources/sql/migrations/V20191108_1700__pec_fix_task_table_sequences.sql
+++ src/main/resources/sql/migrations/V20191108_1700__pec_fix_task_table_sequences.sql
@@ -7,15 +7,15 @@ ALTER COLUMN task_id DROP DEFAULT;
 
 DROP SEQUENCE IF EXISTS moodle_course_task_id_seq;
 
-SELECT setval('ess.personnel_task_task_id_seq', (SELECT COALESCE(MAX(task_id), 0) FROM personnel_task));
+SELECT setval('ess.personnel_task_task_id_seq', (SELECT COALESCE(MAX(task_id), 1) FROM personnel_task));
 
 ALTER SEQUENCE personnel_assigned_task_id_seq RENAME TO personnel_task_assignment_id_seq;
-SELECT setval('ess.personnel_task_assignment_id_seq', (SELECT COALESCE(MAX(id), 0) FROM personnel_task_assignment));
+SELECT setval('ess.personnel_task_assignment_id_seq', (SELECT COALESCE(MAX(id), 1) FROM personnel_task_assignment));
 
 ALTER SEQUENCE IF EXISTS policy_policy_id_seq RENAME TO ack_doc_ack_doc_id_seq;
 ALTER SEQUENCE IF EXISTS ack_doc_id_seq RENAME TO ack_doc_ack_doc_id_seq;
-SELECT setval('ess.ack_doc_ack_doc_id_seq', (SELECT COALESCE(MAX(ack_doc_id), 0) FROM ack_doc));
+SELECT setval('ess.ack_doc_ack_doc_id_seq', (SELECT COALESCE(MAX(ack_doc_id), 1) FROM ack_doc));
 
-SELECT setval('ess.pec_video_id_seq', (SELECT COALESCE(MAX(pec_video_id), 0) FROM pec_video));
+SELECT setval('ess.pec_video_id_seq', (SELECT COALESCE(MAX(pec_video_id), 1) FROM pec_video));
 
-SELECT setval('ess.pec_video_code_id_seq', (SELECT COALESCE(MAX(id), 0) FROM pec_video_code));
+SELECT setval('ess.pec_video_code_id_seq', (SELECT COALESCE(MAX(id), 1) FROM pec_video_code));
