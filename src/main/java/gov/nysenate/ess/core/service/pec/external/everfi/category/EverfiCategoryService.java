5ea5fc137c684d02ffd5112736c42f1ff485c477
aa29cala <aacal050@gmail.com>
2020-11-16 10:32:29 -0500

    Fix content null build issue
    Mvn verify could not build due to a content argument being null. THis
    has been corrected

--- src/main/java/gov/nysenate/ess/core/service/pec/external/everfi/category/EverfiCategoryService.java
+++ src/main/java/gov/nysenate/ess/core/service/pec/external/everfi/category/EverfiCategoryService.java
@@ -43,7 +43,6 @@ public class EverfiCategoryService {
         this.client = client;
         this.respHeadDao = respHeadDao;
         this.employeeInfoService = employeeInfoService;
-        loadCategories();
     }
 
     /**
