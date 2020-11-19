0b0f5bb161dab64aac2b526c28998a4218bee4ec
iaace-NA <jcheng@nysenate.gov>
2020-11-19 12:09:26 -0500

    Reset NewRequestCtrl values to 0

--- src/main/webapp/assets/js/src/time/timeoff/new-request-ctrl.js
+++ src/main/webapp/assets/js/src/time/timeoff/new-request-ctrl.js
@@ -1,7 +1,7 @@
 var essTimeOff = angular.module('essTime');
 
 essTimeOff.controller('NewRequestCtrl', ['$scope', '$http', function($scope, $http){
-    $scope.vac = 21;
-    $scope.sick = 482;
-    $scope.personal = 34;
+    $scope.vac = 0;
+    $scope.sick = 0;
+    $scope.personal = 0;
 }]);
