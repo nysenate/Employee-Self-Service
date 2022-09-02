f23befde4abc76fac2d119fe438eff7c9afd0aaf
Kevin Caseiras <kcaseiras@gmail.com>
2022-09-02 13:42:53 -0400

    Implement App navigation in top menu bar

--- src/main/webapp/WEB-INF/app/index.js
+++ src/main/webapp/WEB-INF/app/index.js
@@ -4,6 +4,7 @@ import MyInfo from "app/views/myinfo/MyInfo";
 import { BrowserRouter, NavLink } from "react-router-dom";
 import { ThemeContext, themes } from "app/contexts/ThemeContext";
 import { createRoot } from "react-dom/client";
+import EssNavBar from "app/shared/core/EssNavBar";
 
 
 function App() {
@@ -23,54 +24,3 @@ function App() {
 
 const root = createRoot(document.getElementById('app'))
 root.render(<App />)
-
-function EssNavBar() {
-  return (
-    <nav className="fixed h-[45px] w-full bg-gray-50 shadow">
-      <div className="w-[1150px] h-full mx-auto">
-        <div className="h-full flex justify-between items-center">
-          <div className="h-full ml-2">
-            <img src="/assets/img/nysslogo.png" alt="logo" className="inline-block h-[35px] w-[35px]"/>
-            <div className="ml-3 mr-6 inline-block">
-              <span className="text-lg font-medium">NYS</span>&nbsp;
-              <span className="text-lg font-light">ESS</span>
-            </div>
-            <ul className="inline-block h-full">
-              <li className="leading-[40px] inline-block h-full">
-                <AppLink name="My Info" to="myinfo"/>
-              </li>
-              <li className="leading-[40px] inline">
-                <AppLink name="Time & Attendance" to="time"/>
-              </li>
-              <li className="leading-[40px] inline">
-                <AppLink name="Supply" to="supply"/>
-              </li>
-              <li className="leading-[40px] inline">
-                <AppLink name="Travel" to="travel"/>
-              </li>
-            </ul>
-          </div>
-          <div>
-            Kevin
-          </div>
-        </div>
-      </div>
-    </nav>
-  )
-}
-
-function AppLink({ to, name }) {
-  const themeColor = '#175B81'
-  const classes = "px-6"
-  const activeClasses = "border-b-[3px] border-blue-700 inline-block h-full"
-  return (
-    <NavLink
-      to={to}
-      className={({ isActive }) =>
-        isActive ? `${classes} ${activeClasses}` : `${classes}`
-      }
-    >
-      {name}
-    </NavLink>
-  )
-}
---
+++ src/main/webapp/WEB-INF/app/shared/core/EssNavBar.js
@@ -0,0 +1,82 @@
+import { NavLink } from "react-router-dom";
+import React from "react";
+import './essNavBar.css'
+import { themes } from "app/contexts/ThemeContext";
+
+
+export default function EssNavBar() {
+  return (
+    <nav className="fixed h-[45px] w-full bg-gray-50 shadow">
+      <div className="w-[1150px] h-full mx-auto">
+        <div className="h-full flex justify-between items-center">
+          <div className="flex h-full items-center ml-2">
+            <img src="/assets/img/nysslogo.png" alt="logo" className="h-[35px] w-[35px]"/>
+            <div className="ml-3 mr-6 inline-block">
+              <span className="text-lg font-medium text-[20.8px]">NYS</span>&nbsp;
+              <span className="text-lg font-light text-[20.8px]">ESS</span>
+            </div>
+            <ul className="h-full">
+              <li className="leading-[40px] inline">
+                <AppLink name="My Info" to="myinfo" theme={themes.myinfo}/>
+              </li>
+              <li className="leading-[40px] inline">
+                <AppLink name="Time & Attendance" to="time" theme={themes.time}/>
+              </li>
+              <li className="leading-[40px] inline">
+                <AppLink name="Supply" to="supply" theme={themes.supply}/>
+              </li>
+              <li className="leading-[40px] inline">
+                <AppLink name="Travel" to="travel" theme={themes.travel}/>
+              </li>
+            </ul>
+          </div>
+          <div>
+            Sign Out
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+function AppLink({ to, name, theme }) {
+  let themeBorder
+  let themeText
+
+  switch (theme) {
+    case themes.myinfo:
+      themeBorder = "border-green-600"
+      themeText = "hover:text-green-600"
+      break
+    case themes.time:
+      themeBorder = "border-teal-600"
+      themeText = "hover:text-teal-600"
+      break
+    case themes.supply:
+      themeBorder = "border-purple-600"
+      themeText = "hover:text-purple-600"
+      break
+    case themes.travel:
+      themeBorder = "border-orange-600"
+      themeText = "hover:text-orange-600"
+      break
+    default:
+      themeBorder = "border-gray-700"
+      themeText = "hover:text-gray-700"
+  }
+
+  const classes = `text-[14.3px] inline-block h-full px-5 mx-0.5 border-0 hover:border-b-[3px] ${themeBorder} ${themeText}`
+  const activeClasses = `font-semibold border-b-[3px]`
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        isActive ? `${classes} ${activeClasses}` : `${classes}`
+      }
+    >
+      <span className="app-link inline-block" title={name}>
+      {name}
+      </span>
+    </NavLink>
+  )
+}
