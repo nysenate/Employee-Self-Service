259584dc84c208d256df1c7253ab72fdb9157fc4
Jacob Keegan <jacobmkeegan@gmail.com>
2023-10-04 17:15:19 -0400

    Donation data now excludes inactive donation entries.

--- src/main/java/gov/nysenate/ess/time/dao/accrual/SqlDonationQuery.java
+++ src/main/java/gov/nysenate/ess/time/dao/accrual/SqlDonationQuery.java
@@ -8,7 +8,8 @@ public enum SqlDonationQuery implements BasicSqlQuery {
     SELECT_DONATIONS_IN_RANGE("""
             SELECT NUTIMEADJ AS donationHours, DTEFFECT AS donationDate
             FROM SASS_OWNER.PM23TIMEPOOL
-            WHERE :startDate <= DTEFFECT AND DTEFFECT <= :endDate AND NUXREFEM = :empId"""
+            WHERE :startDate <= DTEFFECT AND DTEFFECT <= :endDate AND NUXREFEM = :empId
+                AND CDSTATUS = 'A'"""
     ),
 
     GET_SICK_TYPE_ID("""
