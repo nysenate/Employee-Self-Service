ec33fac7f3f100074be9c17495d7cd0e9e51a98f
Jacob Keegan <jacobmkeegan@gmail.com>
2023-11-08 16:10:49 -0500

    Cleanup of some (perhaps unused) email classes.

--- src/main/java/gov/nysenate/ess/core/service/notification/base/message/component/UTF8PaintText.java
+++ src/main/java/gov/nysenate/ess/core/service/notification/base/message/component/UTF8PaintText.java
@@ -2,7 +2,6 @@ package gov.nysenate.ess.core.service.notification.base.message.component;
 
 import gov.nysenate.ess.core.service.notification.base.message.base.Text;
 
-import java.awt.*;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -10,38 +9,16 @@ import java.nio.charset.StandardCharsets;
  * Created by Chenguang He on 6/14/2016.
  */
 public abstract class UTF8PaintText implements Text {
-    public StringBuilder path = new StringBuilder(Text.path).append(".")
-            .append(UTF8PaintText.class.getSimpleName());
-
     private final String content;
-    private final Color color;
 
-    /**
-     * the constructor
-     *
-     * @param color   the color
-     * @param content the content
-     */
-    public UTF8PaintText(Color color, String content) {
-        this.color = color;
+    public UTF8PaintText(String content) {
         this.content = content;
         StandardCharsets.UTF_8.encode(content);
-    }
-
-    @Override
-    public String getEncoding() {
-        return "utf-8";
     }
 
 
     @Override
     public String getContent() {
         return content;
-    }
-
-
-    @Override
-    public Color getColor() {
-        return color;
     }
 }
