nysenate/Employee-Self-Service

commit d607c0a829358bc3678ecab604708c98e2732c6f

Verify PEC task id with ethics live codes
The task id was not being verified when submitting codes.
This has been corrected and it stops employees from submitting codes from a previous task
for the current task

src/main/java/gov/nysenate/ess/core/controller/api/PECCodeApiCtrl.java
@@ -3,7 +3,7 @@ package gov.nysenate.ess.core.controller.api;
 import gov.nysenate.ess.core.client.response.base.SimpleResponse;
 import gov.nysenate.ess.core.client.response.error.ErrorCode;
 import gov.nysenate.ess.core.client.response.error.ErrorResponse;
-import gov.nysenate.ess.core.client.view.pec.video.PECVideoCodeSubmission;
+import gov.nysenate.ess.core.client.view.pec.video.PECCodeSubmission;
 import gov.nysenate.ess.core.dao.pec.assignment.PersonnelTaskAssignmentDao;
 import gov.nysenate.ess.core.dao.pec.task.detail.EthicsLiveCourseTaskDetailDao;
 import gov.nysenate.ess.core.dao.pec.task.detail.VideoTaskDetailDao;
@@ -12,7 +12,7 @@ import gov.nysenate.ess.core.model.auth.CorePermissionObject;
 import gov.nysenate.ess.core.model.base.InvalidRequestParamEx;
 import gov.nysenate.ess.core.model.pec.PersonnelTask;
 import gov.nysenate.ess.core.model.pec.ethics.EthicsLiveCourseTask;
-import gov.nysenate.ess.core.model.pec.video.IncorrectPECVideoCodeEx;
+import gov.nysenate.ess.core.model.pec.video.IncorrectPECCodeEx;
 import gov.nysenate.ess.core.model.pec.video.VideoTask;
 import gov.nysenate.ess.core.service.pec.notification.PECNotificationService;
 import gov.nysenate.ess.core.service.pec.task.PersonnelCodeGenerationService;
@@ -27,8 +27,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.sql.Timestamp;
 import java.util.List;
 
 import static gov.nysenate.ess.core.model.pec.PersonnelTaskType.ETHICS_LIVE_COURSE;
@@ -138,12 +136,12 @@ public class PECCodeApiCtrl extends BaseRestApiCtrl {
      * (POST)    /api/v1/personnel/task/ethics/live/code
      *
      * Request body:
-     * @param submission {@link PECVideoCodeSubmission}
+     * @param submission {@link PECCodeSubmission}
      *
      * @return {@link SimpleResponse} if successful
      */
     @RequestMapping(value = "/ethics/live/code", method = POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public SimpleResponse submitEthicsLiveCourseCodes(@RequestBody PECVideoCodeSubmission submission) {
+    public SimpleResponse submitEthicsLiveCourseCodes(@RequestBody PECCodeSubmission submission) {
         checkPermission(new CorePermission(submission.getEmpId(), CorePermissionObject.PERSONNEL_TASK, POST));
 
         ensureEmpIdExists(submission.getEmpId(), "empId");
@@ -152,7 +150,7 @@ public class PECCodeApiCtrl extends BaseRestApiCtrl {
 
         EthicsLiveCourseTask ethicsLiveCourseTask = (EthicsLiveCourseTask) getEthicsLiveCourseFromIdParams(submission.getTaskId(), "taskId");
 
-        personnelCodeVerificationService.verifyDateRangedEthics(submission.getCodes(),trainingDate);
+        personnelCodeVerificationService.verifyDateRangedEthics(submission, trainingDate);
         int authenticatedEmpId = ShiroUtils.getAuthenticatedEmpId();
         assignedTaskDao.setTaskComplete(submission.getEmpId(), ethicsLiveCourseTask.getTaskId(), authenticatedEmpId);
         pecNotificationService.sendCompletionEmail(submission.getEmpId(), ethicsLiveCourseTask);
@@ -169,12 +167,12 @@ public class PECCodeApiCtrl extends BaseRestApiCtrl {
      * (POST)    /api/v1/personnel/task/video/code
      *
      * Request body:
-     * @param submission {@link PECVideoCodeSubmission}
+     * @param submission {@link PECCodeSubmission}
      *
      * @return {@link SimpleResponse} if successful
      */
     @RequestMapping(value = "/video/code", method = POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public SimpleResponse submitVideoCodes(@RequestBody PECVideoCodeSubmission submission) {
+    public SimpleResponse submitVideoCodes(@RequestBody PECCodeSubmission submission) {
         checkPermission(new CorePermission(submission.getEmpId(), CorePermissionObject.PERSONNEL_TASK, POST));
 
         ensureEmpIdExists(submission.getEmpId(), "empId");
@@ -193,13 +191,13 @@ public class PECCodeApiCtrl extends BaseRestApiCtrl {
     /**
      * Handles submission of incorrect codes by returning a special error response.
      *
-     * @param ex {@link IncorrectPECVideoCodeEx}
+     * @param ex {@link IncorrectPECCodeEx}
      * @return {@link ErrorResponse}
      */
-    @ExceptionHandler(IncorrectPECVideoCodeEx.class)
+    @ExceptionHandler(IncorrectPECCodeEx.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public ErrorResponse handleIncorrectCode(IncorrectPECVideoCodeEx ex) {
+    public ErrorResponse handleIncorrectCode(IncorrectPECCodeEx ex) {
         return new ErrorResponse(ErrorCode.INVALID_PEC_CODE);
     }
 

src/main/java/gov/nysenate/ess/core/model/pec/ethics/EthicsLiveCourseTask.java
@@ -2,7 +2,7 @@ package gov.nysenate.ess.core.model.pec.ethics;
 
 import com.google.common.collect.ImmutableList;
 import gov.nysenate.ess.core.model.pec.PersonnelTask;
-import gov.nysenate.ess.core.model.pec.video.IncorrectPECVideoCodeEx;
+import gov.nysenate.ess.core.model.pec.video.IncorrectPECCodeEx;
 import gov.nysenate.ess.core.model.pec.video.VideoTaskCode;
 
 import java.net.MalformedURLException;
@@ -40,16 +40,16 @@ public class EthicsLiveCourseTask extends PersonnelTask {
      * Verify the given codes, throwing an exception if they don't match the codes for this video
      *
      * @param codeSubmission List<String>
-     * @throws IncorrectPECVideoCodeEx if the codes are wrong.
+     * @throws IncorrectPECCodeEx if the codes are wrong.
      */
-    public void verifyCodes(List<String> codeSubmission) throws IncorrectPECVideoCodeEx {
+    public void verifyCodes(List<String> codeSubmission) throws IncorrectPECCodeEx {
         List<String> expectedCodes = codes.stream()
                 .sorted()
                 .map(VideoTaskCode::getCode)
                 .collect(toList());
 
         if (!expectedCodes.equals(codeSubmission)) {
-            throw new IncorrectPECVideoCodeEx();
+            throw new IncorrectPECCodeEx();
         }
     }
 

src/main/java/gov/nysenate/ess/core/model/pec/ethics/IncorrectPECCodeAmountEx.java
@@ -1,0 +1,7 @@
+package gov.nysenate.ess.core.model.pec.ethics;
+
+public class IncorrectPECCodeAmountEx extends RuntimeException {
+    public IncorrectPECCodeAmountEx() {
+        super("The incorrect number of codes were submitted for a PEC task");
+    }
+}

src/main/java/gov/nysenate/ess/core/model/pec/video/IncorrectPECCodeEx.java
@@ -1,0 +1,10 @@
+package gov.nysenate.ess.core.model.pec.video;
+
+/**
+ * Exception thrown when incorrect video codes are submitted for a {@link VideoTask}
+ */
+public class IncorrectPECCodeEx extends RuntimeException {
+    public IncorrectPECCodeEx() {
+        super("The submitted codes were incorrect for a PEC task");
+    }
+}

src/main/java/gov/nysenate/ess/core/model/pec/video/VideoTask.java
@@ -36,15 +36,15 @@ public class VideoTask extends PersonnelTask {
      * Verify the given codes, throwing an exception if they don't match the codes for this video
      *
      * @param codeSubmission List<String>
-     * @throws IncorrectPECVideoCodeEx if the codes are wrong.
+     * @throws IncorrectPECCodeEx if the codes are wrong.
      */
-    public void verifyCodes(List<String> codeSubmission) throws IncorrectPECVideoCodeEx {
+    public void verifyCodes(List<String> codeSubmission) throws IncorrectPECCodeEx {
         List<String> expectedCodes = codes.stream()
                 .sorted()
                 .map(VideoTaskCode::getCode).toList();
 
         if (!expectedCodes.equals(codeSubmission)) {
-            throw new IncorrectPECVideoCodeEx();
+            throw new IncorrectPECCodeEx();
         }
     }
 

src/main/java/gov/nysenate/ess/core/service/pec/task/PersonnelCodeVerificationService.java
@@ -2,9 +2,11 @@ package gov.nysenate.ess.core.service.pec.task;
 
 
 
+import gov.nysenate.ess.core.client.view.pec.video.PECCodeSubmission;
 import gov.nysenate.ess.core.dao.pec.task.PersonnelTaskDao;
 import gov.nysenate.ess.core.model.pec.ethics.DateRangedEthicsCode;
-import gov.nysenate.ess.core.model.pec.video.IncorrectPECVideoCodeEx;
+import gov.nysenate.ess.core.model.pec.ethics.IncorrectPECCodeAmountEx;
+import gov.nysenate.ess.core.model.pec.ethics.IncorrectPECEthicsCodeEx;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,17 +24,32 @@ public class PersonnelCodeVerificationService {
         this.personnelTaskDao = personnelTaskDao;
     }
 
-    public void verifyDateRangedEthics(List<String> codeSubmission, String dateInput)throws IncorrectPECVideoCodeEx {
+    public void verifyDateRangedEthics(PECCodeSubmission codeSubmission, String dateInput)
+            throws IncorrectPECCodeAmountEx, IncorrectPECEthicsCodeEx {
         //History of codes
         List<DateRangedEthicsCode> codeList = personnelTaskDao.getEthicsCodes();
         Collections.sort(codeList);
+        //error out if missing codes
+        if (codeSubmission.getCodes().isEmpty() || codeSubmission.getCodes().size() < 2) {
+            throw new IncorrectPECCodeAmountEx();
+        }
 
         //User submitted codes
-        String code1 = codeSubmission.get(0).toUpperCase();
-        String code2 = codeSubmission.get(1).toUpperCase();
+        String code1 = codeSubmission.getCodes().get(0).toUpperCase();
+        String code2 = codeSubmission.getCodes().get(1).toUpperCase();
         LocalDateTime parsedDate = LocalDateTime.parse(dateInput.substring(0, 19));
         LocalDate codeDate = parsedDate.toLocalDate();
+        //Submitted Task Id
+        int submittedTaskId = codeSubmission.getTaskId();
         int matchedEntries = 0;
+
+        //Attempt to match submitted task id to the ethics code id
+        Integer confirmedEthicsCodeId = personnelTaskDao.getEthicsCodeId(submittedTaskId);
+
+        //Cant confirm task
+        if (confirmedEthicsCodeId == null) {
+            throw new IncorrectPECEthicsCodeEx();
+        }
 
         //Cycle thru the history of codes to see if 2 codes match and are within the correct date range
         for (DateRangedEthicsCode drec : codeList) {
@@ -44,11 +61,11 @@ public class PersonnelCodeVerificationService {
             LocalDate drecStartDate = drec.getStartDate().toLocalDate();
             LocalDate drecEndDate = drec.getEndDate().toLocalDate();
 
-            if (drec.getCode().equals(code1) && ( (drecStartDate.isBefore(codeDate) && drecEndDate.isAfter(codeDate))
+            if (drec.getEthicsCodeId() == confirmedEthicsCodeId && drec.getCode().equals(code1) && ( (drecStartDate.isBefore(codeDate) && drecEndDate.isAfter(codeDate))
                     || drecStartDate.equals(codeDate) || drecEndDate.equals(codeDate) ) ) {
                 matchedEntries++;
             }
-            else if (drec.getCode().equals(code2) && ( (drecStartDate.isBefore(codeDate) && drecEndDate.isAfter(codeDate))
+            else if (drec.getEthicsCodeId() == confirmedEthicsCodeId && drec.getCode().equals(code2) && ( (drecStartDate.isBefore(codeDate) && drecEndDate.isAfter(codeDate))
                     || drecStartDate.equals(codeDate) || drecEndDate.equals(codeDate) ) ) {
                 matchedEntries++;
             }
@@ -56,7 +73,7 @@ public class PersonnelCodeVerificationService {
 
         //1 or more incorrect codes so error out
         if (matchedEntries < 2) {
-            throw new IncorrectPECVideoCodeEx();
+            throw new IncorrectPECEthicsCodeEx();
         }
 
     }

src/main/java/gov/nysenate/ess/core/client/view/pec/video/PECCodeSubmission.java
@@ -5,7 +5,7 @@ import java.util.List;
 /**
  * Structure for the request body of a video code submission.
  */
-public class PECVideoCodeSubmission {
+public class PECCodeSubmission {
 
     private int empId;
     private int taskId;
@@ -13,7 +13,7 @@ public class PECVideoCodeSubmission {
 
     private String trainingDate;
 
-    private PECVideoCodeSubmission() {}
+    private PECCodeSubmission() {}
 
     public int getEmpId() {
         return empId;

src/main/java/gov/nysenate/ess/core/dao/pec/task/SqlPersonnelTaskDao.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.List;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import static gov.nysenate.ess.core.dao.pec.task.SqlPersonnelTaskQuery.*;
@@ -141,7 +140,7 @@ public class SqlPersonnelTaskDao extends SqlBaseDao implements PersonnelTaskDao 
         localNamedJdbc.update(INSERT_ETHICS_CODE.getSql(schemaMap()), updateParams);
     }
 
-    public List<DateRangedEthicsCode> getEthicsCodes(){
+    public List<DateRangedEthicsCode> getEthicsCodes() {
         return localNamedJdbc.query(SELECT_ETHICS_CODES.getSql(schemaMap()), ethicsCodeRowMapper);
     }
 

commit 3c0181415d086a3326aec1d37be67393472520c3

Fix bug introduced in commit d607c0a829358bc3678ecab604708c98e2732c6f that causes an incorrect PEC code submission to throw an unhandled exception (which displays the ESS error dialog).  This was due to a mismatch between the exception class name in the exception handler and the exception class name that is thrown in the verification method.  The various PEC code verification exceptions have now been consolidated into a single exception.

src/main/java/gov/nysenate/ess/core/controller/api/PECCodeApiCtrl.java
@@ -10,9 +10,9 @@ import gov.nysenate.ess.core.dao.pec.task.detail.VideoTaskDetailDao;
 import gov.nysenate.ess.core.model.auth.CorePermission;
 import gov.nysenate.ess.core.model.auth.CorePermissionObject;
 import gov.nysenate.ess.core.model.base.InvalidRequestParamEx;
+import gov.nysenate.ess.core.model.pec.IncorrectCodeException;
 import gov.nysenate.ess.core.model.pec.PersonnelTask;
 import gov.nysenate.ess.core.model.pec.ethics.EthicsLiveCourseTask;
-import gov.nysenate.ess.core.model.pec.video.IncorrectPECCodeEx;
 import gov.nysenate.ess.core.model.pec.video.VideoTask;
 import gov.nysenate.ess.core.service.pec.notification.PECNotificationService;
 import gov.nysenate.ess.core.service.pec.task.PersonnelCodeGenerationService;
@@ -185,13 +185,13 @@ public class PECCodeApiCtrl extends BaseRestApiCtrl {
     /**
      * Handles submission of incorrect codes by returning a special error response.
      *
-     * @param ex {@link IncorrectPECCodeEx}
+     * @param ex {@link IncorrectCodeException}
      * @return {@link ErrorResponse}
      */
-    @ExceptionHandler(IncorrectPECCodeEx.class)
+    @ExceptionHandler(IncorrectCodeException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public ErrorResponse handleIncorrectCode(IncorrectPECCodeEx ex) {
+    public ErrorResponse handleIncorrectCode(IncorrectCodeException ex) {
         return new ErrorResponse(ErrorCode.INVALID_PEC_CODE);
     }
 

src/main/java/gov/nysenate/ess/core/model/pec/IncorrectCodeException.java
@@ -1,0 +1,14 @@
+package gov.nysenate.ess.core.model.pec;
+
+/**
+ * Exception thrown when incorrect verification codes are submitted for a task.
+ */
+public class IncorrectCodeException extends RuntimeException {
+    public IncorrectCodeException(String msg) {
+        super(msg);
+    }
+
+    public IncorrectCodeException() {
+        super("The submitted codes were incorrect for this task");
+    }
+}

src/main/java/gov/nysenate/ess/core/model/pec/ethics/EthicsLiveCourseTask.java
@@ -1,8 +1,8 @@
 package gov.nysenate.ess.core.model.pec.ethics;
 
 import com.google.common.collect.ImmutableList;
+import gov.nysenate.ess.core.model.pec.IncorrectCodeException;
 import gov.nysenate.ess.core.model.pec.PersonnelTask;
-import gov.nysenate.ess.core.model.pec.video.IncorrectPECCodeEx;
 import gov.nysenate.ess.core.model.pec.video.VideoTaskCode;
 
 import java.net.MalformedURLException;
@@ -40,16 +40,16 @@ public class EthicsLiveCourseTask extends PersonnelTask {
      * Verify the given codes, throwing an exception if they don't match the codes for this video
      *
      * @param codeSubmission List<String>
-     * @throws IncorrectPECCodeEx if the codes are wrong.
+     * @throws IncorrectCodeException if the codes are wrong.
      */
-    public void verifyCodes(List<String> codeSubmission) throws IncorrectPECCodeEx {
+    public void verifyCodes(List<String> codeSubmission) throws IncorrectCodeException {
         List<String> expectedCodes = codes.stream()
                 .sorted()
                 .map(VideoTaskCode::getCode)
                 .collect(toList());
 
         if (!expectedCodes.equals(codeSubmission)) {
-            throw new IncorrectPECCodeEx();
+            throw new IncorrectCodeException();
         }
     }
 

src/main/java/gov/nysenate/ess/core/model/pec/ethics/IncorrectPECCodeAmountEx.java
@@ -1,7 +1,0 @@
-package gov.nysenate.ess.core.model.pec.ethics;
-
-public class IncorrectPECCodeAmountEx extends RuntimeException {
-    public IncorrectPECCodeAmountEx() {
-        super("The incorrect number of codes were submitted for a PEC task");
-    }
-}

src/main/java/gov/nysenate/ess/core/model/pec/ethics/IncorrectPECEthicsCodeEx.java
@@ -1,7 +1,0 @@
-package gov.nysenate.ess.core.model.pec.ethics;
-
-public class IncorrectPECEthicsCodeEx extends RuntimeException {
-    public IncorrectPECEthicsCodeEx() {
-        super("The submitted codes were incorrect for an Ethics task");
-    }
-}

src/main/java/gov/nysenate/ess/core/model/pec/video/IncorrectPECCodeEx.java
@@ -1,10 +1,0 @@
-package gov.nysenate.ess.core.model.pec.video;
-
-/**
- * Exception thrown when incorrect video codes are submitted for a {@link VideoTask}
- */
-public class IncorrectPECCodeEx extends RuntimeException {
-    public IncorrectPECCodeEx() {
-        super("The submitted codes were incorrect for a PEC task");
-    }
-}

src/main/java/gov/nysenate/ess/core/model/pec/video/VideoTask.java
@@ -1,6 +1,7 @@
 package gov.nysenate.ess.core.model.pec.video;
 
 import com.google.common.collect.ImmutableList;
+import gov.nysenate.ess.core.model.pec.IncorrectCodeException;
 import gov.nysenate.ess.core.model.pec.PersonnelTask;
 
 import java.util.Collection;
@@ -36,15 +37,15 @@ public class VideoTask extends PersonnelTask {
      * Verify the given codes, throwing an exception if they don't match the codes for this video
      *
      * @param codeSubmission List<String>
-     * @throws IncorrectPECCodeEx if the codes are wrong.
+     * @throws IncorrectCodeException if the codes are wrong.
      */
-    public void verifyCodes(List<String> codeSubmission) throws IncorrectPECCodeEx {
+    public void verifyCodes(List<String> codeSubmission) throws IncorrectCodeException {
         List<String> expectedCodes = codes.stream()
                 .sorted()
                 .map(VideoTaskCode::getCode).toList();
 
         if (!expectedCodes.equals(codeSubmission)) {
-            throw new IncorrectPECCodeEx();
+            throw new IncorrectCodeException();
         }
     }
 

src/main/java/gov/nysenate/ess/core/service/pec/task/PersonnelCodeVerificationService.java
@@ -2,9 +2,8 @@ package gov.nysenate.ess.core.service.pec.task;
 
 import gov.nysenate.ess.core.client.view.pec.video.PECCodeSubmission;
 import gov.nysenate.ess.core.dao.pec.task.PersonnelTaskDao;
+import gov.nysenate.ess.core.model.pec.IncorrectCodeException;
 import gov.nysenate.ess.core.model.pec.ethics.DateRangedEthicsCode;
-import gov.nysenate.ess.core.model.pec.ethics.IncorrectPECCodeAmountEx;
-import gov.nysenate.ess.core.model.pec.ethics.IncorrectPECEthicsCodeEx;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,15 +21,14 @@ public class PersonnelCodeVerificationService {
         this.personnelTaskDao = personnelTaskDao;
     }
 
-    public void verifyDateRangedEthics(PECCodeSubmission codeSubmission)
-                throws IncorrectPECCodeAmountEx, IncorrectPECEthicsCodeEx {
+    public void verifyDateRangedEthics(PECCodeSubmission codeSubmission) throws IncorrectCodeException {
         //History of codes
         List<DateRangedEthicsCode> codeList = personnelTaskDao.getEthicsCodes();
         Collections.sort(codeList);
 
         //error out if missing codes
         if (codeSubmission.getCodes().isEmpty() || codeSubmission.getCodes().size() < 2) {
-            throw new IncorrectPECCodeAmountEx();
+            throw new IncorrectCodeException("A minimum of two codes must be submitted");
         }
 
         //User submitted codes and training date (converted to Epoch days)
@@ -42,9 +40,9 @@ public class PersonnelCodeVerificationService {
         //Attempt to match submitted task id to the ethics code id
         Integer confirmedEthicsCodeId = personnelTaskDao.getEthicsCodeId(codeSubmission.getTaskId());
 
-        //Cant confirm task
+        // Unable to reference the EthicsCodeId using the TaskId
         if (confirmedEthicsCodeId == null) {
-            throw new IncorrectPECEthicsCodeEx();
+            throw new IncorrectCodeException("Unable to reference the Ethics Code ID for this task");
         }
 
         //Cycle thru the history of codes to see if 2 codes that are associated
@@ -66,7 +64,7 @@ public class PersonnelCodeVerificationService {
 
         // Must match both codes.  If not, then throw an error.
         if (matchedEntries < 2) {
-            throw new IncorrectPECEthicsCodeEx();
+            throw new IncorrectCodeException();
         }
 
     }