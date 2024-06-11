8976c29284999ca610943446afccf5c05f4e7b2f
Ethan <eapresco@gmail.com>
2024-06-11 15:44:17 -0400

    First Assign Task Push

--- src/main/webapp/assets/js/src/myinfo/myinfo-ctrl.js
+++ src/main/webapp/assets/js/src/myinfo/myinfo-ctrl.js
@@ -19,7 +19,6 @@
             function setCount(assignments) {
                 var count = assignments
                     .filter(function (assignment) {
-                        console.log(assignment);
                         return assignment.hasOwnProperty('completed') && !assignment.completed && assignment.active
                             && assignment.task.active
                     })
