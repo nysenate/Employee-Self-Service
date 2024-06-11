7c58f84abb031768888b3abb0d88f3ef03905449
Yuerden <john.cohen.work@gmail.com>
2024-06-11 11:14:20 -0400

    asdf

--- src/main/webapp/WEB-INF/app/views/time/accruals/SickLeaveDonation.js
+++ src/main/webapp/WEB-INF/app/views/time/accruals/SickLeaveDonation.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import Hero from "../../../components/Hero";
 
+// const Donation = (amount, setAmount) => ();
+
 export default function SickLeaveDonation() {
+    // const [amount, setAmount]
     return (
         <div>
             <Hero>Sick Leave Donation</Hero>
