10cbcd9b1683881277a6c0c7de0725fdab13a476
Yuerden <john.cohen.work@gmail.com>
2024-06-14 12:12:45 -0400

    loading + pagin separation

--- src/main/webapp/WEB-INF/app/views/supply/requisition/RequisitionFormIndex.js
+++ src/main/webapp/WEB-INF/app/views/supply/requisition/RequisitionFormIndex.js
@@ -4,6 +4,8 @@ import styles from './RequisitionFormIndex.module.css';
 import { Button } from "../../../components/Button";
 import { fetchApiJson } from "app/utils/fetchJson";
 import useAuth from "app/contexts/Auth/useAuth";
+import LoadingIndicator from "app/components/LoadingIndicator";
+import Pagination from "./Pagination";
 
 const SelectDestination = ({ locations, tempDestination, handleTempDestinationChange, handleConfirmClick }) => {
   return (
@@ -16,7 +18,7 @@ const SelectDestination = ({ locations, tempDestination, handleTempDestinationCh
           onChange={handleTempDestinationChange}
         >
           <option value="">Select Destination</option>
-          {locations.map((location) => (
+          {locations && locations.map((location) => (
             <option key={location.locId} value={location.locId}>
               {location.code} ({location.locationDescription})
             </option>
@@ -28,13 +30,17 @@ const SelectDestination = ({ locations, tempDestination, handleTempDestinationCh
   );
 };
 
-const DestinationDetails = ({ destination, handleChangeClick, items }) => {
+const DestinationDetails = ({ locations, destination, handleChangeClick, items, handleSortChange, sortOption }) => {
+  const location = locations?.find(loc => loc.locId === destination);
+
+  if (!location) return <div>Location not found</div>;
+
   return (
     <div className={styles.destinationDetails}>
       <div className={styles.detailsRow}>
         <div className={styles.destinationInfo}>
           <span>Destination: </span>
-          <span style={{ marginLeft: '10px', color: 'black' }}>{destination}</span>
+          <span style={{ marginLeft: '10px', color: 'black' }}>{location.code} ({location.locationDescription})</span>
           <button onClick={handleChangeClick} className={styles.changeButton}>[change]</button>
         </div>
         <div className={styles.searchBar}>
@@ -44,7 +50,7 @@ const DestinationDetails = ({ destination, handleChangeClick, items }) => {
         </div>
         <div className={styles.sortBy}>
           <label>Sort By:</label>
-          <select className={styles.sortSelect}>
+          <select className={styles.sortSelect} value={sortOption} onChange={handleSortChange}>
             <option value="name">Name</option>
             <option value="category">Category</option>
           </select>
@@ -56,11 +62,11 @@ const DestinationDetails = ({ destination, handleChangeClick, items }) => {
 
 const ItemsGrid = ({ items, currentPage, itemsPerPage }) => {
   const startIndex = (currentPage - 1) * itemsPerPage;
-  const currentItems = items.slice(startIndex, startIndex + itemsPerPage);
+  const currentItems = items?.slice(startIndex, startIndex + itemsPerPage);
 
   return (
     <div className={styles.itemGrid}>
-      {currentItems.map((item) => (
+      {currentItems?.map((item) => (
         <ItemDisplay key={item.id} item={item} />
       ))}
     </div>
@@ -74,150 +80,78 @@ const ItemDisplay = ({ item }) => {
         <img
           src={`/assets/supply_photos/${item.commodityCode}.jpg`}
           alt={item.description}
-          width="160"
           height="120"
         />
       </div>
       <div className={styles.itemDescription}>
         <h4>{item.description}</h4>
-        <p>{item.packageSize}</p>
-      </div>
-      <div className={styles.itemPrice}>
-        <p>{item.price}</p>
+        <p>{item.unit}</p>
       </div>
       <Button>Add to Cart</Button>
     </div>
   );
-}
-
-const Pagination = ({ currentPage, totalPages, onPageChange, top }) => {
-  const calculatePageNumbers = (currentPage, totalPages) => {
-    let pageNumbers = [];
-    pageNumbers.push(1);
-
-    if (currentPage > 5 && totalPages>9) {
-      pageNumbers.push('...');
-    }
-
-    let dl = Math.abs(currentPage - 1) - 4;
-    let dr = Math.abs(currentPage - totalPages) - 4;
-    let rangeStart = currentPage - 3;
-
-    console.log("currentPage", currentPage);
-    console.log("dl", dl);
-    console.log("dr", dr);
-
-    if (dl < 0) {
-      rangeStart = currentPage -3 - dl;
-      console.log("rangeStart: ", rangeStart, " = ", currentPage, " -3 - ", dl)
-    }
-    if (dr < 0) {
-      rangeStart = currentPage -3 + dr;
-    }
-
-    for (let i = rangeStart; i <= rangeStart + 6 && i < totalPages; i++) {
-      if (i > 1 && i < totalPages) {
-        pageNumbers.push(i);
-      }
-    }
-
-    if (currentPage < totalPages - 4 && totalPages>9) {
-      pageNumbers.push('...');
-    }
-    pageNumbers.push(totalPages);
-
-    return pageNumbers;
-  };
-
-  const pageNumbers = calculatePageNumbers(currentPage, totalPages);
-
-  return (
-    <div className={`${styles.pagination} ${top ? styles.paginationTop : styles.paginationBottom}`}>
-      <button
-        className={styles.pageButton}
-        onClick={() => onPageChange(1)}
-        disabled={currentPage === 1}
-      >
-        &laquo;
-      </button>
-      <button
-        className={styles.pageButton}
-        onClick={() => onPageChange(currentPage - 1)}
-        disabled={currentPage === 1}
-      >
-        &lsaquo;
-      </button>
-      {pageNumbers.map((number, index) => (
-        <button
-          key={index}
-          className={`${styles.pageButton} ${number === currentPage ? styles.activePage : ''}`}
-          onClick={() => (number >= 1 && number <= totalPages) ? onPageChange(number) : console.log("nah")}
-        >
-          {number}
-        </button>
-      ))}
-      <button
-        className={styles.pageButton}
-        onClick={() => onPageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}
-      >
-        &rsaquo;
-      </button>
-      <button
-        className={styles.pageButton}
-        onClick={() => onPageChange(totalPages)}
-        disabled={currentPage === totalPages}
-      >
-        &raquo;
-      </button>
-    </div>
-  );
 };
 
-const getLocations = async empId => {
+const getLocations = async (empId) => {
   return await fetchApiJson(`/supply/destinations/${empId}`)
     .then((body) => body);
 };
-const getItems = async locId => {
+
+const getItems = async (locId) => {
   return await fetchApiJson(`/supply/items/orderable/${locId}`)
     .then((body) => body.result);
 };
 
 export default function RequisitionFormIndex() {
   const auth = useAuth();
-  const [destination, setDestination] = useState(
-    () => JSON.parse(localStorage.getItem('destination')) || ''  );
+  const [locations, setLocations] = useState([]); // Initialize as an empty array
+  const [destination, setDestination] = useState();
   const [tempDestination, setTempDestination] = useState(destination);
-  const [locations, setLocations] = useState([]);
   const [items, setItems] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 16; // Adjust the number of items per page as needed
+  const [currentPage, setCurrentPage] = useState(1);
+  const [sortOption, setSortOption] = useState('name');
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    localStorage.setItem('destination', JSON.stringify(destination));
-    if (destination) {
-      fetchItems(destination);
-    }
-  }, [destination]);
-  useEffect(() => {
-    getLocations(auth.empId()).then(r => setLocations(r.result));
+    const fetchInitialData = async () => {
+      setIsLoading(true);
+      const fetchedLocations = await getLocations(auth.empId());
+      setLocations(fetchedLocations.result);
+      setIsLoading(false);
+    };
+    fetchInitialData();
   }, [auth]);
 
-  const fetchItems = async (destination) => {
-    const items = await getItems(destination);
-    setItems(items);
-  };
+  useEffect(() => {
+    if (destination) {
+      const fetchItems = async () => {
+        setIsLoading(true);
+        const fetchedItems = await getItems(destination);
+        setItems(fetchedItems);
+        setIsLoading(false);
+      };
+      fetchItems();
+    }
+  }, [destination]);
+
   const handleTempDestinationChange = (e) => {
     setTempDestination(e.target.value);
   };
+
   const handleConfirmClick = () => {
     setDestination(tempDestination);
     setCurrentPage(1);
   };
+
   const handleChangeClick = () => {
     setTempDestination('');
     setDestination('');
     setItems([]);
+  };
+
+  const handleSortChange = (e) => {
+    setSortOption(e.target.value);
   };
 
   const handlePageChange = (page) => {
@@ -226,15 +160,41 @@ export default function RequisitionFormIndex() {
     }
   };
 
+  if (isLoading || !locations.length) { // Adjust the condition to check for empty locations array
+    return (
+      <div>
+        <Hero>Requisition Form</Hero>
+        <LoadingIndicator />
+      </div>
+    );
+  }
+
   return (
     <div>
       <Hero>Requisition Form</Hero>
       {destination ? (
         <div>
-          <DestinationDetails destination={destination} handleChangeClick={handleChangeClick} items={items} />
-          <Pagination currentPage={currentPage} totalPages={Math.ceil(items.length / itemsPerPage)} onPageChange={handlePageChange} top={true} />
+          <DestinationDetails
+            locations={locations}
+            destination={destination}
+            handleChangeClick={handleChangeClick}
+            items={items}
+            handleSortChange={handleSortChange}
+            sortOption={sortOption}
+          />
+          <Pagination
+            currentPage={currentPage}
+            totalPages={Math.ceil(items.length / itemsPerPage)}
+            onPageChange={handlePageChange}
+            top={true}
+          />
           <ItemsGrid items={items} currentPage={currentPage} itemsPerPage={itemsPerPage} />
-          <Pagination currentPage={currentPage} totalPages={Math.ceil(items.length / itemsPerPage)} onPageChange={handlePageChange} top={false} />
+          <Pagination
+            currentPage={currentPage}
+            totalPages={Math.ceil(items.length / itemsPerPage)}
+            onPageChange={handlePageChange}
+            top={false}
+          />
         </div>
       ) : (
          <SelectDestination
