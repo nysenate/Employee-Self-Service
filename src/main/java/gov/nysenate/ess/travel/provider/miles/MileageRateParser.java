ff81dd498cb1c4dbf7a0455eaa9445f079eb6cbf
JacobKeegan <jacobmkeegan@gmail.com>
2024-06-17 13:15:52 -0400

    Changed code to more consistently parse mileage rates.

--- src/main/java/gov/nysenate/ess/travel/provider/miles/MileageRateParser.java
+++ src/main/java/gov/nysenate/ess/travel/provider/miles/MileageRateParser.java
@@ -4,7 +4,9 @@ import gov.nysenate.ess.core.util.DateUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -17,25 +19,19 @@ public final class MileageRateParser {
      * Parses the gsa irs reimbursement rates website to extract the current {@link MileageRate}.
      * <a href="https://www.gsa.gov/travel/plan-book/transportation-airfare-rates-pov-rates-etc/privately-owned-vehicle-pov-mileage-reimbursement-rates">...</a>
      * @param content The HTML content of the website.
-     * @return
+     * @return the parsed MileageRate.
      */
     public static MileageRate parseMileageRate(String content) {
         checkArgument(content != null, "content cannot be null.");
         checkArgument(!content.isEmpty(), "content cannot be an empty string.");
-
-        // Parse mileage rate.
-        Document document = Jsoup.parse(content);
-        Element bodyContentEl = document.getElementById("asto-content");
-        Element tableBodyEl = bodyContentEl.getElementsByTag("tbody").first();
-        Element autoRowEl = tableBodyEl.getElementsByTag("tr").get(1);
-        Element autoRateEl = autoRowEl.getElementsByTag("td").last();
-        String rate = autoRateEl.text().replace("$", "");
+        Element autoRowEl = Jsoup.parse(content).getElementsByTag("table").first()
+                .getElementsByTag("tbody").first().getElementsByTag("tr").get(1);
+        String rate = autoRowEl.getElementsByTag("td").last().text().replace("$", "");
 
         // Parse rate's start date.
         Element startDateEl = autoRowEl.select("td strong").first();
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("MMMM d, yyyy");
         LocalDate startDate = LocalDate.parse(startDateEl.text(), dateFormat);
-
         return new MileageRate(startDate, DateUtils.THE_FUTURE, rate);
     }
 }
