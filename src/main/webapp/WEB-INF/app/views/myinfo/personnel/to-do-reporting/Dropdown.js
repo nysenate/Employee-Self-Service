66bf6fc9fb66ee46ef54b29a91f10b9aaf048288
harshachittaluru <163359182+harshachittaluru@users.noreply.github.com>
2024-06-30 15:18:27 -0400

    Update Code

---
+++ src/main/webapp/WEB-INF/app/views/myinfo/personnel/to-do-reporting/Dropdown.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Dropdown.module.css';
+
+function Dropdown ({ options, selectedValue, onSelectedValueChange }) {
+  const keys = Object.keys(options);
+  return (
+    <select
+      className={styles.select}
+      value={selectedValue}
+      onChange={(e) => onSelectedValueChange(e.target.value)}
+    >
+      {keys.map((key) => (
+        <option key={key} value={options[key]}>
+          {options[key]}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Dropdown;
--- src/main/webapp/WEB-INF/app/views/myinfo/personnel/to-do-reporting/TrainingFilters.js
+++ src/main/webapp/WEB-INF/app/views/myinfo/personnel/to-do-reporting/TrainingFilters.js
@@ -2,13 +2,16 @@ import React from "react";
 import styles from "./TrainingFilters.module.css"
 import Card from "app/components/Card";
 import InputFilters from "app/views/myinfo/personnel/to-do-reporting/InputFilters";
+import EmployeeFilters from "app/views/myinfo/personnel/to-do-reporting/EmployeeFilters";
 export default function TrainingFilters(){
   return (
     <Card className={styles.card}>
-    <div className={styles.trainingFilters}>
-      <span className={styles.filterName}>Training Filters</span>
-      <InputFilters/>
-    </div>
+      <div className={styles.trainingFilters}>
+        <span className={styles.filterName}>Training Filters</span>
+        <InputFilters/>
+        <span className={styles.filterName}>Employee Filters</span>
+        <EmployeeFilters/>
+      </div>
     </Card>
   )
 }
