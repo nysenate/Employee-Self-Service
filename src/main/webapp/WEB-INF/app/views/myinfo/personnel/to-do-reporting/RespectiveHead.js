8e50c88311f1188c08d9c01c19281c51a1cc41ff
harshachittaluru <163359182+harshachittaluru@users.noreply.github.com>
2024-07-07 13:54:47 -0400

    Office Search

--- src/main/webapp/WEB-INF/app/views/myinfo/personnel/to-do-reporting/RespectiveHead.js
+++ src/main/webapp/WEB-INF/app/views/myinfo/personnel/to-do-reporting/RespectiveHead.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { Multiselect } from "multiselect-react-dropdown";
+import styles from "./EmployeeFilters.module.css";
 
 export default function RespectiveHead(){
-  const [activeTasks, setActiveTasks] = useState([]);
-  const [inActiveTasks, setInActiveTasks] = useState([]);
+  const [offices, setOffices] = useState([]);
+  const [selection, setSelection] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSelectChange = (selectedItems) => {
+    setSelection(selectedItems);
+  };
 
   useEffect(() => {
     const fetchRespectiveHeadDetails = async () => {
@@ -15,17 +22,68 @@ export default function RespectiveHead(){
           },
           cache: 'no-store',
         };
-        const response = await fetch(`/api/v1/respctr/head/search`, init);
+        const response = await fetch(`/api/v1/respctr/head/search?limit=20`, init);
         if (!response.ok) {
           throw new Error('Failed to fetch data');
         }
         const data = await response.json();
         console.log(data.result);
+        setOffices(data.result);
       } catch (error) {
         console.error('Error fetching task details:', error);
       }
     };
     fetchRespectiveHeadDetails();
   }, []);
-  return <div>I am done</div>;
+
+  return (
+    <div>
+      <select
+        multiple
+        value={selection}
+        onChange={(e) => handleSelectChange(Array.from(e.target.selectedOptions, option => option.value))}
+      >
+        {offices.map(rch => (
+          <option key={rch.code} value={rch.code}>
+            {rch.name}({rch.shortName})
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <Multiselect
+        options={offices}
+        displayValue="name"
+        style={{
+          chips: {
+            background: 'white',
+            borderRadius: '0px',
+            color: '#000',
+          },
+          searchBox: {
+            border: '1px solid #999',
+            borderRadius: '0px',
+            height: '25px',
+            width: '200px',
+            display: 'block',
+            fontFamily: 'inherit',
+          },
+          optionContainer: {
+            maxHeight: '200px',
+          },
+          option: {
+            fontFamily: 'inherit',
+            border: '1px solid #999',
+            backgroundColor: '#fff',
+            color: '#000',
+          },
+        }}
+      />
+    </div>
+  );
 }
+
