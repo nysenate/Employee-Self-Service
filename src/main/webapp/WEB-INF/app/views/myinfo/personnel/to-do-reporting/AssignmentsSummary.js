cda8cde18f19aebece77db0eeffbf84469b08305
Kevin Caseiras <kcaseiras@gmail.com>
2024-12-03 11:41:40 -0500

    Remove components which are no longer used

--- src/main/webapp/WEB-INF/app/views/myinfo/personnel/to-do-reporting/AssignmentsSummary.js
+++ src/main/webapp/WEB-INF/app/views/myinfo/personnel/to-do-reporting/AssignmentsSummary.js
@@ -1,29 +1,11 @@
 import React, { useState } from 'react';
-import EmployeeCount from "./EmployeeCount";
-import Employees from "./Employees";
 import PaginationComponent from "./PaginationComponent";
 import LoadingIndicator from "app/components/LoadingIndicator";
 import AssignmentsTable from "app/views/myinfo/personnel/to-do-reporting/AssignmentsTable";
 import { searchTaskAssignmentsQueryParams } from "app/api/searchTaskAssignmentsApi";
 import { setOffset } from "app/views/myinfo/personnel/to-do-reporting/todoReportingActions";
 
-// export default function Results({ params, onChildDataChange, finalData, loading, allTasks }) {
 export default function AssignmentsSummary({ taskAssignmentQuery, state, dispatch }) {
-
-  // const pageSize = 10; // Number of items per page
-  // const [ currentPage, setCurrentPage ] = useState(1);
-  // const handlePageChange = (page) => {
-  //   setCurrentPage(page);
-  //   const offset = (page - 1) * pageSize + 1;
-  //   onChildDataChange({ offset: offset });
-  // };
-
-  // const pageCount = finalData ? Math.ceil(finalData.total / pageSize) : 1;
-  // const pageCount = 1
-
-  // const handleChildDataChange = (data) => {
-  //   onChildDataChange(data);
-  // };
 
   const onPageChange = selectedPage => {
     const offset = (selectedPage - 1) * state.limit + 1
@@ -71,48 +53,3 @@ function CsvDownload({ state }) {
     </a>
   )
 }
-
-//   return (
-//     <div className={styles.card}>
-//
-//       {/*<EmployeeSearch*/}
-//       {/*  params={params}*/}
-//       {/*  onChildDataChange={handleChildDataChange}/>*/}
-//
-//       {taskAssignmentQuery.isPending ? (
-//         <div className="flex items-center justify-center">
-//           <LoadingIndicator/>
-//         </div>
-//       ) : (
-//          <>
-//            <EmployeeCount finalData={finalData}/>
-//            <br/>
-//            {/*{pageCount > 1 && (*/}
-//            {/*  <PaginationComponent*/}
-//            {/*    currentPage={currentPage}*/}
-//            {/*    totalPages={pageCount}*/}
-//            {/*    onPageChange={handlePageChange}*/}
-//            {/*  />*/}
-//            {/*)}*/}
-//            <Employees
-//              finalData={finalData}
-//              allTasks={allTasks}
-//              params={params}
-//              onChildDataChange={handleChildDataChange}/>
-//
-//            {/*{pageCount > 1 && (*/}
-//            {/*  <PaginationComponent*/}
-//            {/*    currentPage={currentPage}*/}
-//            {/*    totalPages={pageCount}*/}
-//            {/*    onPageChange={handlePageChange}*/}
-//            {/*  />*/}
-//            {/*)}*/}
-//          </>
-//        )}
-//       {/*TODO display error - error boundary?*/}
-//       {/*<p className={"text-red-600 font-extrabold text-2xl"}>*/}
-//       {/*Error Loading the Page*/}
-//       {/*</p>*/}
-//     </div>
-//   );
-//}
--- src/main/webapp/WEB-INF/app/views/myinfo/personnel/to-do-reporting/respctrHeadSearchApi.js
+++ src/main/webapp/WEB-INF/app/views/myinfo/personnel/to-do-reporting/respctrHeadSearchApi.js
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { fetchApiJson } from "app/utils/fetchJson";
 
-// /api/v1/respctr/head/search?limit=20&offset=1&term=tedi
 export function useResponsibilityCenterHeadSearch(term) {
   return useQuery({
     queryKey: ['respctr', 'head', 'search', term],
