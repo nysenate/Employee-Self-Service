0f1bdc8136de3840fe57695a8caec9931fc1e8de
Kevin Caseiras <kcaseiras@gmail.com>
2025-02-26 09:59:52 -0500

    Fix stub travel page

--- src/main/webapp/WEB-INF/app/views/travel/Travel.js
+++ src/main/webapp/WEB-INF/app/views/travel/Travel.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Travel() {
   return (
     <>
-      Travel
+      Travel Statistics
     </>
   )
 }
--- src/main/webapp/WEB-INF/app/views/travel/TravelRouter.js
+++ src/main/webapp/WEB-INF/app/views/travel/TravelRouter.js
@@ -2,22 +2,15 @@ import React from "react";
 import { ThemeContext, themes } from "app/contexts/ThemeContext";
 import Navigation from "app/components/Navigation";
 import AppLayout from "app/components/AppLayout";
-import { Navigate, Route, Routes } from "react-router-dom";
-import SummaryIndex from "app/views/myinfo/personnel/summary/SummaryIndex";
-import CheckHistoryIndex from "app/views/myinfo/payroll/checkhistory/CheckHistoryIndex";
-import EmergencyAlertInfoIndex from "app/views/myinfo/personnel/emergency-alert-info/EmergencyAlertInfoIndex";
-import TaskAssignmentsListIndex from "app/views/myinfo/personnel/pec/task-assignments/assignment-list/TaskAssignmentsListIndex";
-import TaskAssignmentIndex from "app/views/myinfo/personnel/pec/task-assignments/assignment-item/TaskAssignmentIndex";
-import ToDoAssignment from "./personnel/pec/to-do-assignment/ToDoAssignment";
-import ToDoReporting from "./personnel/pec/to-do-reporting/ToDoReporting";
 import Travel from "app/views/travel/Travel";
+import { Route, Routes } from "react-router-dom";
 
 export default function TravelRouter() {
   return (
     <ThemeContext.Provider value={themes.travel}>
       <Routes>
-        <Route path="" element={<TravelLayout />}>
-          <Route path="/stats" element={<Travel />} />
+        <Route path="" element={<TravelLayout/>}>
+          <Route path="/stats" element={<Travel/>}/>
         </Route>
       </Routes>
     </ThemeContext.Provider>
