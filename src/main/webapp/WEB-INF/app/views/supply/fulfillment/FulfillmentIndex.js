10b339138a3b8c0438a63ee4ba69523fb6e2960a
Kevin Caseiras <kcaseiras@gmail.com>
2025-07-22 14:37:50 -0400

    Create a reusable queue componenet

--- src/main/webapp/WEB-INF/app/views/supply/fulfillment/FulfillmentIndex.js
+++ src/main/webapp/WEB-INF/app/views/supply/fulfillment/FulfillmentIndex.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
 import Hero from "app/components/Hero";
-import PendingQueue from "app/views/supply/fulfillment/PendingQueue";
 import { useRequisitionSocket } from "app/views/supply/fulfillment/useRequisitionSocket";
 import { endOfDay, formatISO, startOfDay } from "date-fns";
 import LoadingIndicator from "app/components/LoadingIndicator";
 import { useRequisitionSearch } from "app/views/supply/useRequisitionSearch";
 import { useQueryClient } from "@tanstack/react-query";
-import ProcessingQueue from "app/views/supply/fulfillment/ProcessingQueue";
-import CompletedQueue from "app/views/supply/fulfillment/CompletedQueue";
-import ApprovedQueue from "app/views/supply/fulfillment/ApprovedQueue";
 import { useSearchParams } from "react-router-dom";
 import RequisitionEditModal from "app/views/supply/fulfillment/modal/RequisitionEditModal";
+import FulfillmentQueue from "app/views/supply/fulfillment/FilfillmentQueue";
+import { QUEUE_COLUMNS } from "app/views/supply/fulfillment/queueUtils";
+import RequisitionInfoModal from "app/views/supply/RequisitionInfoModal";
 
 export const REQUISITION_ID_SEARCH_PARAM = "requisitionId";
 
@@ -42,10 +41,10 @@ export default function FulfillmentIndex() {
     queryClient.invalidateQueries(["supply", "requisition", "list"]);
   });
 
-  // The Requisition edit modal is controlled by the search params. The modal is displayed when
+  // The Requisition modal is controlled by the search params. The modal is displayed when
   // REQUISITION_ID_SEARCH_PARAM is set and the relevant requisition is loaded.
   // e.g. /fulfillment?requisitionId=23 will display the modal for requisition 23.
-  const isEditModalOpen = () =>
+  const isModalOpen = () =>
     searchParams.get(REQUISITION_ID_SEARCH_PARAM) !== null;
 
   // The full requisition object for the requisition specified in search params or undefined.
@@ -56,6 +55,26 @@ export default function FulfillmentIndex() {
         parseInt(searchParams.get(REQUISITION_ID_SEARCH_PARAM)),
     );
 
+  // Display either the info or editable modal depending on the status of the requisition.
+  const displayRequisitionModal = (req) => {
+    if (req.status === "APPROVED" || req.status === "REJECTED") {
+      return (
+        <RequisitionInfoModal
+          isOpen={isModalOpen()}
+          onResolve={() => setSearchParams({})}
+          requisition={req}
+        />
+      );
+    }
+    return (
+      <RequisitionEditModal
+        isOpen={isModalOpen()}
+        onResolve={() => setSearchParams({})}
+        requisition={req}
+      />
+    );
+  };
+
   if (requisitionQuery.isPending) {
     return <LoadingIndicator />;
   }
@@ -64,41 +83,80 @@ export default function FulfillmentIndex() {
     <div>
       <Hero>Fulfillment</Hero>
       <div className="my-4">
-        <PendingQueue
+        <FulfillmentQueue
           requisitions={requisitionQuery.data.result.filter(
             (r) => r.status === "PENDING",
           )}
+          title="Pending Requisition Requests"
+          colorClass="bg-orange-600"
+          columns={[
+            QUEUE_COLUMNS.DELIVERY_METHOD_ICON,
+            QUEUE_COLUMNS.ID,
+            QUEUE_COLUMNS.LOCATION,
+            QUEUE_COLUMNS.CUSTOMER,
+            QUEUE_COLUMNS.ITEM_COUNT,
+            QUEUE_COLUMNS.ORDERED_DATE,
+            QUEUE_COLUMNS.ISSUER,
+          ]}
         />
       </div>
       <div className="my-4">
-        <ProcessingQueue
+        <FulfillmentQueue
           requisitions={requisitionQuery.data.result.filter(
             (r) => r.status === "PROCESSING",
           )}
+          title="Processing Requisition Requests"
+          colorClass="bg-teal-500"
+          columns={[
+            QUEUE_COLUMNS.DELIVERY_METHOD_ICON,
+            QUEUE_COLUMNS.ID,
+            QUEUE_COLUMNS.LOCATION,
+            QUEUE_COLUMNS.CUSTOMER,
+            QUEUE_COLUMNS.ITEM_COUNT,
+            QUEUE_COLUMNS.ORDERED_DATE,
+            QUEUE_COLUMNS.ISSUER,
+          ]}
         />
         <div className="my-4">
-          <CompletedQueue
+          <FulfillmentQueue
             requisitions={requisitionQuery.data.result.filter(
               (r) => r.status === "COMPLETED",
             )}
+            title="Completed Requisition Requests"
+            colorClass="bg-green-600"
+            columns={[
+              QUEUE_COLUMNS.DELIVERY_METHOD_ICON,
+              QUEUE_COLUMNS.ID,
+              QUEUE_COLUMNS.LOCATION,
+              QUEUE_COLUMNS.CUSTOMER,
+              QUEUE_COLUMNS.ITEM_COUNT,
+              QUEUE_COLUMNS.ORDERED_DATE,
+              QUEUE_COLUMNS.ISSUER,
+            ]}
           />
         </div>
         <div className="my-4">
-          <ApprovedQueue
+          <FulfillmentQueue
             requisitions={requisitionQuery.data.result.filter(
               (r) => r.status === "APPROVED",
             )}
+            title="Approved Requisition Requests"
+            colorClass="bg-purple-600"
+            columns={[
+              QUEUE_COLUMNS.DELIVERY_METHOD_ICON,
+              QUEUE_COLUMNS.ID,
+              QUEUE_COLUMNS.LOCATION,
+              QUEUE_COLUMNS.CUSTOMER,
+              QUEUE_COLUMNS.ITEM_COUNT,
+              QUEUE_COLUMNS.APPROVED_DATE,
+              QUEUE_COLUMNS.ISSUER,
+              QUEUE_COLUMNS.SYNC_STATUS_ICON,
+            ]}
           />
         </div>
       </div>
 
-      {isEditModalOpen() && (
-        <RequisitionEditModal
-          isOpen={isEditModalOpen()}
-          onResolve={() => setSearchParams({})}
-          requisition={selectedRequisition()}
-        />
-      )}
+      {isModalOpen() && displayRequisitionModal(selectedRequisition())}
     </div>
   );
 }
