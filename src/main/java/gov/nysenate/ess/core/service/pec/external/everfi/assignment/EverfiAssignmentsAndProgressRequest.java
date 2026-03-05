0a284263ea00002b37a9ebeac8787c2d29ae9f25
Kevin Caseiras <kcaseiras@gmail.com>
2026-03-05 16:05:06 -0500

    Clean up to the EverfiAssignmentsAndProgressRequest

--- src/main/java/gov/nysenate/ess/core/service/pec/external/everfi/assignment/EverfiAssignmentsAndProgressRequest.java
+++ src/main/java/gov/nysenate/ess/core/service/pec/external/everfi/assignment/EverfiAssignmentsAndProgressRequest.java
@@ -3,6 +3,8 @@ package gov.nysenate.ess.core.service.pec.external.everfi.assignment;
 import gov.nysenate.ess.core.service.pec.external.everfi.EverfiApiClient;
 import gov.nysenate.ess.core.util.OutputUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.util.Assert;
 
 import java.io.IOException;
 import java.util.List;
@@ -12,73 +14,79 @@ import java.util.List;
  */
 public class EverfiAssignmentsAndProgressRequest {
 
-    private static final String BASE_ENDPOINT = "/v1/progress/user_assignments";
-    private final String fullEndpoint;
-    private final EverfiApiClient httpClient;
+    private static final String USER_ASSIGNMENTS_PATH = "/v1/progress/user_assignments";
+    private final String path;
+    private final EverfiApiClient everfiApiClient;
     private final String scrollId;
     private final String since;
     private final int limit;
     private EverfiAssignmentsAndProgressResponse response;
 
-    private EverfiAssignmentsAndProgressRequest(String fullEndpoint, EverfiApiClient httpClient, String scrollId,
-                                               String since, int limit) {
-        this.fullEndpoint = fullEndpoint;
-        this.httpClient = httpClient;
+    private EverfiAssignmentsAndProgressRequest(
+            String path,
+            EverfiApiClient everfiApiClient,
+            String scrollId,
+            String since,
+            int limit) {
+
+        Assert.hasText(path, "path must not be empty");
+        Assert.notNull(everfiApiClient, "httpClient must not be null");
+        Assert.isTrue(limit >= 1 && limit <= 1000,
+                "limit must be between 1 and 1000 inclusive");
+
+        this.path = path;
+        this.everfiApiClient = everfiApiClient;
         this.scrollId = scrollId;
         this.since = since;
         this.limit = limit;
     }
 
-    public static EverfiAssignmentsAndProgressRequest allUserAssignments(EverfiApiClient everfiClient) {
-        return EverfiAssignmentsAndProgressRequest.allUserAssignments(everfiClient, "", 100);
-    }
-
     /**
      * Static Factory Constructor - constructs a request for getting all user assignments and progress.
-     * @param everfiClient
-     * @param since Optional, ISO date time string representing where we left off querying last time. Can be
-     *              used to prevent importing duplicate records.
-     * @param limit How many results to get in each request.
+     *
+     * @param everfiApiClient
+     * @param since           Optional, ISO date time string representing where we left off querying last time. Can be
+     *                        used to prevent importing duplicate records.
+     * @param limit           How many results to get in each request.
      * @return
      */
-    public static EverfiAssignmentsAndProgressRequest allUserAssignments(EverfiApiClient everfiClient,
+    public static EverfiAssignmentsAndProgressRequest allUserAssignments(EverfiApiClient everfiApiClient,
                                                                          String since, int limit) {
-        return new EverfiAssignmentsAndProgressRequest(BASE_ENDPOINT, everfiClient, null, since, limit);
-    }
-
-    public static EverfiAssignmentsAndProgressRequest userAssignments(EverfiApiClient everfiClient, String userUuid) {
-        return EverfiAssignmentsAndProgressRequest.userAssignments(everfiClient, userUuid, "", 100);
+        return new EverfiAssignmentsAndProgressRequest(USER_ASSIGNMENTS_PATH, everfiApiClient, null, since, limit);
     }
 
     /**
      * Static Factory Constructor - constructs a request for getting a single user's assignments and progress.
-     * @param everfiClient
-     * @param userUuid The everfi user uuid.
-     * @param since Optional, ISO date time string representing where we left off querying last time. Can be
-     *              used to prevent importing duplicate records.
-     * @param limit How many results to get in each request.
+     *
+     * @param everfiApiClient
+     * @param userUuid        The everfi user uuid.
+     * @param since           Optional, ISO date time string representing where we left off querying last time. Can be
+     *                        used to prevent importing duplicate records.
+     * @param limit           How many results to get in each request.
      * @return
      */
-    public static EverfiAssignmentsAndProgressRequest userAssignments(EverfiApiClient everfiClient, String userUuid,
+    public static EverfiAssignmentsAndProgressRequest userAssignments(EverfiApiClient everfiApiClient, String userUuid,
                                                                       String since, int limit) {
-        String endpoint = BASE_ENDPOINT + "/" + userUuid;
-        return new EverfiAssignmentsAndProgressRequest(endpoint, everfiClient, null, since, limit);
+        Assert.hasText(userUuid, "userUuid must not be empty");
+        String endpoint = USER_ASSIGNMENTS_PATH + "/" + userUuid;
+        return new EverfiAssignmentsAndProgressRequest(endpoint, everfiApiClient, null, since, limit);
     }
 
     /**
      * Get a page of results for this request
+     *
      * @return A list of {@link EverfiAssignmentAndProgress} or an empty list if there are no more results.
      * @throws IOException
      */
     public List<EverfiAssignmentAndProgress> fetch() throws IOException {
-        String data = httpClient.get(endpoint());
+        String data = everfiApiClient.get(buildRequestPath());
         response = OutputUtils.jsonToObject(data, EverfiAssignmentsAndProgressResponse.class);
         return response.getAssignmentsAndProgress();
     }
 
     /**
      * @return a new {@code EverfiAssignmentsAndProgressRequest} that can fetch the next page of results,
-     *         or null if there are no more pages to fetch.
+     * or null if there are no more pages to fetch.
      * @throws IllegalStateException if this method is called before a successful call to {@code fetch()}.
      */
     public EverfiAssignmentsAndProgressRequest next() {
@@ -88,7 +96,7 @@ public class EverfiAssignmentsAndProgressRequest {
         if (response.getNext().getScrollId() == null) {
             return null;
         }
-        return new EverfiAssignmentsAndProgressRequest(fullEndpoint, httpClient, response.getNext().getScrollId(),
+        return new EverfiAssignmentsAndProgressRequest(path, everfiApiClient, response.getNext().getScrollId(),
                 response.getNext().getSince(), this.limit);
     }
 
@@ -96,16 +104,14 @@ public class EverfiAssignmentsAndProgressRequest {
         return this.since;
     }
 
-    private String endpoint() {
-        StringBuilder builder = new StringBuilder(fullEndpoint);
-        builder.append("?scroll_size=" + limit);
-        if (StringUtils.isNotEmpty(scrollId)) {
-            builder.append("&scroll_id=" + scrollId);
-        }
-        if (StringUtils.isNotEmpty(since)) {
-            builder.append("&since=" + since);
-        }
+    private String buildRequestPath() {
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromPath(path)
+                .queryParam("scroll_size", limit);
 
-        return builder.toString();
+        if (StringUtils.isNotBlank(scrollId)) builder.queryParam("scroll_id", scrollId);
+        if (StringUtils.isNotBlank(since)) builder.queryParam("since", since);
+        return builder.toUriString();
     }
+
 }
